775ff168a5a261965d2bf39599d7466ecc3cc749
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-02-09 16:03:45 +0200

    Add timer methods that collect statistic to the .yaml file
    - decorator @duration(is_uniq) - uses timestat()
      * Usage:
    
        @duration
        def run_ostf(...):
    
    - context manager timestat(name, is_uniq)
      * Usage:
    
        with timestat('first_ostf_in_test'):
            run_ostf(...)
    
      Can be used to measure how long a method is executed, and store
      the collected data to the .yaml file in seconds.
    
      By default, .yaml files are created in the
      settings.LOGS_DIR folder using the unique file names:
      'timestat_YYYYMMDD_HHMMSS.yaml'
      Path and file name can be specified with environment variable:
      export TIMESTAT_YAML_PATH='./logs/statistic.yaml'
    
      * Variables:
        name : string, used as the name of yaml element where
               the time in seconds will be stored.
    
        is_uniq=True : tells to overwrite the previous collected data
                       for the method with the new data.
        is_uniq=False : tells to collect all measurements using
                        increasing numeric suffix.
    
      * Examples:
    
        -----------------------
        --- With the decorator:
        -----------------------
    
        @duration
        def deploy_cluster_wait(...):
    
        --- Resulting yaml:
    
        ha_flat_scalability:
          deploy_cluster_wait_00: '1023.45'
          deploy_cluster_wait_01: '1405.76'
          deploy_cluster_wait_02: '1920.32'
    
        -----------------------------
        --- With the context manager:
        -----------------------------
    
        with timestat('first_deploy', is_uniq=True):
            self.fuel_web.deploy_cluster_wait(cluster_id)
        ...
        with timestat('second_deploy', is_uniq=True):
            self.fuel_web.deploy_cluster_wait(cluster_id)
        ...
        with timestat('third_deploy'):
            self.fuel_web.deploy_cluster_wait(cluster_id)
        ...
        with timestat():
            self.fuel_web.deploy_cluster_wait(cluster_id)
        ...
        with timestat():
            self.fuel_web.deploy_cluster_wait(cluster_id)
    
        --- Resulting yaml:
    
        ha_flat_scalability:
          first_deploy: '1023.45'
          second_deploy: '1405.76'
          third_deploy_00: '1920.32'
          timestat_00: '2021.32'
          timestat_01: '2378.55'
    
    Change-Id: I56fdfd0b589d9ee85829a86cb7e35dc434b1bb1e
    Partly-Implements: blueprint granular-deployment-testing

--- fuelweb_test/helpers/decorators.py
+++ fuelweb_test/helpers/decorators.py
@@ -37,6 +37,7 @@ from fuelweb_test.helpers.regenerate_repo import CustomRepo
 from fuelweb_test.helpers.utils import get_current_env
 from fuelweb_test.helpers.utils import pull_out_logs_via_ssh
 from fuelweb_test.helpers.utils import store_astute_yaml
+from fuelweb_test.helpers.utils import timestat
 
 
 def save_logs(url, filename):
@@ -60,7 +61,7 @@ def log_snapshot_on_error(func):
     def wrapper(*args, **kwargs):
         logger.info("\n" + "<" * 5 + "#" * 30 + "[ {} ]"
                     .format(func.__name__) + "#" * 30 + ">" * 5 + "\n{}"
-                    .format(func.__doc__))
+                    .format(''.join(func.__doc__)))
         try:
             return func(*args, **kwargs)
         except SkipTest:
@@ -295,4 +296,12 @@ def download_astute_yaml(func):
                 logger.warning("Can't download astute.yaml: "
                                "Unexpected class is decorated.")
         return result
+    return wrapper
+
+
+def duration(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        with timestat(func.__name__):
+            return func(*args, **kwargs)
     return wrapper
