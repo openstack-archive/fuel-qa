aec75acac49fc757dd86793dfce90d1862e7af21
Tatyana Leontovich <tleontovich@mirantis.com>
2015-02-09 14:23:06 +0200

    Add test for single invocation of gd tasks
    Add support of api:
    * get tasks for cluster
    * get tasks for release
    * get tasks ended with
    * put task for cluster
    * put tasks for release
    
    Add version of base test for granular
    deployment:
    * Step 1 - deploy master node, create 2 node cluster,
      provision cnodes
    * Step 2 Run task end with host
    * Step 3 -8- run controller specific task
    * Step 9 - run compute task
    * Step 10 - run cinder task
    * Step 11 - run post -deployment
    
    The same in second test covers ha with 3 controller,
    mongo and ceph
    
    Change-Id: Idece933217b442bbe7353ce99eb0280c8b562952
    Implements: granular-deployment-testing

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1495,3 +1495,17 @@ class FuelWebClient(object):
         devops_node = self.find_devops_node_by_nailgun_fqdn(
             fqdn, self.environment.d_env.nodes().slaves)
         return devops_node
+
+    @logwrap
+    def get_nailgun_primary_mongo(self, slave):
+        #returns mongo that is primary in nailgun
+        remote = self.get_ssh_for_node(slave.name)
+        data = yaml.load(''.join(
+            remote.execute('cat /etc/astute.yaml')['stdout']))
+        node_name = [node['fqdn'] for node in data['nodes']
+                     if node['role'] == 'primary-mongo'][0]
+        logger.debug("node name is {0}".format(node_name))
+        fqdn = self.get_fqdn_by_hostname(node_name)
+        devops_node = self.find_devops_node_by_nailgun_fqdn(
+            fqdn, self.environment.d_env.nodes().slaves)
+        return devops_node
