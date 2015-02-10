fa981d320583142abf2760075aed57977ddfb895
Egor Kotko <ykotko@mirantis.com>
2015-02-10 19:22:19 +0100

    Command line tests
    - Offline node deletion, checking its absence
    into cobbler system list
    - Cluster deletion, checking its absence into db
    
    Change-Id: I47883924bcf3e668a9e501c6bcefdea8b5cc366a
    blueprint: remove-node-from-fuel-inventory

--- fuelweb_test/tests/test_cli.py
+++ fuelweb_test/tests/test_cli.py
@@ -14,10 +14,15 @@
 
 from proboscis import test
 from proboscis.asserts import assert_equal
+from proboscis.asserts import assert_true
+from proboscis.asserts import assert_false
 
 from devops.error import TimeoutError
 from devops.helpers.helpers import wait
+from devops.helpers.helpers import _wait
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.helpers.checkers import check_node
+from fuelweb_test.helpers.checkers import check_cluster_presence
 from fuelweb_test.settings import DEPLOYMENT_MODE
 from fuelweb_test.tests.base_test_case import TestBasic
 from fuelweb_test.tests.test_admin_node import TestAdminNodeCustomManifests
@@ -72,3 +77,89 @@ class CommandLine(TestBasic):
                               (node_id))['stdout'][0].rstrip()
         assert_equal(role, 'primary-controller', "node with deployed hiera "
                                                  "was not found")
+
+    @test(depends_on_groups=['prepare_ha_neutron'],
+          groups=["node_deletion_check"])
+    @log_snapshot_on_error
+    def node_deletion_check(self):
+        """
+        Scenario:
+            1. Revert snapshot 'prepare_ha_neutron'
+            2. Check 'slave-01' is present
+            3. Destroy 'slave-01'
+            4. Wait until 'slave-01' become offline
+            5. Delete offline 'slave-01' from db
+            6. Check presence of 'slave-01'
+
+        Duration 30m
+
+        """
+        self.env.revert_snapshot("prepare_ha_neutron")
+
+        remote = self.env.d_env.get_admin_remote()
+        hostname = (self.fuel_web.get_nailgun_node_by_devops_node(
+            self.env.d_env.nodes().slaves[0])
+            ['fqdn']).split('.')[0]
+        node_id = hostname.split('-')[1]
+        assert_true(check_node(remote, node_id),
+                    "node is not found")
+        self.env.d_env.nodes().slaves[0].destroy()
+        try:
+            wait(
+                lambda: not self.fuel_web.get_nailgun_node_by_devops_node(
+                    self.env.d_env.nodes().
+                    slaves[0])['online'], timeout=60 * 6)
+        except TimeoutError:
+            raise TimeoutError(
+                "slave-01 was not destroyed")
+        remote.execute(
+            'fuel node --node-id {0} --delete-from-db --force'.format(node_id))
+        try:
+            wait(
+                lambda: not remote.execute(
+                    "fuel node | awk '{{print $1}}' | grep -w '{0}'".
+                    format(node_id))['exit_code'] == 0, timeout=60 * 2)
+        except TimeoutError:
+            raise TimeoutError(
+                "After deletion node is found in fuel list")
+
+        assert_false(check_node(remote, node_id),
+                     "After deletion node is found in cobbler list")
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        self.fuel_web.verify_network(cluster_id)
+        _wait(lambda:
+              self.fuel_web.run_ostf(
+                  cluster_id=cluster_id, test_sets=['ha', 'smoke', 'sanity'],
+                  should_fail=1), interval=60, timeout=60 * 6)
+
+    @test(depends_on_groups=['prepare_ha_neutron'],
+          groups=["cluster_deletion"])
+    @log_snapshot_on_error
+    def cluster_deletion(self):
+        """
+        Scenario:
+            1. Revert snapshot 'deploy_ha'
+            2. Delete cluster via cli
+            3. Check cluster absence in the list
+
+        Duration 25m
+
+        """
+        self.env.revert_snapshot("deploy_ha")
+
+        remote = self.env.d_env.get_admin_remote()
+        cluster_id = self.fuel_web.get_last_created_cluster()
+        remote.execute('fuel --env {0} env delete'.format(cluster_id))
+        try:
+            wait(lambda:
+                 remote.execute(
+                     "fuel env |  awk '{print $1}' |  tail -n 1 | grep '^.$'")
+                 ['exit_code'] == 1, timeout=60 * 6)
+        except TimeoutError:
+            raise TimeoutError(
+                "cluster was not deleted")
+        assert_false(
+            check_cluster_presence(cluster_id, self.env.postgres_actions),
+            "cluster is found")
