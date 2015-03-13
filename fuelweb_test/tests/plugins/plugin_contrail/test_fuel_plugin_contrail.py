7e0c3a57cfcfd72794c4a54daa4ae0023d24cde3
Iryna Vovk <ivovk@mirantis.com>
2015-03-13 16:53:29 +0200

    "update_node_networks" method was updated for Contrail plugin and "Install plugin" test case was added
    In the original implementation it is impossible to execute tests for Contrail plugin. For "update_node_networks"
    method was added boolean flag that specifies whether to use standard method for updating data or specific for Contrail tests.
    Added methods for Contrail plugin installation with the following scenario:
    1. Upload plugin to the master node
    2. Install plugin with VLAN segmentation
    Added import in run_tests.py for running Contrail test cases and updated settings.py file
    
    Change-Id: Id05a77d368d32a4ca8645a759111d7a00d0fba9b

---
+++ fuelweb_test/tests/plugins/plugin_contrail/test_fuel_plugin_contrail.py
@@ -0,0 +1,71 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+import pdb
+from proboscis.asserts import assert_equal
+from proboscis import test
+
+from fuelweb_test import logger
+from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.helpers import checkers
+from fuelweb_test.settings import DEPLOYMENT_MODE_HA
+from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
+from fuelweb_test.settings import CONTRAIL_PLUGIN_PATH
+from fuelweb_test.settings import DEPLOYMENT_MODE
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+
+
+@test(groups=["plugins"])
+class ContrailPlugin(TestBasic):
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_9],
+          groups=["deploy_contrail_simple"])
+    @log_snapshot_on_error
+    def deploy_contrail_simple(self):
+        """Install Plugin
+
+        Scenario:
+            1. Upload plugin to the master node
+            2. Install plugin with VLAN segmentation
+
+        Snapshot deploy_contrail_simple
+
+        """
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        # copy plugin to the master node
+        checkers.upload_tarball(
+            self.env.d_env.get_admin_remote(),
+            CONTRAIL_PLUGIN_PATH, '/var')
+
+        # install plugin
+
+        checkers.install_plugin_check_code(
+            self.env.d_env.get_admin_remote(),
+            plugin=os.path.basename(CONTRAIL_PLUGIN_PATH))
+
+        segment_type = 'vlan'
+        self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type,
+            }
+        )
+
+        self.env.make_snapshot("deploy_contrail_simple")
+
+
