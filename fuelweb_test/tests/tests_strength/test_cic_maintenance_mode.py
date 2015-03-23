1282f5badf779b433e78e83b26820370ca2749fd
Veronica Krayneva <vkrayneva@mirantis.com>
2015-03-23 17:51:32 +0300

    Create tests for controller maintenance mode
    Add first test for feature, which deploys cluster
    and switch in maintenance mode on each controller.
    The second test which deploys the cluster and
    automatically goes into the maintenance mode
    when unexpected reboot.
    
    Implements: blueprint controller-maintenance-mode-tests
    
    Change-Id: Ib94c584ff8de06dfa8f9d3fb7b4477a362b9a433

---
+++ fuelweb_test/tests/tests_strength/test_cic_maintenance_mode.py
@@ -0,0 +1,178 @@
+import time
+
+from devops.helpers.helpers import wait
+from proboscis.asserts import assert_true
+from proboscis import test
+
+from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test import logger
+from fuelweb_test import settings
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+
+
+@test(groups=["cic_maintenance_mode"])
+class CICMaintenanceMode(TestBasic):
+
+    def check_available_mode(self, slave):
+        remote = self.fuel_web.get_ssh_for_node(slave)
+        command = ('umm status | grep runlevel &>/dev/null && echo "True" '
+                   '|| echo "False"')
+        return remote.execute(command)
+
+    def check_auto_mode(self, slave):
+        remote = self.fuel_web.get_ssh_for_node(slave)
+        command = ('umm status | grep umm &>/dev/null && echo "True" '
+                   '|| echo "False"')
+        return remote.execute(command)
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["manual_cic_maintenance_mode"])
+    @log_snapshot_on_error
+    def cic_maintenance_mode_single_node(self):
+        """Deploy cluster in HA mode with 3 controller
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 node with controller and mongo roles
+            3. Add 2 node with compute and cinder roles
+            4. Deploy the cluster
+            5. Switch in maintenance mode
+            6. Wait until controller is rebooting
+            7. Exit maintenance mode
+            8. Check the controller become available
+
+        Duration 155m
+        """
+        self.env.revert_snapshot("ready_with_5_slaves")
+        data = {
+            'ceilometer': True
+        }
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_HA,
+            settings=data)
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller', 'mongo'],
+                'slave-02': ['controller', 'mongo'],
+                'slave-03': ['controller', 'mongo'],
+                'slave-04': ['compute', 'cinder'],
+                'slave-05': ['compute', 'cinder']
+            }
+        )
+
+        # Cluster deploy
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        for nailgun_node in self.env.d_env.nodes().slaves[0:3]:
+            assert_true(self.check_available_mode(nailgun_node.name))
+
+            remote = self.fuel_web.get_ssh_for_node(nailgun_node.name)
+            logger.info('Maintenance mode for node %s', nailgun_node.name)
+            remote.execute('umm on')
+            logger.info('Wait a %s node offline status', nailgun_node.name)
+            wait(
+                lambda: not self.fuel_web.
+                get_nailgun_node_by_devops_node(nailgun_node)['online'],
+                timeout=60 * 10)
+
+            wait(lambda: remote, timeout=60 * 10)
+
+            assert_true(self.check_auto_mode(nailgun_node.name))
+            remote = self.fuel_web.get_ssh_for_node(nailgun_node.name)
+            remote.execute('umm off')
+
+            logger.info('Wait a %s node online status', nailgun_node.name)
+            wait(
+                lambda: self.fuel_web.
+                get_nailgun_node_by_devops_node(nailgun_node)['online'],
+                timeout=60 * 10)
+
+            # Wait for keystone-memcache consistency workaround
+            time.sleep(600)
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["auto_cic_maintenance_mode"])
+    @log_snapshot_on_error
+    def cic_maintenance_mode_single_node_auto(self):
+        """Deploy cluster in HA mode with 3 controller
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 node with controller and mongo roles
+            3. Add 2 node with compute and cinder roles
+            4. Deploy the cluster
+            5. Unexpected reboot
+            6. Wait until controller is switching in maintenance mode
+            7. Exit maintenance mode
+            8. Check the controller become available
+
+        Duration 155m
+        """
+        self.env.revert_snapshot("ready_with_5_slaves")
+        data = {
+            'ceilometer': True
+        }
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_HA,
+            settings=data)
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller', 'mongo'],
+                'slave-02': ['controller', 'mongo'],
+                'slave-03': ['controller', 'mongo'],
+                'slave-04': ['compute', 'cinder'],
+                'slave-05': ['compute', 'cinder']
+            }
+        )
+
+        # Cluster deploy
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        for nailgun_node in self.env.d_env.nodes().slaves[0:3]:
+            assert_true(self.check_available_mode(nailgun_node.name))
+
+            logger.info('Reboot node %s', nailgun_node.name)
+            remote = self.fuel_web.get_ssh_for_node(nailgun_node.name)
+            command1 = ("echo -e 'UMM=yes\nREBOOT_COUNT=0\n"
+                        "COUNTER_RESET_TIME=10' > /etc/umm.conf")
+            remote.execute(command1)
+            remote.execute('reboot --force >/dev/null &')
+
+            logger.info('Wait a %s node offline status', nailgun_node.name)
+            wait(
+                lambda: not self.fuel_web.
+                get_nailgun_node_by_devops_node(nailgun_node)['online'],
+                timeout=90 * 10)
+
+            assert_true(self.check_auto_mode(nailgun_node.name))
+            remote = self.fuel_web.get_ssh_for_node(nailgun_node.name)
+            remote.execute('umm off')
+
+            command2 = ("echo -e 'UMM=yes\nREBOOT_COUNT=2\n"
+                        "COUNTER_RESET_TIME=10' > /etc/umm.conf")
+            remote.execute(command2)
+            remote.execute('reboot')
+
+            logger.info('Wait a %s node offline status', nailgun_node.name)
+            wait(
+                lambda: not self.fuel_web.
+                get_nailgun_node_by_devops_node(nailgun_node)['online'],
+                timeout=90 * 10)
+
+            logger.info('Wait a %s node online status', nailgun_node.name)
+            wait(
+                lambda: self.fuel_web.
+                get_nailgun_node_by_devops_node(nailgun_node)['online'],
+                timeout=90 * 10)
+
+            # Wait for keystone-memcache consistency workaround
+            time.sleep(900)
