c7557ec87ce9e61f1da19cafa399b77b4979ab31
Maksym Strukov <mstrukov@mirantis.com>
2015-03-30 20:35:45 +0300

    Add test for installing Fuel from USB instead of cdrom
    Testing Fuel installing from USB drive
    This patch has depends in fuel-devops,
    so please merge after https://review.openstack.org/#/c/169018/
    
    Change-Id: I1747a496ef83abb8f23544a04fa37f239c4013e3
    Closes-Bug: #1425941

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -23,6 +23,8 @@ ENV_NAME = os.environ.get("ENV_NAME", "fuel_system_test")
 VIRTUAL_ENV = os.environ.get("VIRTUAL_ENV", "")
 
 ISO_PATH = os.environ.get('ISO_PATH')
+# cdrom or usb
+ISO_CONNECT_AS = os.environ.get('ISO_CONNECT_AS', 'cdrom')
 DNS = os.environ.get('DNS', '8.8.8.8')
 
 OPENSTACK_RELEASE_CENTOS = 'centos'
