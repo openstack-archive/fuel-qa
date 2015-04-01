e579520709d2a87a6ef75d0d419e5b29fe2ff895
NastyaUrlapova <aurlapova@mirantis.com>
2015-04-01 16:01:16 +0300

    Install fuel's packages on master node
    Change-Id: I9e6069da70e510c3dad2c3b09df1d83dc4703054

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -29,6 +29,7 @@ from fuelweb_test.helpers import checkers
 from fuelweb_test import logwrap
 from fuelweb_test import logger
 from fuelweb_test.helpers.decorators import custom_repo
+from fuelweb_test.helpers.decorators import update_fuel
 from fuelweb_test.helpers.decorators import download_astute_yaml
 from fuelweb_test.helpers.decorators import duration
 from fuelweb_test.helpers.decorators import update_ostf
@@ -310,6 +311,7 @@ class FuelWebClient(object):
     @logwrap
     @upload_manifests
     @update_ostf
+    @update_fuel
     def create_cluster(self,
                        name,
                        settings=None,
