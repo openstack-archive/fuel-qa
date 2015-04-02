3cc8dd220ad3322218eb8db03795068bb8850cb2
asledzinskiy <asledzinskiy@mirantis.com>
2015-04-02 17:47:14 +0300

    patching
    Change-Id: I972848ede2a8cc6f5c33044b111dcd90e30d2e1b

--- fuelweb_test/tests/tests_patching/test_patching.py
+++ fuelweb_test/tests/tests_patching/test_patching.py
@@ -16,12 +16,17 @@ import time
 
 from proboscis import test
 from proboscis.asserts import assert_is_not_none
+from proboscis.asserts import assert_true
 
+from devops.helpers.helpers import wait
+from devops.error import TimeoutError
 from fuelweb_test import logger
 from fuelweb_test import settings
 from fuelweb_test.helpers import patching
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.tests.test_ha_one_controller import HAOneControllerFlat
 
 
 @test(groups=["patching"])
@@ -101,6 +106,81 @@ class PatchingTests(TestBasic):
         # Step #9
         # Run Rally benchmarks, compare new results with previous,
         # coming soon...
+
+
+@test(groups=["patching_master"])
+class PatchingMasterTests(TestBasic):
+
+    def __init__(self):
+        self.pkgs = settings.PATCHING_PKGS
+        super(PatchingMasterTests, self).__init__()
+
+    @test(groups=["patching_master"],
+          depends_on=[HAOneControllerFlat.deploy_ha_one_controller_flat])
+    @log_snapshot_on_error
+    def patching_master(self):
+        """Apply patches on deployed master
+
+        Scenario:
+        1. Revert snapshot of deployed master
+        2. Run Rally benchmark tests and store results
+        3. Download patched packages on master node and make local repositories
+        4. Perform actions required to apply patches
+        5. Verify that fix works
+        6. Run OSTF
+        7. Run Rally benchmark tests and compare results
+
+        Duration 15m
+        Snapshot patching_master
+        """
+
+        if not self.env.revert_snapshot(self.snapshot_name):
+            raise PatchingTestException('Environment revert from snapshot "{0}'
+                                        '" failed.'.format(self.snapshot_name))
+
+        # Step #1
+        patching_repos = patching.add_remote_repositories(self.env)
+
+        for repo in patching_repos:
+            patching.connect_admin_to_repo(self.env, repo)
+
+        # Step #2
+        logger.info('Applying fix...')
+        patching.apply_patches(self.env)
+
+        # Step #3
+        logger.info('Verifying fix...')
+        patching.verify_fix(self.env)
+
+        # Step #4
+        cluster_id = self.fuel_web.get_last_created_cluster()
+        if self.fuel_web.get_last_created_cluster():
+            self.fuel_web.run_ostf(cluster_id=cluster_id)
+            self.fuel_web.verify_network(cluster_id)
+            self.fuel_web.stop_reset_env_wait(cluster_id)
+            for nailgun_node in self.fuel_web.client.list_cluster_nodes(
+                    cluster_id):
+                try:
+                    wait(lambda: nailgun_node['online'], timeout=60 * 10)
+                except TimeoutError:
+                    assert_true(nailgun_node['Online'],
+                                '{0} should be online, but it is not'.format(
+                                    nailgun_node['name']))
+            self.fuel_web.client.delete_cluster(cluster_id)
+            for nailgun_node in self.fuel_web.client.list_nodes():
+                try:
+                    wait(lambda: nailgun_node['status'] == 'discover',
+                         timeout=60 * 4)
+                except TimeoutError:
+                    assert_true(nailgun_node['status'] == 'discover',
+                                '{0} should be discovered, but it is not'.
+                                format(nailgun_node['name']))
+        for node in self.env.d_env.nodes().slaves[2:5]:
+                node.start()
+                time.sleep(2)
+        self.fuel_web.wait_nodes_get_online_state(
+            self.env.d_env.nodes().slaves[2:5], timeout=6 * 60)
+        self.env.make_snapshot("prepare_5_slaves", is_make=True)
 
 
 class PatchingTestException(Exception):
