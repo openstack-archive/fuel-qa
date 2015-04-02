b131df13d718e296e24ef42d72bb9dc9a9ed2ca3
asledzinskiy <asledzinskiy@mirantis.com>
2015-04-02 17:47:14 +0300

    Add test for master patching
    - Add tests to patch master node with existing environment,
    reset and delete cluster and then deploy new cluster
    
    Change-Id: I972848ede2a8cc6f5c33044b111dcd90e30d2e1b
    Implements: blueprint mos-patching-tests

--- fuelweb_test/tests/tests_patching/test_patching.py
+++ fuelweb_test/tests/tests_patching/test_patching.py
@@ -16,11 +16,15 @@ import time
 
 from proboscis import test
 from proboscis.asserts import assert_is_not_none
+from proboscis.asserts import assert_true
 
+from devops.error import TimeoutError
+from devops.helpers.helpers import wait
 from fuelweb_test import logger
 from fuelweb_test import settings
 from fuelweb_test.helpers import patching
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.helpers.utils import install_pkg
 from fuelweb_test.tests.base_test_case import TestBasic
 
 
@@ -101,6 +105,150 @@ class PatchingTests(TestBasic):
         # Step #9
         # Run Rally benchmarks, compare new results with previous,
         # coming soon...
+
+
+@test(groups=["patching_master_tests"])
+class PatchingMasterTests(TestBasic):
+
+    def __init__(self):
+        self.snapshot_name = settings.PATCHING_SNAPSHOT
+        self.pkgs = settings.PATCHING_PKGS
+        super(PatchingMasterTests, self).__init__()
+
+    @test(groups=['prepare_master_environment'])
+    def prepare_patching_environment(self):
+        logger.debug('Creating snapshot of environment deployed for patching.')
+        self.env.make_snapshot(snapshot_name=self.snapshot_name,
+                               is_make=True)
+
+    @test(groups=["patching_test"],
+          depends_on_groups=['prepare_master_environment'])
+    @log_snapshot_on_error
+    def patching_test(self):
+        """Apply patches on deployed master
+
+        Scenario:
+        1. Download patched packages on master node and make local repositories
+        2. Perform actions required to apply patches
+        3. Verify that fix works
+        4. Run OSTF
+        5. Run network verification
+        6. Reset and delete cluster
+        7. Bootstrap 3 slaves
+
+        Duration 30m
+        """
+
+        if not self.env.revert_snapshot(self.snapshot_name):
+            raise PatchingTestException('Environment revert from snapshot "{0}'
+                                        '" failed.'.format(self.snapshot_name))
+
+        # Step #1
+        remote = self.env.d_env.get_admin_remote()
+        install_pkg(remote, 'yum-utils')
+        patching_repos = patching.add_remote_repositories(self.env)
+
+        for repo in patching_repos:
+            patching.connect_admin_to_repo(self.env, repo)
+
+        # Step #2
+        logger.info('Applying fix...')
+        patching.apply_patches(self.env)
+
+        # Step #3
+        logger.info('Verifying fix...')
+        patching.verify_fix(self.env)
+
+        # Step #4
+        active_nodes = []
+        for node in self.env.d_env.nodes().slaves:
+            if node.driver.node_active(node):
+                active_nodes.append(node)
+        logger.debug('active nodes are {}'.format(active_nodes))
+        cluster_id = self.fuel_web.get_last_created_cluster()
+        if self.fuel_web.get_last_created_cluster():
+            number_of_nodes = len(self.fuel_web.client.list_cluster_nodes(
+                cluster_id))
+            self.fuel_web.run_ostf(cluster_id=cluster_id)
+            if number_of_nodes > 1:
+                self.fuel_web.verify_network(cluster_id)
+            self.env.bootstrap_nodes(
+                self.env.d_env.nodes().
+                slaves[number_of_nodes:number_of_nodes + 1])
+            self.fuel_web.update_nodes(
+                cluster_id,
+                {'slave-0{}'.format(number_of_nodes + 1): ['compute']},
+                True, False)
+            self.fuel_web.deploy_cluster_wait(cluster_id)
+            nailgun_nodes = self.fuel_web.update_nodes(
+                cluster_id,
+                {'slave-0{}'.format(number_of_nodes + 1): ['compute']},
+                False, True)
+            task = self.fuel_web.deploy_cluster(cluster_id)
+            self.fuel_web.assert_task_success(task)
+            nodes = filter(
+                lambda x: x["pending_deletion"] is True, nailgun_nodes)
+            wait(
+                lambda: self.fuel_web.is_node_discovered(nodes[0]),
+                timeout=6 * 60)
+
+            self.fuel_web.stop_reset_env_wait(cluster_id)
+            self.fuel_web.wait_nodes_get_online_state(
+                active_nodes, timeout=10 * 60)
+            self.fuel_web.client.delete_cluster(cluster_id)
+            try:
+                wait((lambda: len(
+                    self.fuel_web.client.list_nodes()) == number_of_nodes),
+                    timeout=5 * 60)
+            except TimeoutError:
+                assert_true(len(
+                    self.fuel_web.client.list_nodes()) == number_of_nodes,
+                    'Nodes are not discovered in timeout 5 *60')
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[:3])
+
+    @test(groups=["patching_master"],
+          depends_on_groups=['patching_test'])
+    @log_snapshot_on_error
+    def patching_master(self):
+        """
+        Deploy cluster after master node patching
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller role
+            3. Add 2 nodes with compute role
+            4. Deploy the cluster
+            5. Run network verification
+            6. Run OSTF
+
+            Duration 50m
+        """
+        segment_type = 'vlan'
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type,
+                'tenant': 'neutronVlan',
+                'user': 'neutronVlan',
+                'password': 'neutronVlan'
+            }
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+                'slave-03': ['compute']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.verify_network(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id)
 
 
 class PatchingTestException(Exception):
