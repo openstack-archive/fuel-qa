33e26a29734c9632c85699d3bc1fdb651d72250f
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-04-16 19:47:34 +0300

    Add auxiliary repositories to a just created cluster
    - after uploading files to auxiliary repos, add the appropriate
    repository to the last created cluster
    
    - fix some issues in regenerate_repo_* scripts to allow working
    with completely empty repositories.
    
    Note that for patching feature the following variables should
    point to the local auxiliary repositories:
    
    export LOCAL_MIRROR_UBUNTU='/var/www/nailgun/ubuntu/auxiliary/'
    export LOCAL_MIRROR_CENTOS='/var/www/nailgun/centos/auxiliary/'
    
    Change-Id: If3a734550659f462a3911da93c6107026ad5702a

--- fuelweb_test/helpers/decorators.py
+++ fuelweb_test/helpers/decorators.py
@@ -153,25 +153,41 @@ def update_fuel(func):
 
             remote = environment.d_env.get_admin_remote()
 
-            cond_upload(remote,
-                        settings.UPDATE_FUEL_PATH,
-                        settings.LOCAL_MIRROR_CENTOS,
-                        "(?i).*\.rpm$")
-            regenerate_centos_repo(remote, settings.LOCAL_MIRROR_CENTOS)
+            centos_files_count = cond_upload(
+                remote, settings.UPDATE_FUEL_PATH,
+                os.path.join(settings.LOCAL_MIRROR_CENTOS,'Packages'),
+                "(?i).*\.rpm$")
 
-            cond_upload(remote,
-                        settings.UPDATE_FUEL_PATH,
-                        settings.LOCAL_MIRROR_UBUNTU,
-                        "(?i).*\.deb$")
-            regenerate_ubuntu_repo(remote, settings.LOCAL_MIRROR_UBUNTU)
+            ubuntu_files_count = cond_upload(
+                remote, settings.UPDATE_FUEL_PATH,
+                os.path.join(settings.LOCAL_MIRROR_UBUNTU,'pool/main'),
+                "(?i).*\.deb$")
 
-            try:
-                remote.execute("for container in $(dockerctl list); do"
-                               " dockerctl shell $container bash -c \"yum "
-                               "clean expire-cache;yum update -y\";dockerctl "
-                               "restart $container; done")
-            except Exception:
-                logger.exception("Fail update of Fuel's package(s).")
+            if centos_files_count > 0:
+                regenerate_centos_repo(remote, settings.LOCAL_MIRROR_CENTOS)
+                try:
+                    logger.info("Updating packages in docker containers ...")
+                    remote.execute("for container in $(dockerctl list); do"
+                                   " dockerctl shell $container bash -c \"yum "
+                                   "clean expire-cache;yum update -y\";"
+                                   "dockerctl restart $container; done")
+                except Exception:
+                    logger.exception("Fail update of Fuel's package(s).")
+
+                logger.info("Waiting for nailgun container ...")
+                environment.nailgun_actions.wait_for_ready_container()
+
+            if ubuntu_files_count > 0:
+                regenerate_ubuntu_repo(remote, settings.LOCAL_MIRROR_UBUNTU)
+
+            cluster_id = environment.fuel_web.get_last_created_cluster()
+            if settings.OPENSTACK_RELEASE_UBUNTU in settings.OPENSTACK_RELEASE:
+                environment.fuel_web.add_local_ubuntu_mirror(
+                    cluster_id, path=settings.LOCAL_MIRROR_UBUNTU)
+            else:
+                environment.fuel_web.add_local_centos_mirror(
+                    cluster_id, path=settings.LOCAL_MIRROR_CENTOS)
+
         return result
     return wrapper
 
