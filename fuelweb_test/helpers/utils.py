21865736652b1af8434a24f6e9db0db3953739df
Tatyana Leontovich <tleontovich@mirantis.com>
2015-04-20 14:24:34 +0300

    Merge tasks end with hosts and virtual_ip in gd
    To get connectivity from computes and cinders
    for firewall tasks change task list for the first step
    from end with host to end with virtial_ips for controller
    in neutron_gre
    
    Change-Id: I57f143697a91692bae1717e94a9dd9cd8483845f
    Closes-Bug: #1446138

--- fuelweb_test/helpers/utils.py
+++ fuelweb_test/helpers/utils.py
@@ -132,6 +132,8 @@ def get_current_env(args):
         return args[0]
     elif args[0].__class__.__name__ == "FuelWebClient":
         return args[0].environment
+    elif args[0].__class__.__name__ == "NeutronGre":
+        return args[0].env
     else:
         logger.warning("Unexpected class!")
 
