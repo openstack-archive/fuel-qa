5579cfedc8e82a5ecf618dfb6a9091b9000d6939
Oleksii Zhurba <ozhurba@mirantis.com>
2015-05-12 17:31:36 +0300

    Initial commit for Cisco ACI plugin tests
    Change-Id: I11da7ae750662c1139a7f74c19a21e48a918d8da

--- fuelweb_test/run_tests.py
+++ fuelweb_test/run_tests.py
@@ -38,6 +38,7 @@ def import_tests():
     from tests import test_zabbix  # noqa
     from tests import test_upgrade  # noqa
     from tests.plugins.plugin_emc import test_plugin_emc  # noqa
+    from tests.plugins.plugin_cisco_aci import test_plugin_cisco_aci  # noqa
     from tests.plugins.plugin_example import test_fuel_plugin_example  # noqa
     from tests.plugins.plugin_contrail import test_fuel_plugin_contrail  # noqa
     from tests.plugins.plugin_glusterfs import test_plugin_glusterfs  # noqa
