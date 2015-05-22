3b608bf2d8477803d48dffacbd0155f7f01c76ff
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-05-22 22:50:06 +0300

    Fix Neutron L3 destructive tests by using proper SSH connection
    After a controller rebooted/destroyed, check_instance_connectivity
    should use remote connetion to an online controller that wont be
    rebooted. Will be used controller from which L3 router agent
    is removed.
    
    Change-Id: I19328dfd4bb8ffbf2035197395fa6c3067a01384
    Closes-Bug:#1458020

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -213,15 +213,15 @@ class FuelWebClient(object):
         if failed_test_name:
             for test_name in failed_test_name:
                 assert_true(test_name in actual_failed_names,
-                            'WARNINg unexpected fail,'
-                            'expected {0} actual {1}'.format(
+                            'WARNING! Unexpected fail: '
+                            'expected {0}, actual {1}'.format(
                                 failed_test_name, actual_failed_names))
 
         assert_true(
-            failed <= should_fail, 'Failed tests,  fails: {} should fail:'
-                                   ' {} failed tests name: {}'
-                                   ''.format(failed, should_fail,
-                                             failed_tests_res))
+            failed <= should_fail, 'Failed {0} OSTF tests; should fail'
+                                   ' {1} tests. Names of failed tests: {2}'
+                                   .format(failed, should_fail,
+                                           failed_tests_res))
 
     def assert_release_state(self, release_name, state='available'):
         logger.info('Assert release %s has state %s', release_name, state)
