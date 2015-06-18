65f8e63a0556cad35b13fe8fdab7f13420c42d26
Alexandr Kostrikov <akostrikov@mirantis.com>
2015-06-18 13:18:55 +0300

    Fix for miratis doc templates.
    To make fuel-qa docs suitable as a
    documentation it has to be in
    Mirantis style.
    This patch adds common mirantis templates.
    To view it online, please refer to
    https://ci.fuel-infra.org/job/verify-fuel-qa-docs/152/Fuel_QA_Docs_build_results/
    
    Change-Id: I8dfecaa9cd38f6b54e2d05661aa42e71e5649c2a

--- doc/conf.py
+++ doc/conf.py
@@ -27,14 +27,43 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
-autodoc_default_flags = ['members', 'show-inheritance', 'inherited-members']
-autodoc_member_order = 'bysource'
-
+project = 'Fuel QA'
 source_suffix = '.rst'
-
 master_doc = 'index'
 
-project = 'Fuel QA'
+pygments_style = 'sphinx'
+templates_path = ['_templates']
+html_theme = 'mirantis'
+htmlhelp_basename = 'fuel-qadoc'
+html_favicon = '_static/favicon.ico'
+
+html_theme_path = ['_templates']
+html_static_path = ['_static']
+htmlhelp_basename = 'fueldoc'
+html_use_smartypants = False
+html_use_index = True
+html_split_index = False
+html_show_sourcelink = False
+html_show_sphinx = False
+html_show_copyright = True
+
+html_theme_options = {
+    'navbar_title': "Documentation",
+    'navbar_site_name': "Modules",
+    'navbar_sidebarrel': True,
+    'navbar_pagenav': True,
+    'navbar_pagenav_name': "Section",
+    'globaltoc_depth': 2,
+    'globaltoc_includehidden': "true",
+    'navbar_class': "navbar",
+    'navbar_fixed_top': "true",
+    'source_link_position': "nav",
+    'bootswatch_theme': "yeti",
+    'bootstrap_version': "3",
+}
+
+autodoc_default_flags = ['members', 'show-inheritance', 'inherited-members']
+autodoc_member_order = 'bysource'
 copyright = 'Copyright 2015 Mirantis, Inc.' \
             'Licensed under the Apache License, Version 2.0' \
             ' (the "License"); you may not use this file except in' \
@@ -42,12 +71,5 @@ copyright = 'Copyright 2015 Mirantis, Inc.' \
             ' of the License at http://www.apache.org/licenses/LICENSE-2.0'
 
 exclude_patterns = ['_build']
-
-pygments_style = 'sphinx'
-
-html_theme = 'classic'
-html_static_path = ['_static']
-htmlhelp_basename = 'fuel-qadoc'
-html_favicon = '_static/favicon.ico'
 
 intersphinx_mapping = {'http://docs.python.org/': None}
