aec18a8a67bd82663e647bb1005d6175c3fe6dfc
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-07-01 18:28:45 +0300

    Template based tests with yaml configs
    Rework fuleweb_test framworks for automation generating TestCase
    
    put yaml confs into fuelweb_test/system_tests_templates
    define Case in fuelweb_test/actions_tests
    proboscis generate group of test for each case and config
    
    For sen new test plans use Jenkins script.
        for all matrix:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests \
                -o --show-plan -k -K
    
        for some case and all configuration:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.create_deploy_ostf \
                -o --show-plan -k -K
    
        for some case and some config
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.create_deploy_ostf.1ctrl_1comp_1cinder_neutronVLAN \
                -o --show-plan -k -K
    
        for run all destroy controllers failover cases
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.failover.destroy_controllers.3ctrl_2comp_1cndr_neutronVLAN \
                -o --show-plan -k -K
    
    Change-Id: I81eeb0df84a0f373be029f20ec6c53e8d3596596

---
+++ fuelweb_test/actions_tests/strength/destroy_controllers.py
@@ -0,0 +1,126 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test.helpers.utils import case_factory
+from proboscis import factory
+
+from fuelweb_test.actions_tests.strength import strenght_base
+from fuelweb_test.helpers.decorators import make_snapshot_if_step_fail
+from fuelweb_test.helpers.decorators import deferred_decorator
+
+
+class StrenghtDestroyFirstContorller(strenght_base.StrenghtBaseActions):
+    """Destroy two controllers and check pacemaker status is correct
+
+    Scenario:
+        1. Create Environment
+        2. Add nodes to Environment
+        3. Run network checker
+        4. Deploy Environment
+        5. Run network checker
+        9. Run OSTF
+        6. Make or use existen snapshot of ready Environment
+        7. Destroy first controller
+        8. Check pacemaker status
+        9. Wait offlie status in nailgun
+        10. Run OSTF
+
+    """
+
+    base_group = ['actions_tests',
+                  'actions_tests.failover',
+                  'actions_tests.failover.destroy_controllers',
+                  'actions_tests.failover.destroy_controllers.second']
+
+    actions_order = [
+        '_action_setup_master',
+        '_action_config_release',
+        '_action_make_slaves',
+        '_action_revert_slaves',
+        '_action_create_env',
+        '_action_add_nodes',
+        '_action_network_check',
+        '_action_deploy_cluster',
+        '_action_network_check',
+        '_action_health_check',
+        '_action_save_load_environment',
+        '_action_destory_first_controller',
+        '_action_check_pacemaker_status',
+        '_action_wait_offline_nodes',
+        '_action_check_ha_service_ready',
+        '_action_check_os_services_ready',
+        '_action_wait_galera_cluster',
+        '_action_health_check',
+    ]
+
+    @deferred_decorator([make_snapshot_if_step_fail])
+    def _action_destory_first_controller(self):
+        """Destory first controller"""
+        super(self.__class__, self)._destory_controller('slave-01')
+
+
+class StrenghtDestroySecondContorller(strenght_base.StrenghtBaseActions):
+    """Destroy two controllers and check pacemaker status is correct
+
+    Scenario:
+        1. Create Environment
+        2. Add nodes to Environment
+        3. Run network checker
+        4. Deploy Environment
+        5. Run network checker
+        9. Run OSTF
+        6. Make or use existen snapshot of ready Environment
+        7. Destroy second controller
+        8. Check pacemaker status
+        9, Wait offlie status in nailgun
+        10. Run OSTF
+
+    """
+
+    base_group = ['actions_tests',
+                  'actions_tests.failover',
+                  'actions_tests.failover.destroy_controllers',
+                  'actions_tests.failover.destroy_controllers.second']
+
+    actions_order = [
+        '_action_setup_master',
+        '_action_config_release',
+        '_action_make_slaves',
+        '_action_revert_slaves',
+        '_action_create_env',
+        '_action_add_nodes',
+        '_action_network_check',
+        '_action_deploy_cluster',
+        '_action_network_check',
+        '_action_health_check',
+        '_action_save_load_environment',
+        '_action_destory_second_controller',
+        '_action_check_pacemaker_status',
+        '_action_wait_offline_nodes',
+        '_action_check_ha_service_ready',
+        '_action_check_os_services_ready',
+        '_action_wait_galera_cluster',
+        '_action_health_check',
+    ]
+
+    @deferred_decorator([make_snapshot_if_step_fail])
+    def _action_destory_second_controller(self):
+        """Destory second controller"""
+        super(self.__class__, self)._destory_controller('slave-02')
+
+
+@factory
+def cases():
+    return (case_factory(StrenghtDestroyFirstContorller) +
+            case_factory(StrenghtDestroySecondContorller))
