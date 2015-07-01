ed9523aba416438e67416445290b8489c5211874
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2015-07-01 18:28:45 +0300

    [WIP] Acceptance test cases in yaml
    Rework fuleweb_test framworks for automation generating TestCase
    
    put yaml conf into fuelweb_test/environments
    define Case in fuelweb_test/acceptance_tests
    proboscis generate group of test for each case and config
    
    For sen new test plans use Jenkins script.
        for all matrix:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=acceptance -o --show-plan -k -K
    
        for some case and all configuration:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=acceptance.create_deploy_ostf -o --show-plan -k -K
    
        for some case and some config
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=acceptance.create_deploy_ostf.1ctrl_1comp_1cinder_neutronVLAN -o --show-plan -k -K
    
    Change-Id: I81eeb0df84a0f373be029f20ec6c53e8d3596596

---
+++ fuelweb_test/acceptance_tests/test_create_deploy_stop.py
@@ -0,0 +1,42 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test.acceptance_tests import actions_base
+from proboscis import factory
+
+
+class CreateDeployStop(actions_base.ActionsBase):
+    """docstring for CreateDeployDelete"""
+
+    base_group = ['acceptance', 'acceptance.create_deploy_stop']
+    actions_order = [
+        '_action_setup_master',
+        '_action_config_release',
+        '_action_make_slaves',
+        '_action_revert_slaves',
+        '_action_create_env',
+        '_action_add_nodes',
+        '_action_network_check',
+        '_action_deploy_cluster',
+        '_action_network_check',
+        '_action_stop_deploy',
+        '_action_network_check',
+        '_action_health_check',
+        '_action_delete_cluster'
+    ]
+
+
+@factory
+def cases():
+    return actions_base.case_factory(CreateDeployStop)
