b51ee75876ccd0cdc9520fe4e24d494f502448c5
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-07-01 18:28:45 +0300

    [WIP] Acceptance test cases in yaml
    Rework fuleweb_test framworks for automation generating TestCase
    
    put yaml conf into fuelweb_test/environments
    define Case in fuelweb_test/acceptance_tests
    proboscis generate group of test for each case and config
    
    For sen new test plans use Jenkins script.
        for all matrix:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests -o --show-plan -k -K
    
        for some case and all configuration:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests.create_deploy_ostf -o --show-plan -k -K
    
        for some case and some config
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests.create_deploy_ostf.1ctrl_1comp_1cinder_neutronVLAN -o --show-plan -k -K
    
    Change-Id: I81eeb0df84a0f373be029f20ec6c53e8d3596596

---
+++ fuelweb_test/actions_tests/test_create_deploy_delete.py
@@ -0,0 +1,54 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test.actions_tests import actions_base
+from fuelweb_test.helpers.utils import case_factory
+from proboscis import factory
+
+
+class CreateDeployDelete(actions_base.ActionsBase):
+    """Case delete Environment after deploy
+
+    Scenario:
+        1. Create Environment
+        2. Add nodes to Environment
+        3. Run network checker
+        4. Deploy Environment
+        5. Run network checker
+        6. Run OSTF
+        7. Delete Environment
+    """
+
+    base_group = ['actions_tests', 'actions_tests.create_deploy_delete']
+    actions_order = [
+        '_action_setup_master',
+        '_action_config_release',
+        '_action_make_slaves',
+        '_action_revert_slaves',
+        '_action_create_env',
+        '_action_add_nodes',
+        '_action_delete_cluster',
+        '_action_create_env',
+        '_action_add_nodes',
+        '_action_network_check',
+        '_action_deploy_cluster',
+        '_action_network_check',
+        '_action_health_check',
+        '_action_delete_cluster'
+    ]
+
+
+@factory
+def cases():
+    return case_factory(CreateDeployDelete)
