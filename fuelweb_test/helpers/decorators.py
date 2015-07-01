ab2495a73ea97f5f8a1d5e6f99962c9a947d2265
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-07-01 18:28:45 +0300

    [WIP] Acceptance test cases in yaml
    Rework fuleweb_test framworks for automation generating TestCase
    
    put yaml conf into fuelweb_test/environments
    define Case in fuelweb_test/acceptance_tests
    proboscis generate group of test for each case and config
    
    For sen new test plans use Jenkins script.
        for all matrix:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests -o --show-plan -k -K
    
        for some case and all configuration:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests.create_deploy_ostf -o --show-plan -k -K
    
        for some case and some config
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME -t test -w "$WORKSPACE" -i $ISO_PATH -o --group=actions_tests.create_deploy_ostf.1ctrl_1comp_1cinder_neutronVLAN -o --show-plan -k -K
    
    Change-Id: I81eeb0df84a0f373be029f20ec6c53e8d3596596

--- fuelweb_test/helpers/decorators.py
+++ fuelweb_test/helpers/decorators.py
@@ -61,6 +61,54 @@ def save_logs(url, path, auth_token=None, chunk_size=1024):
                 fp.flush()
 
 
+def make_snapshot_if_step_fail(func):
+    """Generate diagnostic snapshot if step fail.
+
+      - Show test case method name and scenario from docstring.
+      - Create a diagnostic snapshot of environment in cases:
+            - if the test case passed;
+            - if error occurred in the test case.
+      - Fetch logs from master node if creating the diagnostic
+        snapshot has failed.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except SkipTest:
+            raise SkipTest()
+        except Exception as test_exception:
+            exc_trace = sys.exc_traceback
+            name = 'error_%s' % func.__name__
+            description = "Failed in method '%s'." % func.__name__
+            if args[0].env is not None:
+                try:
+                    create_diagnostic_snapshot(args[0].env,
+                                               "fail", name)
+                except:
+                    logger.error("Fetching of diagnostic snapshot failed: {0}".
+                                 format(traceback.format_exc()))
+                    try:
+                        with args[0].env.d_env.get_admin_remote()\
+                                as admin_remote:
+                            pull_out_logs_via_ssh(admin_remote, name)
+                    except:
+                        logger.error("Fetching of raw logs failed: {0}".
+                                     format(traceback.format_exc()))
+                finally:
+                    logger.debug(args)
+                    try:
+                        args[0].env.make_snapshot(snapshot_name=name[-50:],
+                                                  description=description,
+                                                  is_make=True)
+                    except:
+                        logger.error("Error making the environment snapshot:"
+                                     " {0}".format(traceback.format_exc()))
+            raise test_exception, None, exc_trace
+        return result
+    return wrapper
+
+
 def log_snapshot_after_test(func):
     """Generate diagnostic snapshot after the end of the test.
 
@@ -512,5 +560,32 @@ def check_repos_management(func):
             for n in nailgun_nodes:
                 with env.d_env.get_ssh_to_remote(n['ip']) as node_ssh:
                     check_repo_managment(node_ssh)
+        return result
+    return wrapper
+
+
+def deferred_decorator(decorator_list):
+    def real_decorator(func):
+        setattr(func, '_deferred_decorator_', decorator_list)
+        return func
+    return real_decorator
+
+
+def step_start_stop(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        with timestat(func) as timer:
+            step_name = getattr(func, '_step_name')
+            start_step = '[ START {} ]'.format(step_name)
+            header = "<<< {:-^142} >>>".format(start_step)
+            logger.info("\n{header}\n".format(header=header))
+            result = func(*args, **kwargs)
+            spend_time = timer.spended_time
+            minutes = int(round(spend_time)) / 60
+            seconds = int(round(spend_time)) % 60
+            finish_step = "[ FINISH {} STEP TOOK {} min {} sec ]".format(
+                step_name, minutes, seconds)
+            footer = "<<< {:-^142} >>>".format(finish_step)
+            logger.info("\n{footer}\n".format(footer=footer))
         return result
     return wrapper
