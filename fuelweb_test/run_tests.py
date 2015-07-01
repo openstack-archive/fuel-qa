e4d999b4131b5a7c50151424d95b92b9368ec2fe
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-07-01 18:28:45 +0300

    Template based tests with yaml configs
    Rework fuleweb_test framworks for automation generating TestCase
    
    put yaml confs into fuelweb_test/system_tests_templates
    define Case in fuelweb_test/actions_tests
    proboscis generate group of test for each case and config
    
    For sen new test plans use Jenkins script.
        for all matrix:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests \
                -o --show-plan -k -K
    
        for some case and all configuration:
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.create_deploy_ostf \
                -o --show-plan -k -K
    
        for some case and some config
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.create_deploy_ostf.1ctrl_1comp_1cinder_neutronVLAN \
                -o --show-plan -k -K
    
        for run all destroy controllers failover cases
            sh -x "utils/jenkins/system_tests.sh" -L -j $JOB_NAME \
                -t test -w "$WORKSPACE" -i $ISO_PATH \
                -o --group=actions_tests.failover.destroy_controllers.3ctrl_2comp_1cndr_neutronVLAN \
                -o --show-plan -k -K
    
    Change-Id: I81eeb0df84a0f373be029f20ec6c53e8d3596596

--- fuelweb_test/run_tests.py
+++ fuelweb_test/run_tests.py
@@ -98,6 +98,12 @@ def import_tests():
     from tests import test_node_reinstallation  # noqa
     from tests import test_ubuntu_bootstrap  # noqa
     from tests import test_net_templates  # noqa
+    from actions_tests import test_create_deploy_delete  # noqa
+    from actions_tests import test_create_deploy_reset  # noqa
+    from actions_tests import test_create_deploy_stop  # noqa
+    from actions_tests import test_create_deploy_ostf  # noqa
+    from actions_tests import test_deploy_check_rados  # noqa
+    from actions_tests.strength import destroy_controllers  # noqa
 
 
 def run_tests():
