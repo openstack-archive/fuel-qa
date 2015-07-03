3ad071ac6c85380e869b559529827081761b1352
Maksym Strukov <mstrukov@mirantis.com>
2015-07-03 21:05:41 +0300

    Update master node and bootstrap when UPDATE_MASTER=True Motivation: To be sure that master node has proposed updates during patching work-flow
    In order to update master node with proposed changes
    code to upload proposed repo and regenerate containers
    was added
    Closes-Bug: #1469779
    Change-Id: I533718a80b840d56f3ecf8ca317df1241d1f4f7c

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -433,6 +433,8 @@ UPDATE_FUEL = os.environ.get('UPDATE_FUEL', 'false') == 'true'
 UPDATE_FUEL_PATH = os.environ.get('UPDATE_FUEL_PATH', '~/fuel/pkgs/')
 UPDATE_FUEL_MIRROR = os.environ.get("UPDATE_FUEL_MIRROR", '').split()
 
+UPDATE_MASTER = get_var_as_bool('UPDATE_MASTER', False)
+
 EMC_PLUGIN_PATH = os.environ.get('EMC_PLUGIN_PATH')
 EMC_SP_A_IP = os.environ.get('EMC_SP_A_IP')
 EMC_SP_B_IP = os.environ.get('EMC_SP_B_IP')
