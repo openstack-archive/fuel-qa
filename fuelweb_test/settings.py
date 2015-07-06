5a250313468e70349f078959ebe182d725dd4f68
asledzinskiy <asledzinskiy@mirantis.com>
2015-07-06 17:29:05 +0300

    Add downloading artifacts and env images clean
    - Add method to clean all images that were generated for
    cluster provisioning in order to deploy next cluster
    not on pre-generated images
    - Add downloading of re-generated images and installing
    them on master node
    
    Change-Id: Iafeb9df355403f55d4ca84a2ebd7c0dfaf2fd572
    Implements: blueprint mos-patching-tests

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -448,3 +448,6 @@ ALWAYS_CREATE_DIAGNOSTIC_SNAPSHOT = os.environ.get(
 RALLY_DOCKER_REPO = os.environ.get('RALLY_DOCKER_REPO', 'rallyforge/rally')
 RALLY_CONTAINER_NAME = os.environ.get('RALLY_CONTAINER_NAME', 'rally')
 RALLY_TAGS = os.environ.get('RALLY_TAGS', 'nova').split(',')
+
+REGENERATE_ENV_IMAGE = os.environ.get('REGENERATE_ENV_IMAGE', 'false') == 'true'
+LATE_ARTIFACTS_JOB_URL = os.environ.get("LATE_ARTIFACTS_JOB_URL", '')
