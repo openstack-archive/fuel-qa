0565ceae7a249a2fc134ce7fe5d051ac43bcc7ae
Dmitry Kalashnik <dkalashnik@mirantis.com>
2015-07-09 17:06:07 +0300

    Add tests for jumbo frames between instances
    !!! DO NOT REVIEW. WORK IN PROGRESS !!!
    
    Change-Id: I403293f77a90d87f11a33ce07d04c96d16b7d90f

--- fuelweb_test/run_tests.py
+++ fuelweb_test/run_tests.py
@@ -77,6 +77,7 @@ def import_tests():
     from tests.gd_based_tests import test_neutron_vlan_ceph_mongo  # noqa
     from tests.tests_patching import test_patching  # noqa
     from tests import test_cli  # noqa
+    from tests import test_jumbo_frames  # noqa
 
 
 def run_tests():
