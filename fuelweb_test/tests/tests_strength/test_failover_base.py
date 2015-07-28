a20070088ed052786a88e76ff6e55199f976b0b8
Maksym Strukov <mstrukov@mirantis.com>
2015-07-28 12:01:03 +0300

    Bring corosync cluster to maintenance mode
    To fix quorum loss tests should bring corosync cluster to maintenance mode
    in following tests:
    * ha_neutron_check_alive_rabbit
    * ha_neutron_check_dead_rabbit
    * ha_nova_check_alive_rabbit
    * ha_nova_check_dead_rabbit
    
    Change-Id: Ia185a12457d08e7ce7bba9d800a555f31e6e60c8
    Closes-Bug: #1462942

--- fuelweb_test/tests/tests_strength/test_failover_base.py
+++ fuelweb_test/tests/tests_strength/test_failover_base.py
@@ -797,8 +797,7 @@ class TestHaFailoverBase(TestBasic):
 
         pcm_nodes.remove(slave1_name)
 
-        slave1_remote.execute('pcs resource unmanage'
-                              ' master_p_rabbitmq-server')
+        slave1_remote.execute('crm configure property maintenance-mode=true')
         slave1_remote.execute('service corosync stop')
 
         remote = self.env.d_env.get_admin_remote()
@@ -865,7 +864,7 @@ class TestHaFailoverBase(TestBasic):
 
         pcm_nodes.remove(slave1_name)
 
-        slave1_remote.execute('pcs resource unmanage master_p_rabbitmq-server')
+        slave1_remote.execute('crm configure property maintenance-mode=true')
         slave1_remote.execute('rabbitmqctl stop_app')
         slave1_remote.execute('service corosync stop')
 
