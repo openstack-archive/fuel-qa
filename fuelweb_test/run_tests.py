5dfe73a86da1c9d86cd1cff0af93557fcdfc8112
root <root@cz7129.bud.mirantis.net>
2015-07-29 23:35:02 +0000

    Automated tests for node reinstallation feature
    Add tests for node reinstallation feature
    
    Change-Id: Ie901009356e0d0accaf1983d2539106bf94467a2
    Implements: blueprint node-reinstallation-tests

--- fuelweb_test/run_tests.py
+++ fuelweb_test/run_tests.py
@@ -84,6 +84,7 @@ def import_tests():
     from tests import test_cli  # noqa
     from tests import test_custom_hostname  # noqa
     from tests import test_jumbo_frames  # noqa
+    from tests import test_node_reinstallation  # noqa
 
 
 def run_tests():
