abb76c1fcf712bda34d557396820bf293d29c4dc
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-07-31 00:18:25 +0300

    Suspend slave nodes in the first place before making a snapshot
    To avoid continuos IO on admin node caused by remote logging from
    slaves, let's suspend slave nodes in the first place.
    
    Change-Id: I72e132347554269d9123469f533fdb72b7023804
    Closes-Bug:#1457802

--- fuelweb_test/models/environment.py
+++ fuelweb_test/models/environment.py
@@ -232,8 +232,9 @@ class EnvironmentModel(object):
 
     def make_snapshot(self, snapshot_name, description="", is_make=False):
         if settings.MAKE_SNAPSHOT or is_make:
-            self.d_env.suspend(verbose=False)
-            time.sleep(10)
+            with self.d_env.get_admin_remote() as remote:
+                remote.execute("dockerctl stop")
+            time.sleep(60)
 
             self.d_env.snapshot(snapshot_name, force=True)
             revert_info(snapshot_name, self.get_admin_node_ip(), description)
@@ -274,6 +275,9 @@ class EnvironmentModel(object):
 
         logger.info("Resuming the snapshot '{0}' ....".format(name))
         self.resume_environment()
+        with self.d_env.get_admin_remote() as remote:
+            remote.execute("dockerctl start")
+        time.sleep(10)
 
         if not skip_timesync:
             nailgun_nodes = [self.fuel_web.get_nailgun_node_by_name(node.name)
