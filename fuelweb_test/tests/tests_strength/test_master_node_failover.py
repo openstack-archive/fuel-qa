885b5580577b496346e967aed5e71c2625b84d72
Maksym Strukov <mstrukov@mirantis.com>
2015-07-31 00:08:19 +0300

    Use neutron in deploy_ha_one_controller_flat_master_node_fail
    * use neutron
    * segment type configured by env var NEUTRON_SEGMENT_TYPE
    
    Change-Id: I79aa224032af54dd9cd7de8c4d03ea429f0e1900
    Closes-Bug: #1478814

--- fuelweb_test/tests/tests_strength/test_master_node_failover.py
+++ fuelweb_test/tests/tests_strength/test_master_node_failover.py
@@ -55,10 +55,10 @@ class DeployHAOneControllerMasterNodeFail(base_test_case.TestBasic):
 
     @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves_3],
           groups=["non_functional",
-                  "deploy_ha_one_controller_flat_master_node_fail"])
+                  "deploy_ha_one_controller_neutron_master_node_fail"])
     @log_snapshot_after_test
-    def deploy_ha_one_controller_flat_master_node_fail(self):
-        """Deploy HA cluster with nova-network and check it without master node
+    def deploy_ha_one_controller_neutron_master_node_fail(self):
+        """Deploy HA cluster with neutron and check it without master node
 
         Scenario:
             1. Create cluster in ha mode with 1 controller
@@ -79,7 +79,11 @@ class DeployHAOneControllerMasterNodeFail(base_test_case.TestBasic):
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
-            mode=settings.DEPLOYMENT_MODE
+            mode=settings.DEPLOYMENT_MODE,
+            settings={
+                'net_provider': 'neutron',
+	            'net_segment_type': settings.NEUTRON_SEGMENT_TYPE
+            }
         )
         self.fuel_web.update_nodes(
             cluster_id,
@@ -131,7 +135,7 @@ class DeployHAOneControllerMasterNodeFail(base_test_case.TestBasic):
         finally:
             self.resume_admin_node()
 
-        self.env.make_snapshot("deploy_flat_master_node_destroy")
+        self.env.make_snapshot("deploy_ha_one_controller_neutron_master_node_fail")
 
     @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves_5],
           groups=["deploy_ha_dns_ntp"])
