9b1a6a25bb957ccf1bc0f74ebe8d63e8516efe16
Maxim Strukov <unbelll@gmail.com>
2015-07-30 11:34:09 +0300

    Use neutron network manager in test_restart system tests
    * Neutron used
    * Segment type vlan by default
      and managed by env var NEUTRON_SEGMENT_TYPE
    
    Change-Id: I99945b370b0efe5655d0b547e15ddd9d023bd4f6
    Closes-Bug: #1478817

--- fuelweb_test/tests/tests_strength/test_restart.py
+++ fuelweb_test/tests/tests_strength/test_restart.py
@@ -17,6 +17,7 @@ from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test import logger
 from fuelweb_test import ostf_test_mapping as map_ostf
 from fuelweb_test.settings import DEPLOYMENT_MODE
+from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 
@@ -218,7 +219,11 @@ class HAOneControllerFlatRestart(TestBasic):
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
-            mode=DEPLOYMENT_MODE
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT_TYPE
+            }
         )
         self.fuel_web.update_nodes(
             cluster_id,
