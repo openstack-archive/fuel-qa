185a363c71a63fd42908b4f1e96257dcb96e7434
Maksym Strukov <mstrukov@mirantis.com>
2015-07-31 00:34:38 +0300

    Use context manager for ssh sessions
    Change-Id: I4dfd0918b1a9664e5c7a775688a059043b591969
    Related-Bug: #1346362

--- fuelweb_test/tests/test_ha_one_controller.py
+++ fuelweb_test/tests/test_ha_one_controller.py
@@ -19,7 +19,6 @@ from proboscis.asserts import assert_equal
 from proboscis.asserts import assert_true
 from proboscis import test
 
-from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test.helpers.eb_tables import Ebtables
 from fuelweb_test.helpers import os_actions
@@ -296,9 +295,8 @@ class HAOneControllerNeutron(HAOneControllerNeutronBase):
         self.fuel_web.run_ostf(cluster_id=cluster_id)
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-03")['ip']
-        remote = self.env.d_env.get_ssh_to_remote(_ip)
-
-        result = remote.execute('readlink /etc/astute.yaml')['stdout']
+        with self.env.d_env.get_ssh_to_remote(_ip) as remote:
+            result = remote.execute('readlink /etc/astute.yaml')['stdout']
 
         assert_true("base-os" in result[0],
                     "Role mismatch. Node slave-03 is not base-os")
