c923148234e4ff9854e6476fd88f4f10c13fd0e4
asledzinskiy <asledzinskiy@mirantis.com>
2015-07-31 18:04:46 +0300

    Refactor nova tests to use neutron
    - Changed test_ha_one_controller_base to
    use neutron
    - Changed test_ha_one_controller to use neutron
    - deploy_ha_one_controller_vlan test was deleted as
    duplicate of deploy_ha_one_controller_neutron
    
    Change-Id: Iff4fe9ce5876f8dd769873189c5c294c7a6c29fd
    Closes-Bug: #1478915

--- fuelweb_test/tests/test_ha_one_controller_base.py
+++ fuelweb_test/tests/test_ha_one_controller_base.py
@@ -12,19 +12,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from fuelweb_test.helpers import os_actions
 from fuelweb_test.settings import DEPLOYMENT_MODE
 from fuelweb_test.tests.base_test_case import TestBasic
 
 
-class HAOneControllerFlatBase(TestBasic):
-    def deploy_ha_one_controller_flat_base(self):
+class HAOneControllerNeutronBase(TestBasic):
+    def deploy_ha_one_controller_neutron_base(self):
         self.env.revert_snapshot("ready_with_3_slaves")
 
         data = {
-            'tenant': 'novaSimpleFlat',
-            'user': 'novaSimpleFlat',
-            'password': 'novaSimpleFlat'
+            'tenant': 'neutronOneController',
+            'user': 'neutronOneController',
+            'password': 'neutronOneController'
 
         }
 
@@ -40,20 +39,12 @@ class HAOneControllerFlatBase(TestBasic):
                 'slave-02': ['compute']
             }
         )
-        self.fuel_web.update_internal_network(cluster_id, '10.1.0.0/24')
         self.fuel_web.deploy_cluster_wait(cluster_id)
-        os_conn = os_actions.OpenStackActions(
-            self.fuel_web.get_public_vip(cluster_id),
-            data['user'], data['password'], data['tenant'])
-        self.fuel_web.assert_cluster_ready(
-            os_conn, smiles_count=6, networks_count=1, timeout=300)
-
-        self.fuel_web.check_fixed_network_cidr(
-            cluster_id, os_conn)
 
         self.fuel_web.verify_network(cluster_id)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id)
 
-        self.env.make_snapshot("deploy_ha_one_controller_flat", is_make=True)
+        self.env.make_snapshot("deploy_ha_one_controller_neutron",
+                               is_make=True)
