853af4a1d26ba49a6ea3a032b48a32ea46f3b9bd
Maksym Strukov <mstrukov@mirantis.com>
2015-08-03 18:18:10 +0300

    Remove compatibility workaround in reboot plugin test
    Previously some code was added to old plugins for compatibility
    with Fuel 7.0. But now FuelPluginBuilder is up to date, so
    workaround have to be removed because it fail test.
    
    Change-Id: I4fc93574a5a2bd4d8fd39965b8590ee7f61ba978
    Closes-Bug: #1480201

--- fuelweb_test/tests/plugins/plugin_reboot/test_plugin_reboot_task.py
+++ fuelweb_test/tests/plugins/plugin_reboot/test_plugin_reboot_task.py
@@ -68,14 +68,6 @@ class RebootPlugin(TestBasic):
         fpb.fpb_replace_plugin_content(
             os.path.join(tasks_path, tasks_file),
             os.path.join('/root/', plugin_name, 'tasks.yaml'))
-        # change default supported version to 7.0
-        fpb.change_yaml_file_in_container(
-            '/root/{}/metadata.yaml'.format(plugin_name),
-            ['fuel_version'], ['7.0'])
-        for elem in range(2):
-            fpb.change_yaml_file_in_container(
-                '/root/{}/metadata.yaml'.format(plugin_name),
-                ['releases', elem, 'version'], '-')
         # build plugin
         fpb.fpb_build_plugin(os.path.join('/root/', plugin_name))
         # copy plugin archive file from nailgun container
