b056fa027064c00588186f3cf56eb5c0bdf7868e
Maksym Strukov <mstrukov@mirantis.com>
2015-08-05 01:37:58 +0300

    Hotfix for upgrade_chains tests
    L106 and L193 contains call to not existed method check_tarball_exists.
    It was renamed to check_file_exists. This will fail tests:
    * 'upgrade_first_stage'
    * 'upgrade_second_stage'
    
    Change-Id: I8ba7285e9b4f6f5a41afc7f00880454a95e02737
    Closes-Bug: #1481503

--- fuelweb_test/tests/tests_upgrade/test_upgrade_chains.py
+++ fuelweb_test/tests/tests_upgrade/test_upgrade_chains.py
@@ -103,10 +103,9 @@ class UpgradeFuelChains(base_test_data.TestBasic):
             release_name=hlp_data.OPENSTACK_RELEASE)
         checkers.upload_tarball(self.env.d_env.get_admin_remote(),
                                 hlp_data.TARBALL_PATH, '/var')
-        checkers.check_tarball_exists(self.env.d_env.get_admin_remote(),
-                                      os.path.basename(hlp_data.
-                                                       TARBALL_PATH),
-                                      '/var')
+        checkers.check_file_exists(self.env.d_env.get_admin_remote(),
+                                   os.path.basename(hlp_data.TARBALL_PATH),
+                                   '/var')
         checkers.untar(self.env.d_env.get_admin_remote(),
                        os.path.basename(hlp_data.
                                         TARBALL_PATH), '/var')
@@ -190,10 +189,9 @@ class UpgradeFuelChains(base_test_data.TestBasic):
 
         checkers.upload_tarball(remote,
                                 hlp_data.TARBALL_PATH, '/var')
-        checkers.check_tarball_exists(remote,
-                                      os.path.basename(hlp_data.
-                                                       TARBALL_PATH),
-                                      '/var')
+        checkers.check_file_exists(remote,
+                                   os.path.basename(hlp_data.TARBALL_PATH),
+                                   '/var')
         checkers.untar(remote,
                        os.path.basename(hlp_data.
                                         TARBALL_PATH), '/var')
