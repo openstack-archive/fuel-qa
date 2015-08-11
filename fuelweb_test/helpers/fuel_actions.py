ce0e9b1bd83ba4221ede66b87173ffb2e7269cd6
Maksym Strukov <mstrukov@mirantis.com>
2015-08-11 11:21:04 +0300

    [WIP] Doc fix
    Change-Id: Id30bfc71a5deecb0553c887d45c64b23ec78f5f4
    Related-Bug: #1482643

--- fuelweb_test/helpers/fuel_actions.py
+++ fuelweb_test/helpers/fuel_actions.py
@@ -100,15 +100,14 @@ class BaseActions(object):
         wait(lambda: self.is_container_ready, timeout=timeout)
 
     def change_content_in_yaml(self, old_file, new_file, element, value):
-        """
-        Changes content in old_file at element is given to the new value
+        """Changes content in old_file at element is given to the new value
         and creates new file with changed content
         :param old_file: a path to the file content from to be changed
         :param new_file: a path to the new file to ve created with new content
         :param element: tuple with path to element to be changed
-            for example: ['root_elem', 'first_elem', 'target_elem']
-            if there are a few elements with equal names use integer
-            to identify which element should be used
+        for example: ['root_elem', 'first_elem', 'target_elem']
+        if there are a few elements with equal names use integer
+        to identify which element should be used
         :return: nothing
         """
 
