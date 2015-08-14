c42ae61c8f4728f0f07d66df46090e53cedcaedf
otsvigun <otsvigun@mirantis.com>
2015-08-14 14:27:43 +0300

    Add system tests
    -add new functions create_networks, create_vms, check_connection_vms
    -add test case dvs_vcenter_add_delete_controller
    Change-Id: Iebe25571dfe520a177cd19c3f0d97a865af86137

--- fuelweb_test/tests/plugins/plugin_dvs/test_plugin_dvs.py
+++ fuelweb_test/tests/plugins/plugin_dvs/test_plugin_dvs.py
@@ -16,6 +16,9 @@ import os
 from proboscis import test
 from proboscis.asserts import assert_true
 from fuelweb_test.helpers import checkers
+from devops.helpers.helpers import wait
+from devops.error import TimeoutError
+
 
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test import logger
@@ -27,8 +30,12 @@ from fuelweb_test.settings import VCENTER_DATACENTER
 from fuelweb_test.settings import VCENTER_DATASTORE
 from fuelweb_test.settings import DVS_PLUGIN_PATH
 from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
+from fuelweb_test.settings import SERVTEST_USERNAME
+from fuelweb_test.settings import SERVTEST_PASSWORD
+from fuelweb_test.settings import SERVTEST_TENANT
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.helpers import os_actions
 
 
 @test(groups=["plugins", 'dvs_vcenter_plugin'])
@@ -59,6 +66,112 @@ class TestDVSPlugin(TestBasic):
         self.fuel_web.update_plugin_data(cluster_id, self.plugin_name, options)
 
         logger.info("cluster is {}".format(cluster_id))
+
+    def create_networks(self, os_conn=None, net_count=None):
+        networks = []
+        for i in range(0, net_count):
+            os_conn.neutron.create_network(
+                body={'network': {'name': "net1%d" % i,
+                                  'admin_state_up': True,
+                                  }
+                      })
+
+            network_id = ''
+            network_dic = os_conn.neutron.list_networks()
+            for dd in network_dic['networks']:
+                if dd.get("name") == "net1%d" % i:
+                    network_id = dd.get("id")
+
+            if network_id == "":
+                logger.error('Network id empty')
+
+            logger.debug("id {0} to master node".format(network_id))
+
+            os_conn.neutron.create_subnet(
+                body={'subnet': {'network_id': network_id,
+                                 'ip_version': 4,
+                                 'cidr': '192.168.112.0/24',
+                                 'name': 'subnet1%d' % i
+                                 }
+                      })
+
+            network = [net.id for net in os_conn.nova.networks.list()
+                       if net.label == "net1%d" % i]
+            networks.append(network)
+        return networks
+
+    def create_vms(self, os_conn=None, vm_count=None, network=None):
+        """Create Vms on available hypervisors
+        :param os_conn: openstack object
+        :param vm_count: interger count of VMs
+        :param networks: list of neutron networks parameters
+        """
+        # Get list of available images,flavors and hipervisors
+        images_list = os_conn.nova.images.list()
+        flavors_list = os_conn.nova.flavors.list()
+        # Create VMs on each of hypervisor
+        for image in images_list:
+            for i in range(0, vm_count):
+                if image.name == 'TestVM-VMDK':
+                    os_conn.nova.servers.create(
+                        flavor=flavors_list[0],
+                        name='test_{0}_{1}'.format(image.name, i),
+                        image=image, availability_zone='vcenter',
+                        nics=[{'net-id': network[0], 'v4-fixed-ip': ''}]
+                    )
+                else:
+                    os_conn.nova.servers.create(
+                        flavor=flavors_list[0],
+                        name='test_{0}_{1}'.format(image.name, i),
+                        image=image,
+                        nics=[{'net-id': network[0], 'v4-fixed-ip': ''}]
+                    )
+
+        # Verify that current state of each VMs is Active
+        srv_list = os_conn.get_servers()
+        for srv in srv_list:
+            assert_true(os_conn.get_instance_detail(srv).status != 'ERROR',
+                        "Current state of Vm {0} is {1}".format(
+                            srv.name, os_conn.get_instance_detail(srv).status))
+            try:
+                wait(
+                    lambda:
+                    os_conn.get_instance_detail(srv).status == "ACTIVE",
+                    timeout=60 * 15)
+            except TimeoutError:
+                logger.error(
+                    "Current state of Vm {0} is {1}".format(
+                        srv.name, os_conn.get_instance_detail(srv).status))
+
+    def check_connection_vms(self, os_conn=None, conn_type='ping'):
+        """Check network connectivity between VMs
+        :param os_conn: openstack object
+        :param conn_type: string type of network connectivity: ping, tcp
+        """
+        # Get ip of VMs
+        if conn_type == 'ping':
+            srv_ip = []
+            srv_list = os_conn.get_servers()
+            for srv in srv_list:
+                ip = srv.networks[srv.networks.keys()[0]][0]
+                srv_ip.append(ip)
+
+            # VMs on different hypervisors should communicate between
+            # each other
+            for ip_1 in srv_ip:
+                ssh = self.fuel_web.get_ssh_for_node("slave-01")
+                logger.info("Connect to VM {0}".format(ip_1))
+                for ip_2 in srv_ip:
+                    if ip_1 != ip_2:
+                        # Check server's connectivity
+                        res = int(os_conn.execute_through_host(
+                            ssh, ip_1, "ping -q -c3 " + ip_2 +
+                            "| grep -o '[0-9] packets received'" +
+                            "| cut -f1 -d ' '"))
+                        assert_true(
+                            res == 3,
+                            "VM{0} not ping from Vm {1}, " +
+                            "received {2} icmp".format(ip_1, ip_2, res))
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
           groups=["dvs_vcenter_smoke", "dvs_vcenter_plugin"])
@@ -130,6 +243,22 @@ class TestDVSPlugin(TestBasic):
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id, test_sets=['smoke', 'sanity'])
+
+        # Create new network
+        os_ip = self.fuel_web.get_public_vip(cluster_id)
+        os_conn = os_actions.OpenStackActions(
+            os_ip, SERVTEST_USERNAME,
+            SERVTEST_PASSWORD,
+            SERVTEST_TENANT)
+
+        networks = self.create_networks(os_conn=os_conn, net_count=1)
+
+        # Create  2 VMs of vcenter and 2 VMs of nova
+        # and attached them to same network
+        self.create_vms(os_conn=os_conn, vm_count=2, network=networks[0])
+
+        # Verify connection between VMs. Send ping Check that ping get responce
+        self.check_connection_vms(os_conn=os_conn)
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_5],
           groups=["dvs_vcenter_ha_mode", "dvs_vcenter_plugin"])
@@ -458,6 +587,110 @@ class TestDVSPlugin(TestBasic):
             cluster_id,
             {
                 'slave-04': ['cinder-vmware'],
+            }
+        )
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id, test_sets=['smoke', 'sanity'])
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_9],
+          groups=["dvs_vcenter_add_delete_controller", "dvs_vcenter_plugin"])
+    @log_snapshot_after_test
+    def dvs_vcenter_add_delete_controller(self):
+        """Deploy cluster with plugin, add  and deletion controler node.
+
+        Scenario:
+            1. Upload plugins to the master node.
+            2. Install plugin.
+            3. Create cluster with vcenter.
+            4. Add 4 node with controller role.
+            5. Add 1 node with cinder-vmdk role.
+            6. Add 1 node with compute role.
+            7. Deploy cluster.
+            8. Run OSTF.
+            9. Remove node with controller role.
+            10. Redeploy cluster.
+            11. Run OSTF.
+            12. Add node with controller role.
+            13. Redeploy cluster.
+            14. Run OSTF.
+
+        Duration 3.5 hours
+
+        """
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        self.install_dvs_plugin()
+
+        # Configure cluster
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "images_vcenter": True,
+                'images_ceph': False,
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT_TYPE,
+            },
+            vcenter_value={
+                "glance": {
+                    "vcenter_username": VCENTER_USERNAME,
+                    "datacenter": VCENTER_DATACENTER,
+                    "vcenter_host": VCENTER_IP,
+                    "vcenter_password": VCENTER_PASSWORD,
+                    "datastore": VCENTER_DATASTORE},
+                "availability_zones": [
+                    {"vcenter_username": VCENTER_USERNAME,
+                     "nova_computes": [
+                         {"datastore_regex": ".*",
+                          "vsphere_cluster": "Cluster1",
+                          "service_name": "vmcluster1"
+                          },
+                         {"datastore_regex": ".*",
+                          "vsphere_cluster": "Cluster2",
+                          "service_name": "vmcluster2"
+                          },
+                     ],
+                     "vcenter_host": VCENTER_IP,
+                     "az_name": "vcenter",
+                     "vcenter_password": VCENTER_PASSWORD,
+                     }]
+            }
+        )
+
+        self.enable_plugin(cluster_id=cluster_id)
+
+        # Assign role to node
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller'],
+             'slave-02': ['controller'],
+             'slave-03': ['controller'],
+             'slave-04': ['controller'],
+             'slave-05': ['cinder-vmware'],
+             'slave-06': ['compute'], })
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id, test_sets=['smoke', 'sanity'])
+
+        # Remove node with controller role
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-04': ['controller'], }, False, True)
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id, test_sets=['smoke', 'sanity'])
+
+        # Remove node with controller role
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-04': ['controller'],
             }
         )
 
