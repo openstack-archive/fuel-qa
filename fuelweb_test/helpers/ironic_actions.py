11ba0fca5d7ad9dc3a2e8a53234f519f46456c7a
Yuriy Yekovenko <yyekovenko@mirantis.com>
2015-08-17 17:51:24 +0300

    [DO NOT MERGE] Test for ironic plugin.
    Change-Id: I9f1fdbf39c37ce35a6c734d492b144f8346719be

---
+++ fuelweb_test/helpers/ironic_actions.py
@@ -0,0 +1,156 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from proboscis import asserts
+import os
+import time
+import random
+
+from fuelweb_test.helpers import os_actions
+from devops.error import TimeoutError
+from devops.helpers import helpers
+from fuelweb_test import logger
+from ironicclient import client as ironicclient
+
+
+class IronicActions(object):
+    """IronicActions."""  # TODO documentation
+
+    def __init__(self, controller_ip, user='admin',
+                 passwd='admin', tenant='admin'):
+        auth_url = 'http://{0}:5000/v2.0/'.format(controller_ip)
+        logger.debug('Auth URL is {0}'.format(auth_url))
+        self.ironic = ironicclient.get_client(api_version=1,
+                                              os_username=user,
+                                              os_password=passwd,
+                                              os_auth_url=auth_url,
+                                              os_tenant_name=tenant)
+        self.os_conn = os_actions.OpenStackActions(controller_ip)
+
+    def initialize_ironic_resources(self):
+        # read env variables: ssh creds, bm cidr
+        pass
+        # import image, create flavor, keypair
+        # read MACs of VMs and baremetal nodes
+
+    def import_ironic_image(self, image_name='virtual_trusty', disk='vda'):
+        image_properties = {
+            'mos_disk_info': '[{{"name": "{disk}", "extra": [], '
+                             '"free_space": 11000, "type": "disk", '
+                             '"id": "{disk}", "size": 11000, '
+                             '"volumes": [{{"mount": "/", '
+                             '"type": "partition", '
+                             '"file_system": "ext4", '
+                             '"size": 10000}}]}}]'.format(disk=disk),
+            'hypervisor_type': 'baremetal',
+            'cpu_arch': 'x86_64'
+        }
+
+        logger.debug('Import Ubuntu image for Ironic')
+        with open(os.environ['UBUNTU_IMAGE_PATH']) as data:
+            img = self.os_conn.create_image(
+                name=image_name + str(random.randint(1, 0x7fffffff)),
+                properties=image_properties,
+                data=data,
+                is_public=True,
+                disk_format='raw',
+                container_format='bare')
+        return img
+
+    def _create_ironic_node(self, driver, server_ip, username, password,
+                            cpus, memory_mb, local_gb,
+                            timeout=180):
+
+        driver_info = {
+            'deploy_kernel': self.os_conn.get_image(
+                'ironic-deploy-linux').id,
+            'deploy_ramdisk': self.os_conn.get_image(
+                'ironic-deploy-initramfs').id,
+            'deploy_squashfs': self.os_conn.get_image(
+                'ironic-deploy-squashfs').id
+        }
+        if 'ipmi' in driver:
+            driver_info['ipmi_address'] = server_ip
+            driver_info['ipmi_username'] = username
+            driver_info['ipmi_password'] = password
+        elif 'ssh' in driver:
+            driver_info['ssh_address'] = server_ip
+            driver_info['ssh_username'] = username
+            driver_info['ssh_password'] = password
+            driver_info['ssh_virt_type'] = 'virsh'
+
+        properties = {
+            'cpus': cpus,
+            'memory_mb': memory_mb,
+            'local_gb': local_gb,
+            'cpu_arch': 'x86_64'
+        }
+
+        ironic_node = self.ironic.node.create(driver=driver,
+                                              driver_info=driver_info,
+                                              properties=properties)
+        # Wait for nova to update hypervisor parameters
+        time.sleep(timeout)
+        return ironic_node
+
+    def create_virtual_node(self, server_ip, ssh_username, ssh_password,
+                            cpus=1, memory_mb=3072, local_gb=50, timeout=180):
+
+        return self._create_ironic_node('fuel_ssh', server_ip, ssh_username,
+                                        ssh_password, cpus, memory_mb,
+                                        local_gb, timeout)
+
+    def create_baremetal_node(self, server_ip, ipmi_username, ipmi_password,
+                              cpus=4, memory_mb=16384, local_gb=1024,
+                              timeout=180):
+
+        return self._create_ironic_node('fuel_ipmitool', server_ip,
+                                        ipmi_username, ipmi_password, cpus,
+                                        memory_mb, local_gb, timeout)
+
+    def create_port(self, address, node_uuid):
+        return self.ironic.port.create(**{'address': address,
+                                          'node_uuid': node_uuid})
+
+    def boot_ironic_instance(self, image_id, flavor_id, net_name='baremetal',
+                             neutron=True, key_name=None, timeout=100,
+                             **kwargs):
+        name = "ironic-vm-" + str(random.randint(1, 0x7fffffff))
+        if neutron:
+            kwargs.update(
+                {
+                    'nics': [
+                        {'net-id': self.os_conn.get_network(net_name)['id']}
+                    ],
+                    'security_groups': [
+                        self.os_conn.create_sec_group_for_ssh().name
+                    ]
+                }
+            )
+        srv = self.os_conn.nova.servers.create(name=name, image=image_id,
+                                               flavor=flavor_id,
+                                               key_name=key_name, **kwargs)
+        try:
+            helpers.wait(
+                lambda: self.os_conn.get_instance_detail(
+                    srv).status == "ACTIVE",
+                timeout=timeout)
+            return self.os_conn.get_instance_detail(srv.id)
+        except TimeoutError:
+            logger.debug("Create server for migration failed by timeout")
+            asserts.assert_equal(
+                self.os_conn.get_instance_detail(srv).status,
+                "ACTIVE",
+                "Instance hasn't reached active state, current state"
+                " is {0}".format(self.os_conn.get_instance_detail(srv).status))
---
+++ fuelweb_test/tests/plugins/plugin_ironic/test_fuel_plugin_ironic.py
@@ -0,0 +1,273 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Before test run, ensure the following environment variables are defined:
+  - NODES_COUNT
+  - IRONIC_PLUGIN_PATH
+  - ENV_NAME
+  - VENV_PATH
+  - ISO_PATH
+
+See ironic_env_vars.sh for example.
+
+If the environment with name=ENV_NAME does not exist it will be created before
+test execution start.
+
+Navigate to fuel-qa directory and use this command to launch tests:
+  ./utils/jenkins/system_tests.sh -t test -k -K -w $(pwd) -j $ENV_NAME -i
+    $ISO_PATH -o --group="ironic_plugin"
+"""
+
+import netaddr
+import os
+import random
+
+from proboscis.asserts import assert_equal, assert_true
+from proboscis import test
+
+from devops.helpers.helpers import wait
+from devops.error import TimeoutError
+from fuelweb_test import logger
+from fuelweb_test import settings as conf
+from fuelweb_test.helpers import checkers
+from fuelweb_test.helpers import os_actions
+from fuelweb_test.helpers import ironic_actions
+from fuelweb_test.settings import DEPLOYMENT_MODE
+from fuelweb_test.settings import IRONIC_PLUGIN_PATH
+from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+
+from fuelweb_test.settings import NEUTRON
+from fuelweb_test import logwrap
+
+@test(groups=["fuel_plugins"])
+class IronicPlugin(TestBasic):
+    """Tests for Ironic plugin."""
+
+    BAREMETAL = netaddr.IPNetwork(os.environ['BAREMETAL_NET'])
+
+    @logwrap
+    def update_nodes_interfaces(self, cluster_id, nailgun_nodes=[]):
+        net_provider = self.fuel_web.client.get_cluster(cluster_id)[
+            'net_provider']
+        if NEUTRON == net_provider:
+            assigned_networks = {
+                'eth1': ['public'],
+                'eth2': ['management'],
+                'eth3': ['private'],
+                'eth4': ['storage'],
+                'eth5': ['baremetal']
+            }
+        else:
+            assigned_networks = {
+                'eth1': ['public'],
+                'eth2': ['management'],
+                'eth3': ['fixed'],
+                'eth4': ['storage'],
+            }
+
+        if not nailgun_nodes:
+            nailgun_nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
+        for node in nailgun_nodes:
+            self.fuel_web.update_node_networks(node['id'], assigned_networks)
+
+    def install_ironic_plugin(self, path_to_ironic_rpm=IRONIC_PLUGIN_PATH):
+        # TODO build ironic plugin
+
+        # copy plugin rpm to the master node
+        checkers.upload_tarball(
+            self.env.d_env.get_admin_remote(),
+            path_to_ironic_rpm, '/var')
+
+        logger.info(os.path.basename(path_to_ironic_rpm))
+        checkers.install_plugin_check_code(
+            self.env.d_env.get_admin_remote(),
+            plugin=os.path.basename(path_to_ironic_rpm))
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["ironic_plugin"])
+    def deploy_cluster_with_ironic(self):
+        self.env.revert_snapshot("ready_with_5_slaves")
+
+        self.install_ironic_plugin()
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": conf.NEUTRON_SEGMENT_TYPE,
+            }
+        )
+        # TODO check if it provides cidr
+        self.BAREMETAL = self.env.d_env.get_network(name='baremetal')[0].ip
+        baremetal_network = {
+            'name': 'baremetal',
+            'group_id': cluster_id,
+            'cidr': str(self.BAREMETAL),
+            'gateway': None,
+            "meta": {
+                "notation": "ip_ranges",
+                "render_type": None,
+                "map_priority": 0,
+                "configurable": True,
+                "unmovable": False,
+                "use_gateway": False,
+                "render_addr_mask": None,
+                "ip_range": [str(self.BAREMETAL[2]),
+                             str(self.BAREMETAL[50])]
+            }
+        }
+        self.fuel_web.client.add_network_group(baremetal_network)
+
+        plugin_name = 'fuel-plugin-ironic'
+        msg = "Plugin couldn't be enabled. Check plugin version. Test aborted"
+        assert_true(
+            self.fuel_web.check_plugin_exists(cluster_id, plugin_name),
+            msg)
+        options = {
+            "metadata/enabled": True,
+            "l3_gateway/value": str(self.BAREMETAL[51]),
+            "l3_allocation_pool/value": "{0}:{1}".format(
+                str(self.BAREMETAL[52]),
+                str(self.BAREMETAL[-2])),
+            "password/value": "I_love_plugins"}
+        if options['metadata/enabled']:
+            self.fuel_web.update_plugin_data(cluster_id, plugin_name, options)
+
+        self.fuel_web.update_nodes_interfaces = self.update_nodes_interfaces
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+                'slave-03': ['ironic']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        # self.fuel_web.verify_network(cluster_id)
+        # self.fuel_web.run_ostf(cluster_id)
+
+        # TODO Add some ping to ironic
+        controller_ip = self.fuel_web.get_public_vip(cluster_id)
+        ironic = ironic_actions.IronicActions(controller_ip)
+        # TODO Not sure it's good enough check
+        assert_true(len(ironic.driver.list()) > 0,
+                    "Ironic is not able to provide list of drivers.")
+
+        self.env.make_snapshot("deploy_cluster_with_ironic")
+
+    @test(depends_on=[deploy_cluster_with_ironic],
+          groups=["ironic_plugin"])
+    def boot_ironic_node_with_fuel_ssh_agent(self):
+        """
+        Scenario:
+            . Create baremetal flavor
+            . Import Ubuntu image
+            . Create ironic node
+            . Create ironic port for this node
+            . Verify that ironic instance can be booted successfully
+
+        Duration 35m
+        Snapshot boot_ironic_node_with_fuel_ssh_agent
+        """
+        self.env.revert_snapshot("deploy_cluster_with_ironic")
+
+        # Data init
+        cpus = 1
+        memory_mb = 3072
+        local_gb = 50
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        controller_ip = self.fuel_web.get_public_vip(cluster_id)
+        os_conn = os_actions.OpenStackActions(controller_ip)
+        ironic = ironic_actions.IronicActions(controller_ip)
+
+        logger.debug('Create ironic node')
+        ironic_node = ironic.create_virtual_node(
+            server_ip=os.environ['HW_SERVER_IP'],
+            ssh_username=os.environ['HW_SSH_USER'],
+            ssh_password=os.environ['HW_SSH_PASS']
+        )
+        logger.debug('Create ironic port')
+        ironic.create_port(
+            address=os.environ['IRONIC_VM_MAC'],
+            node_uuid=ironic_node.uuid
+        )
+
+        key_name = 'ironic_key' + str(random.randint(1, 0x7fffffff))
+        os_conn.create_key(key_name)
+        img = ironic.import_ironic_image()
+        bm_flavor = os_conn.create_flavor(
+            "virtual-baremetal-" + str(random.randint(1, 0x7fffffff)),
+            memory_mb, cpus, local_gb
+        )
+        logger.debug('Boot ironic VM')
+        srv = ironic.boot_ironic_instance(
+            image_id=img.name,
+            flavor_id=bm_flavor.id,
+            timeout=600,
+            key_name=key_name,
+            userdata='#!/bin/bash\ntouch /home/ubuntu/sucess.txt'
+        )
+
+        self.env.make_snapshot("boot_ironic_node_with_fuel_ssh_agent")
+
+    @test(depends_on=[deploy_cluster_with_ironic],
+          groups=['ironic_plugin'])
+    def boot_ironic_node_with_fuel_ipmi_agent(self):
+        # Data init
+        cpus = 4
+        memory_mb = 16384
+        local_gb = 1024
+
+        self.env.revert_snapshot("deploy_cluster_with_ironic")
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        controller_ip = self.fuel_web.get_public_vip(cluster_id)
+        os_conn = os_actions.OpenStackActions(controller_ip)
+        ironic = ironic_actions.IronicActions(controller_ip)
+
+        logger.debug('Create ironic node')
+        ironic_node = ironic.create_baremetal_node(
+            server_ip=os.environ['IPMI_SERVER_IP'],
+            ipmi_username=os.environ['IPMI_USER'],
+            ipmi_password=os.environ['IPMI_PASS']
+        )
+        logger.debug('Create ironic port')
+        ironic.create_port(
+            address=os.environ['IRONIC_BM_MAC'],
+            node_uuid=ironic_node.uuid
+        )
+        key_name = 'ironic_key' + str(random.randint(1, 0x7fffffff))
+        os_conn.create_key(key_name)
+        img = ironic.import_ironic_image(disk='sda')
+        bm_flavor = os_conn.create_flavor(
+            "real-baremetal-" + str(random.randint(1, 0x7fffffff)),
+            memory_mb, cpus, local_gb
+        )
+
+        logger.debug('Boot ironic baremetal instance')
+        srv = ironic.boot_ironic_instance(
+            image_id=img.id,
+            flavor_id=bm_flavor.id,
+            timeout=600,
+            key_name=key_name,
+            userdata='#!/bin/bash\ntouch /home/ubuntu/sucess.txt'
+        )
+
+        # self.env.make_snapshot("boot_ironic_node_with_fuel_ipmi_agent")
