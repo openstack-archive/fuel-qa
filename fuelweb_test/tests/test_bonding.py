555090abadef8ca2ae1ba0d2cfd2538c788923cf
Alexandr Kostrikov <akostrikov@mirantis.com>
2015-09-30 20:08:39 +0300

    Network templates with bonding.
    Change-Id: Ie355ce00ded39c776ae351955e6d1ac75789db78
    Closes-Bug: #1479742

--- fuelweb_test/tests/test_bonding.py
+++ fuelweb_test/tests/test_bonding.py
@@ -20,6 +20,7 @@ from proboscis.asserts import assert_raises
 from proboscis import test
 
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.helpers.utils import get_network_template
 from fuelweb_test.settings import DEPLOYMENT_MODE
 from fuelweb_test.settings import NEUTRON_SEGMENT
 from fuelweb_test.tests.base_test_case import SetupEnvironment
@@ -386,3 +387,107 @@ class BondingHA(BondingTest):
         self.check_interfaces_config_after_reboot(cluster_id)
 
         self.env.make_snapshot("deploy_bonding_neutron_tun")
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=['deploy_bonding_with_template'])
+    @log_snapshot_after_test
+    def deploy_bonding_with_template(self):
+        """Deploy cluster with active-backup bonding and Neutron VXLAN
+        with network template.
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 nodes with controller role
+            3. Add 1 node with compute role and 1 node with cinder
+            4. Setup bonding for all interfaces (including admin interface
+               bonding)
+            5. Upload network template
+            6. Run network verification
+            7. Deploy the cluster
+            8. Run network verification
+            9. Run OSTF
+            10. Save network configuration from slave nodes
+            11. Reboot all environment nodes
+            12. Verify that network configuration is the same after reboot
+
+        Duration 70m
+        Snapshot deploy_bonding_with_template
+        """
+
+        self.env.revert_snapshot("ready_with_5_slaves")
+
+        segment_type = NEUTRON_SEGMENT['tun']
+
+        self.show_step(1)
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type,
+            }
+        )
+
+        self.show_step(2)
+        self.show_step(3)
+        self.fuel_web.update_nodes(
+            cluster_id, {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute'],
+                'slave-05': ['cinder']
+            }
+        )
+
+        net_params = self.fuel_web.client.get_networks(cluster_id)
+
+        self.show_step(4)
+        nailgun_nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
+        for node in nailgun_nodes:
+            self.fuel_web.update_node_networks(
+                node['id'], interfaces_dict=deepcopy(self.INTERFACES),
+                raw_data=deepcopy(self.BOND_CONFIG))
+
+        self.show_step(5)
+        network_template = get_network_template('ceph')
+        self.fuel_web.client.upload_network_template(
+            cluster_id=cluster_id, network_template=network_template)
+        networks = self.generate_networks_for_template(
+            template=network_template,
+            ip_nets={'default': '10.200.0.0/16'},
+            ip_prefixlen='24')
+        existing_networks = self.fuel_web.client.get_network_groups()
+        networks = self.create_custom_networks(networks, existing_networks)
+
+        self.show_step(6)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(7)
+        self.fuel_web.deploy_cluster_wait(cluster_id, check_services=False)
+
+        cluster = self.fuel_web.client.get_cluster(cluster_id)
+        assert_equal(str(cluster['net_provider']), 'neutron')
+        assert_equal(str(net_params["networking_parameters"]
+                         ['segmentation_type']), segment_type)
+
+        self.show_step(8)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(9)
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['smoke', 'sanity',
+                                          'ha', 'tests_platform'])
+
+        self.show_step(10)
+        self.show_step(11)
+        self.show_step(12)
+        self.check_interfaces_config_after_reboot(cluster_id)
+        self.check_ipconfig_for_template(cluster_id, network_template,
+                                         networks)
+        self.check_services_networks(cluster_id, network_template)
+        self.check_ipconfig_for_template(cluster_id, network_template,
+                                         networks)
+        self.check_services_networks(cluster_id, network_template)
+
+        self.env.make_snapshot('deploy_bonding_with_template')
