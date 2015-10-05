96bccaccdc5ecc4c6274f22169ea589304a4174d
Maksym Strukov <unbelll@gmail.com>
2015-10-05 17:44:41 +0300

    Close remote connections
    Refactor system tests as a context manager to get remote
    connection closed at the end of test
    
    Change-Id: I066e71914300c9fb745891c0a19a717fc84fd8f6
    Closes-Bug: #1346362

--- fuelweb_test/tests/tests_separate_services/test_separate_multiroles.py
+++ fuelweb_test/tests/tests_separate_services/test_separate_multiroles.py
@@ -53,35 +53,38 @@ class SeparateAllServices(TestBasic):
         self.check_run("separate_all_service")
         self.env.revert_snapshot("ready_with_9_slaves")
 
-        # copy plugins to the master node
+        with self.env.d_env.get_admin_remote() as remote:
 
-        checkers.upload_tarball(
-            self.env.d_env.get_admin_remote(),
-            settings.SEPARATE_SERVICE_DB_PLUGIN_PATH, "/var")
+            # copy plugins to the master node
 
-        checkers.upload_tarball(
-            self.env.d_env.get_admin_remote(),
-            settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH, "/var")
+            checkers.upload_tarball(
+                remote,
+                settings.SEPARATE_SERVICE_DB_PLUGIN_PATH, "/var")
 
-        checkers.upload_tarball(
-            self.env.d_env.get_admin_remote(),
-            settings.SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH, "/var")
+            checkers.upload_tarball(
+                remote,
+                settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH, "/var")
 
-        # install plugins
+            checkers.upload_tarball(
+                remote,
+                settings.SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH, "/var")
 
-        checkers.install_plugin_check_code(
-            self.env.d_env.get_admin_remote(),
-            plugin=os.path.basename(settings.SEPARATE_SERVICE_DB_PLUGIN_PATH))
+            # install plugins
 
-        checkers.install_plugin_check_code(
-            self.env.d_env.get_admin_remote(),
-            plugin=os.path.basename(
-                settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH))
+            checkers.install_plugin_check_code(
+                remote,
+                plugin=os.path.basename(
+                    settings.SEPARATE_SERVICE_DB_PLUGIN_PATH))
 
-        checkers.install_plugin_check_code(
-            self.env.d_env.get_admin_remote(),
-            plugin=os.path.basename(
-                settings.SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH))
+            checkers.install_plugin_check_code(
+                remote,
+                plugin=os.path.basename(
+                    settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH))
+
+            checkers.install_plugin_check_code(
+                remote,
+                plugin=os.path.basename(
+                    settings.SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH))
 
         data = {
             'tenant': 'separateall',
--- fuelweb_test/tests/tests_separate_services/test_separate_rabbitmq.py
+++ fuelweb_test/tests/tests_separate_services/test_separate_rabbitmq.py
@@ -52,18 +52,20 @@ class SeparateRabbit(TestBasic):
         self.check_run("separate_rabbit_service")
         self.env.revert_snapshot("ready_with_9_slaves")
 
-        # copy plugins to the master node
+        with self.env.d_env.get_admin_remote() as remote:
 
-        checkers.upload_tarball(
-            self.env.d_env.get_admin_remote(),
-            settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH, "/var")
+            # copy plugins to the master node
 
-        # install plugins
+            checkers.upload_tarball(
+                remote,
+                settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH, "/var")
 
-        checkers.install_plugin_check_code(
-            self.env.d_env.get_admin_remote(),
-            plugin=os.path.basename(
-                settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH))
+            # install plugins
+
+            checkers.install_plugin_check_code(
+                remote,
+                plugin=os.path.basename(
+                    settings.SEPARATE_SERVICE_RABBIT_PLUGIN_PATH))
 
         data = {
             'tenant': 'separaterabbit',
