6c919be575f686c2e7bc94acf0bf89cf70f9c08b
Artem Grechanichenko <agrechanichenko@mirantis.com>
2015-10-21 22:51:01 +0300

    Test for pacemaker resources when disk is full
     Adding test case for covering filling root situation on
     primary controller.
     Move _action_health_check to ostf_base_actions.py.
     Add _action_health_check_ha to ostf_base_actions.py.
    
     Partial-Bug: #1500446
    
    Change-Id: I8e08867de41519f786f106e62dbfa1cd8380516a

---
+++ system_test/tests/ostf_base_actions.py
@@ -0,0 +1,73 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from proboscis import SkipTest
+
+from system_test.helpers.decorators import make_snapshot_if_step_fail
+from system_test.helpers.decorators import deferred_decorator
+from system_test.tests import base_actions_factory
+
+
+class HealthCheckActions(base_actions_factory.BaseActionsFactory):
+    """Basic actions for OSTF tests
+    _action_health_check - run sanity and smoke OSTF tests
+    _action_health_check_ha - run ha OSTF tests
+    """
+    def __init__(self, config=None):
+        super(HealthCheckActions, self).__init__()
+
+    @deferred_decorator([make_snapshot_if_step_fail])
+    def _action_health_check(self):
+        """Run health checker
+
+        Skip action if cluster doesn't exist
+        """
+        if self.cluster_id is None:
+            raise SkipTest()
+
+        self.fuel_web.run_ostf(
+            cluster_id=self.cluster_id,
+            should_fail=getattr(self, 'ostf_tests_should_failed', 0))
+
+    @deferred_decorator([make_snapshot_if_step_fail])
+    def _action_health_check_ha(self):
+        """Run health checker
+
+        Skip action if cluster doesn't exist
+        """
+        if self.cluster_id is None:
+            raise SkipTest()
+
+        self.fuel_web.run_ostf(
+            cluster_id=self.cluster_id,
+            test_sets=['ha'],
+            should_fail=getattr(self, 'ostf_tests_should_failed', 0))
+
+    @deferred_decorator([make_snapshot_if_step_fail])
+    def _action_health_check_repeatably(self,
+                                        test_name=None,
+                                        test_retries=None,
+                                        checks=None):
+        """Run health checker repeatably
+
+        Skip action if cluster doesn't exist
+        """
+        if self.cluster_id is None:
+            raise SkipTest()
+
+        self.fuel_web.run_ostf_repeatably(
+            cluster_id=self.cluster_id,
+            test_name=test_name,
+            test_retries=test_retries,
+            checks=checks)
