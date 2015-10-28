9716e6968001a6e4fceeb1f662ad2a46267d19c9
Maksym Strukov <mstrukov@mirantis.com>
2015-10-28 19:46:01 +0200

    [WIP] l3-multiple-racks/tests
    deploy_controllers_from_custom_nodegroup https://trello.com/c/25pGZXXk/13-create-test-with-assigning-controllers-to-non-default-nodegroup
    add_nodes_from_custom_nodegroup_to_deployed_env https://trello.com/c/XoXiPh03/12-create-test-with-adding-nodegroup-to-operational-environment
    
    Change-Id: I3b6db9a1ca0c98444998ad05353a03efc8243c71

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1345,7 +1345,7 @@ class FuelWebClient(object):
                 networks['public']['ip_range'] = [
                     str(static[2]), str(static[-1])]
 
-                # use the secong half of public network as floating range
+                # use the second half of public network as floating range
                 net_settings[net_provider]['config']['floating_ranges'] = \
                     [[str(floating[0]), str(floating[-2])]]
 
@@ -1399,6 +1399,17 @@ class FuelWebClient(object):
             networking_parameters=new_settings["networking_parameters"],
             networks=new_settings["networks"]
         )
+
+    # TODO(mstrukov): remove me
+    ''' testing purposes '''
+    def get_networks(self, cluster_id):
+        return self.client.get_networks(cluster_id)
+
+    @logwrap
+    def update_cluster_network(self, cluster_id, new_settings):
+        settings = self.client.get_networks(cluster_id)
+        settings.update(new_settings)
+        self.client.update_network(settings)
 
     def _get_true_net_name(self, name, net_pools):
         """Find a devops network name in net_pools"""
