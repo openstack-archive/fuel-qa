2b00f5ba0da7ef052f083f5425cea68f598594de
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-10-28 16:40:02 +0200

    Add new runner for system test
    For run the tests from both test suites (fuelweb_test, system_test) we
        may use new runner - run_system_test.py.
    
    Features of new runner:
        - auto discovering all test in both test suites
        - show the groups from the test suites
        - explain content of groups
        - run the several groups at the same time
        - combine configuration with the test groups from new suite
    
    Change-Id: Ic4086dde60ca8a94dcd2ee079376c97ce719ff03
    Implemets blueprint template-based-testcases

--- system_test/helpers/utils.py
+++ system_test/helpers/utils.py
@@ -32,37 +32,54 @@ def copy_func(f, name=None):
     return fn
 
 
-def load_yaml_files(path):
+def get_list_confignames(filelist):
+    """Get list of config name from file list"""
+    ret = []
+    for f in filelist:
+        ret.append(get_configname(f))
+    return ret
+
+
+def get_configname(path):
+    """Get config name from path to yaml file"""
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def get_path_to_config():
+    """Find path to directory with config files"""
+    import system_test
+    return os.path.join(os.path.dirname(system_test.__file__),
+                        'tests_templates/tests_configs')
+
+
+def collect_yamls(path):
+    """Walk through config directory and find all yaml files"""
+    ret = []
+    for r, d, f in os.walk(path):
+        for one in f:
+            if os.path.splitext(one)[1] in ('.yaml', '.yml'):
+                    ret.append(os.path.join(r, one))
+    return ret
+
+
+def load_yaml_files(path=None):
+    """Convert yaml files to dicts with parameters"""
     def yaml_include(loader, node):
         file_name = os.path.join(os.path.dirname(loader.name), node.value)
         with file(file_name) as inputfile:
             return yaml.load(inputfile)
 
-    def collect_yamls(path):
-        ret = []
-        for n in os.listdir(path):
-            n = "{}/{}".format(path, n)
-            if os.path.isdir(n):
-                ret.extend(collect_yamls(n))
-            else:
-                ret.append(n)
-        return ret
-
     yamls = collect_yamls(path)
     yaml.add_constructor("!include", yaml_include)
-    return [yaml.load(open(y)) for y in yamls]
+    return {get_configname(y): yaml.load(open(y)) for y in yamls}
 
 
 def get_configs():
     """Return list of dict environment configurations"""
-    import system_test
-    path = os.path.join(os.path.dirname(system_test.__file__),
-                        'tests_templates/tests_configs')
-    return load_yaml_files(path)
+    return load_yaml_files(get_path_to_config())
 
 
 def case_factory(baseclass):
     """Return list of instance """
     configs = get_configs()
-    return [baseclass.caseclass_factory(
-        c['template']['group-name'])(c) for c in configs]
+    return [baseclass.caseclass_factory(g)(c) for g, c in configs.iteritems()]
