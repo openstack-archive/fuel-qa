9bad0dc9197284adf8d4165567d86ccddab86e11
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2015-10-28 16:40:02 +0200

    Add new runner for system test
    For run the tests from both test suites (fuelweb_test, system_test) we
        may use new runner - run_system_test.py.
    
    Features of new runner:
        - auto discovering all test in both test suites
        - show the groups from the test suites
        - explain content of groups
        - run the several groups at the same time
        - combine configuration with the test groups from new suite
    
    Change-Id: Ic4086dde60ca8a94dcd2ee079376c97ce719ff03
    Implemets blueprint template-based-testcases

--- fuelweb_test/testrail/testrail_client.py
+++ fuelweb_test/testrail/testrail_client.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from settings import logger
+from fuelweb_test.settings import logger
 from testrail import APIClient
 from testrail import APIError
 
