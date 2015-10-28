2154693bda2994247100bde71a4b0857a62bda07
Alexander Kislitsky <sand8080@gmail.com>
2015-10-28 19:03:36 +0300

    Task status 'pending' handled in tests
    Status 'pending' introduced for proper tasks as intermediate status
    for tasks ready for sending or already sent to astute, but not
    acknoledged by astute. After task is acknoledged by astute it
    changes status to 'running'.
    
    Change-Id: Idedb061b7b5c4dca4a0ca7adcaa570cecbb691af
    Partial-Bug: #1498827

--- fuelweb_test/tests/test_cli.py
+++ fuelweb_test/tests/test_cli.py
@@ -90,6 +90,103 @@ class CommandLineMinimal(TestBasic):
 class CommandLineTest(test_cli_base.CommandLine):
     """CommandLine."""  # TODO documentation
 
+    @logwrap
+    def get_task(self, remote, task_id):
+        tasks = run_on_remote(remote, 'fuel task --task-id {0} --json'
+                              .format(task_id), jsonify=True)
+        return tasks[0]
+
+    @logwrap
+    def get_network_filename(self, cluster_id, remote):
+        cmd = ('fuel --env {0} network --download --dir /tmp --json'
+               .format(cluster_id))
+        net_download = ''.join(run_on_remote(remote, cmd))
+        # net_download = 'Network ... downloaded to /tmp/network_1.json'
+        return net_download.split()[-1]
+
+    @logwrap
+    def get_networks(self, cluster_id, remote):
+        net_file = self.get_network_filename(cluster_id, remote)
+        return run_on_remote(remote, 'cat {0}'.format(net_file), jsonify=True)
+
+    @logwrap
+    def update_network(self, cluster_id, remote, net_config):
+        net_file = self.get_network_filename(cluster_id, remote)
+        data = json.dumps(net_config)
+        cmd = 'echo {data} > {net_file}'.format(data=json.dumps(data),
+                                                net_file=net_file)
+        run_on_remote(remote, cmd)
+        cmd = ('cd /tmp; fuel --env {0} network --upload --json'
+               .format(cluster_id))
+        run_on_remote(remote, cmd)
+
+    def assert_cli_task_success(
+            self, task, remote, timeout=70 * 60, interval=20):
+        logger.info('Wait {timeout} seconds for task: {task}'
+                    .format(timeout=timeout, task=task))
+        start = time.time()
+        try:
+            wait(
+                lambda: (self.get_task(remote, task['id'])['status']
+                         not in ('pending', 'running')),
+                interval=interval,
+                timeout=timeout
+            )
+        except TimeoutError:
+            raise TimeoutError(
+                "Waiting timeout {timeout} sec was reached for task: {task}"
+                .format(task=task["name"], timeout=timeout))
+        took = time.time() - start
+        task = self.get_task(remote, task['id'])
+        logger.info('Task finished in {took} seconds with the result: {task}'
+                    .format(took=took, task=task))
+        assert_equal(
+            task['status'], 'ready',
+            "Task '{name}' has incorrect status. {} != {}".format(
+                task['status'], 'ready', name=task["name"]
+            )
+        )
+
+    @logwrap
+    def update_cli_network_configuration(self, cluster_id, remote):
+        """Update cluster network settings with custom configuration.
+        Place here an additional config changes if needed (e.g. nodegroups'
+        networking configuration.
+        Also this method checks downloading/uploading networks via cli.
+        """
+        net_config = self.get_networks(cluster_id, remote)
+        new_settings = net_config
+        self.update_network(cluster_id, remote, new_settings)
+
+    def get_public_vip(self, cluster_id, remote):
+        networks = self.get_networks(cluster_id, remote)
+        return networks['public_vip']
+
+    def download_settings(self, cluster_id, remote):
+        cmd = ('fuel --env {0} settings --download --dir /tmp --json'.format(
+            cluster_id))
+        run_on_remote(remote, cmd)
+        return run_on_remote(remote,
+                             'cd /tmp && cat settings_{0}.json'.format(
+                                 cluster_id), jsonify=True)
+
+    def upload_settings(self, cluster_id, remote, settings):
+        data = json.dumps(settings)
+        cmd = 'cd /tmp && echo {data} > settings_{id}.json'.format(
+            data=json.dumps(data),
+            id=cluster_id)
+        run_on_remote(remote, cmd)
+        cmd = ('fuel --env {0} settings --upload --dir /tmp --json'.format(
+            cluster_id))
+        run_on_remote(remote, cmd)
+
+    @logwrap
+    def update_ssl_configuration(self, cluster_id, remote):
+        settings = self.download_settings(cluster_id, remote)
+        cn = self.get_public_vip(cluster_id, remote)
+        change_cluster_ssl_config(settings, cn)
+        self.upload_settings(cluster_id, remote, settings)
+
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
           groups=["cli_selected_nodes_deploy"])
     @log_snapshot_after_test
