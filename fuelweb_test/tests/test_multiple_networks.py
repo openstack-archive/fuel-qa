d2c36ad361c93e415779c0f1e02184308d6b2c01
Maksym Strukov <mstrukov@mirantis.com>
2015-10-28 19:46:01 +0200

    [WIP] l3-multiple-racks/tests
    deploy_controllers_from_custom_nodegroup https://trello.com/c/25pGZXXk/13-create-test-with-assigning-controllers-to-non-default-nodegroup
    add_nodes_from_custom_nodegroup_to_deployed_env https://trello.com/c/XoXiPh03/12-create-test-with-adding-nodegroup-to-operational-environment
    
    Change-Id: I3b6db9a1ca0c98444998ad05353a03efc8243c71

--- fuelweb_test/tests/test_multiple_networks.py
+++ fuelweb_test/tests/test_multiple_networks.py
@@ -177,3 +177,221 @@ class TestMultipleClusterNets(TestBasic):
         self.show_step(9)
         self.fuel_web.run_ostf(cluster_id=cluster_id)
         self.env.make_snapshot("deploy_ceph_ha_nodegroups")
+
+    @test(depends_on=[SetupEnvironment.prepare_release],
+          groups=["multiple_cluster_networks",
+                  "multiple_cluster_net_ceph_ha", "thread_7"])
+    @log_snapshot_after_test
+    def deploy_controllers_from_custom_nodegroup(self):
+        """Assigning controllers to non-default nodegroup
+
+        Scenario:
+            1. Revert snapshot with ready master node
+            2. Bootstrap slaves from custom nodegroup
+            3. Create environment with Neutron VXLAN and custom nodegroup
+            ?. Create new 'custom' nodegroup for the environment and configure
+            its networks (floating ranges and ALL VIPs must be from networks
+            which belong to 'custom' nodegroup)
+            4. Bootstrap slave nodes from default nodegroup
+            ?. Set IPs for VIPs (use the same subnet, but change addresses
+            automatically assigned by Nailgun)
+            5. Add 3 nodes from 'custom' nodegroup as controllers
+            Add 2 nodes from 'default' nodegroup as computes
+            Add 2 node from 'default' nodegroup as cinder/ceph
+            6. Run network verification
+            7. Deploy environment
+            8. Run network verification
+            9. Run OSTF
+
+        Duration ???m
+        Snapshot deploy_controllers_from_custom_nodegroup
+
+        """
+
+        if not MULTIPLE_NETWORKS:
+            raise SkipTest()
+
+        self.show_step(1)
+        self.env.revert_snapshot("ready")
+
+        self.show_step(2)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[1:6:2])  # 246
+
+        self.show_step(3)
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_HA,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT['tun'],
+                'tenant': 'haVxlanCeph',
+                'user': 'haVxlanCeph',
+                'password': 'haVxlanCeph'
+            }
+        )
+
+        self.show_step(4)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[0:8:2])  # 1357
+
+
+        ''' ???
+        TODO:
+        Configure its networks (floating ranges and ALL VIPs must be from
+        networks which belong to 'custom' nodegroup)
+        '''
+
+        ''' ???
+        TODO:
+        Set IPs for VIPs (use the same subnet, but change addresses
+        automatically assigned by Nailgun)
+        '''
+
+        self.show_step(5)
+        default_nodegroup = NODEGROUPS[0]['name']
+        custom_nodegroup = NODEGROUPS[1]['name']
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-02': [['controller'], custom_nodegroup],
+                'slave-04': [['controller'], custom_nodegroup],
+                'slave-06': [['controller'], custom_nodegroup],
+                'slave-01': [['compute'], default_nodegroup],
+                'slave-03': [['compute'], default_nodegroup],
+                'slave-05': [['cinder'], default_nodegroup],
+                'slave-07': [['cinder'], default_nodegroup],
+            }
+        )
+
+        self.show_step(6)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(7)
+        self.fuel_web.deploy_cluster_wait(cluster_id, timeout=150 * 60)
+
+        self.show_step(8)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(9)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+
+        self.env.make_snapshot("deploy_controllers_from_custom_nodegroup")
+
+
+    @test(depends_on=[SetupEnvironment.prepare_release],
+    groups=["multiple_cluster_networks",
+            "multiple_cluster_net_ceph_ha", "thread_7"])
+    @log_snapshot_after_test
+    def add_nodes_from_custom_nodegroup_to_deployed_env(self):
+        '''Add nodes from custom nodegroup to deployed cluster
+
+        Scenario:
+            1. Revert snapshot with ready master node
+            2. Bootstrap slaves from default nodegroup
+            3. Create environment with Neutron VXLAN and default nodegroup
+            4. Add nodes from 'default' nodegroup
+            5. Run network verification
+            6. Deploy environment
+            7. Run network verification
+            8. Run OSTF
+            9. Bootstrap slaves from custom nodegroup
+            ?. Create new 'custom' nodegroup for the environment and configure its
+            networks
+            10. Add nodes from 'custom' nodegroup to the environment
+            11. Run network verification
+            12. Deploy changes
+            13. Run network verification
+            14. Run OSTF
+            15. Check that nodes from 'default' nodegroup can reach nodes from
+            'custom' nodegroup via management and storage networks (check route
+            presence, verify by ping)
+
+        Duration ???m
+        Snapshot add_nodes_from_custom_nodegroup_to_deployed_env
+        '''
+
+        if not MULTIPLE_NETWORKS:
+            raise SkipTest()
+
+        self.show_step(1)
+        self.env.revert_snapshot("ready")
+
+        self.show_step(2)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[0:9:2])  # 13579
+
+        self.show_step(3)
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_HA,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT['tun'],
+                'tenant': 'haVxlanCeph',
+                'user': 'haVxlanCeph',
+                'password': 'haVxlanCeph'
+            }
+        )
+
+        self.show_step(4)
+        default_nodegroup = NODEGROUPS[0]['name']
+        custom_nodegroup = NODEGROUPS[1]['name']
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': [['controller'], default_nodegroup],
+                'slave-03': [['controller'], default_nodegroup],
+                'slave-05': [['controller'], default_nodegroup],
+                'slave-07': [['compute'], default_nodegroup],
+                'slave-09': [['cinder'], default_nodegroup],
+            }
+        )
+
+        self.show_step(5)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(6)
+        self.fuel_web.deploy_cluster_wait(cluster_id, timeout=150 * 60)
+
+        self.show_step(7)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(8)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+
+        self.show_step(9)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[1:4:2])  # 24
+
+        self.show_step(10)
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': [['controller'], default_nodegroup],
+                'slave-03': [['controller'], default_nodegroup],
+                'slave-05': [['controller'], default_nodegroup],
+                'slave-07': [['compute'], default_nodegroup],
+                'slave-09': [['cinder'], default_nodegroup],
+                'slave-02': [['compute'], custom_nodegroup],
+                'slave-04': [['compute'], custom_nodegroup],
+            }
+        )
+
+        self.show_step(11)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(12)
+        self.fuel_web.deploy_cluster_wait(cluster_id, timeout=150 * 60)
+
+        self.show_step(13)
+        self.fuel_web.verify_network(cluster_id)
+
+        self.show_step(14)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+
+        self.show_step(15)
+        '''
+        TODO:
+        Check that nodes from 'default' nodegroup can reach nodes from
+        'custom' nodegroup via management and storage networks (check route
+        presence, verify by ping)
+        '''
+
+        self.env.make_snapshot("add_nodes_from_custom_nodegroup_to_deployed_env")
