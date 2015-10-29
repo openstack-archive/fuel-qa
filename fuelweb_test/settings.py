2a41824aa594db46ac2ad1846eb8f526aea9f03e
Artur Kaszuba <akaszuba@mirantis.com>
2015-10-29 17:01:32 +0100

    Add external snapshots variable
    Change-Id: I0e9dc4b14d296fa0fd20ad58c78a5e64f7a7ab3a

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -507,3 +507,5 @@ NESSUS_PORT = os.environ.get("NESSUS_PORT", 8834)
 NESSUS_USERNAME = os.environ.get("NESSUS_USERNAME")
 NESSUS_PASSWORD = os.environ.get("NESSUS_PASSWORD")
 NESSUS_SSL_VERIFY = get_var_as_bool("NESSUS_SSL_VERIFY", False)
+
+SNAPSHOTS_EXTERNAL = get_var_as_bool('SNAPSHOTS_EXTERNAL', False)
