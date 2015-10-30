fcc8ed64fbf32c22532085da4ec5dc22292b733a
Tatyana Leontovich <tleontovich@mirantis.com>
2015-10-30 16:41:13 +0200

    Increase timeout for env deleteion over cli
    In test delete cluster iver cli we use
    revert mechanism, but do not check if all the nodes
    become online after revert. This lead to the situiation
    when cluster deletion takes a liitle bit more time.
    So In this patch delete timeout was increased.
    Also add check for node statuses after revert
    and log with error level if some of them is offline.
    We do not interrupt tests here according to cluster should be deleted,
    even all the nodes are offline
    
    Change-Id: I769e5a17875595b632d111d6b5f1a782989e3a37
    Closes-Bug: #1508007

--- fuelweb_test/tests/test_cli.py
+++ fuelweb_test/tests/test_cli.py
@@ -325,7 +325,7 @@ class CommandLine(TestBasic):
                 wait(
                     lambda: not remote.execute(
                         "fuel node | awk '{{print $1}}' | grep -w '{0}'".
-                        format(node_id))['exit_code'] == 0, timeout=60 * 2)
+                        format(node_id))['exit_code'] == 0, timeout=60 * 4)
             except TimeoutError:
                 raise TimeoutError(
                     "After deletion node-{0} is found in fuel list".
@@ -365,6 +365,14 @@ class CommandLine(TestBasic):
         self.env.revert_snapshot("cli_selected_nodes_deploy")
 
         cluster_id = self.fuel_web.get_last_created_cluster()
+
+        nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
+        online_nodes = [node for node in nodes if node['online']]
+        if nodes != online_nodes:
+            logger.error(
+                'Some slaves do not become online after revert!!'
+                ' Expected {0} Actual {1}'.format(nodes, online_nodes))
+
         with self.env.d_env.get_admin_remote() as remote:
             res = remote.execute('fuel --env {0} env delete'
                                  .format(cluster_id))
@@ -376,7 +384,7 @@ class CommandLine(TestBasic):
                 wait(lambda:
                      remote.execute("fuel env |  awk '{print $1}'"
                                     " |  tail -n 1 | grep '^.$'")
-                     ['exit_code'] == 1, timeout=60 * 6)
+                     ['exit_code'] == 1, timeout=60 * 10)
             except TimeoutError:
                 raise TimeoutError(
                     "cluster {0} was not deleted".format(cluster_id))
