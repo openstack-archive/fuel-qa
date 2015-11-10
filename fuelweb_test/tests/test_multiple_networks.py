a0ac070354122c2d69976bd8080feb367e447a7b
Tatyana Leontovich <tleontovich@mirantis.com>
2015-11-10 17:57:40 +0200

    Update networks over cli
    Add nexts steps:
    * download net config over cli
    * upload it
    * check that it is successfully applied
    
    Closes-Bug: #1512314
    Change-Id: I4b93289aacf78a5e02f0c830a822ad2ba048b010

--- fuelweb_test/tests/test_multiple_networks.py
+++ fuelweb_test/tests/test_multiple_networks.py
@@ -14,15 +14,20 @@
 
 from ipaddr import IPAddress
 from ipaddr import summarize_address_range
+import netaddr
+import json
 
 from devops.helpers.helpers import wait
 from devops.error import TimeoutError
+from proboscis import asserts
 from proboscis import SkipTest
 from proboscis import test
-from proboscis.asserts import assert_true
 
+from fuelweb_test.helpers.checkers import check_get_network_data_over_cli
+from fuelweb_test.helpers.checkers import check_update_network_data_over_cli
 from fuelweb_test.helpers.decorators import check_fuel_statistics
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.helpers import utils
 from fuelweb_test.settings import DEPLOYMENT_MODE_HA
 from fuelweb_test.settings import MULTIPLE_NETWORKS
 from fuelweb_test.settings import NEUTRON_SEGMENT
@@ -37,6 +42,14 @@ class TestMultipleClusterNets(TestBasic):
     """TestMultipleClusterNets."""  # TODO documentation
 
     @staticmethod
+    def get_modified_ranges(net_dict, net_name, group_id):
+        for net in net_dict['networks']:
+            if net_name in net['name'] and net['group_id'] == group_id:
+                cidr = net['cidr']
+                sliced_list = list(netaddr.IPNetwork(cidr))[5:-5]
+                return [str(sliced_list[0]), str(sliced_list[-1])]
+
+    @staticmethod
     def change_default_admin_range(networks, number_excluded_ips):
         """Change IP range for admin network by excluding N of first addresses
         from default range
@@ -46,9 +59,9 @@ class TestMultipleClusterNets(TestBasic):
         default_admin_network = [n for n in networks
                                  if (n['name'] == "fuelweb_admin" and
                                      n['group_id'] is None)]
-        assert_true(len(default_admin_network) == 1,
-                    "Default 'admin/pxe' network not found "
-                    "in cluster network configuration!")
+        asserts.assert_true(len(default_admin_network) == 1,
+                            "Default 'admin/pxe' network not found "
+                            "in cluster network configuration!")
         default_admin_range = [IPAddress(ip) for ip
                                in default_admin_network[0]["ip_ranges"][0]]
         new_admin_range = [default_admin_range[0] + number_excluded_ips,
@@ -84,11 +97,16 @@ class TestMultipleClusterNets(TestBasic):
             2. Bootstrap slaves from default nodegroup
             3. Create cluster with Neutron VXLAN and custom nodegroup
             4. Bootstrap slave nodes from custom nodegroup
-            5. Add 3 controller nodes from default nodegroup
-            6. Add 2 compute nodes from custom nodegroup
-            7. Deploy cluster
-            8. Run network verification
-            9. Run health checks (OSTF)
+            5. Download network configuration
+            6. Update network.json  with customized ip ranges
+            7. Put new json on master node and update network data
+            8. Verify that new IP ranges are applied for network config
+            9. Add 3 controller nodes from default nodegroup
+            10. Add 2 compute nodes from custom nodegroup
+            11. Deploy cluster
+            12. Run network verification
+            13. Verify that excluded ip is not used for nodes or VIP
+            14. Run health checks (OSTF)
 
         Duration 110m
         Snapshot deploy_neutron_tun_ha_nodegroups
@@ -121,7 +139,126 @@ class TestMultipleClusterNets(TestBasic):
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[1:5:2])
 
         self.show_step(5)
+        with self.env.d_env.get_admin_remote() as remote:
+            check_get_network_data_over_cli(remote, cluster_id, '/var/log/')
+
+        management_ranges_default = []
+        management_ranges_custom = []
+        storage_ranges_default = []
+        storage_ranges_custom = []
+        default_group_id = self.fuel_web.get_nodegroup(cluster_id)['id']
+        custom_group_id = self.fuel_web.get_nodegroup(
+            cluster_id, name=NODEGROUPS[1]['name'])['id']
+
         self.show_step(6)
+        with self.env.d_env.get_admin_remote() as remote:
+            current_net = json.loads(remote.open(
+                '/var/log/network_1.json').read())
+            # Get storage ranges for default group
+            storage_ranges_default.append(self.get_modified_ranges(
+                current_net, 'storage', group_id=default_group_id))
+            # Update storage ranges default group
+            for net in current_net['networks']:
+                if 'storage' in net['name'] \
+                        and net['group_id'] == default_group_id:
+                    net['ip_ranges'] = storage_ranges_default
+                    net['meta']['notation'] = 'ip_ranges'
+
+            # Get storage ranges for custom group
+            storage_ranges_custom.append(self.get_modified_ranges(
+                current_net, 'storage', group_id=custom_group_id))
+            # Update storage ranges custom group
+            for net in current_net['networks']:
+                if 'storage' in net['name'] \
+                        and net['group_id'] == custom_group_id:
+                    net['ip_ranges'] = storage_ranges_custom
+                    net['meta']['notation'] = 'ip_ranges'
+
+            # Get management ranges default group
+            management_ranges_default.append(self.get_modified_ranges(
+                current_net, 'management', group_id=default_group_id))
+            # Update management ranges default group
+            for net in current_net['networks']:
+                if 'management' in net['name'] \
+                        and default_group_id == net['group_id']:
+                    net['ip_ranges'] = management_ranges_default
+                    net['meta']['notation'] = 'ip_ranges'
+
+            # Get management ranges custom group
+            management_ranges_custom.append(self.get_modified_ranges(
+                current_net, 'management', group_id=custom_group_id))
+            # Update management ranges default group
+            for net in current_net['networks']:
+                if 'management' in net['name'] \
+                        and custom_group_id == net['group_id']:
+                    net['ip_ranges'] = management_ranges_custom
+                    net['meta']['notation'] = 'ip_ranges'
+
+            logger.debug(
+                'Plan to update ranges for default group to {0} for storage '
+                'and {1} for management and for custom group storage {2},'
+                ' management {3}'.format(storage_ranges_default,
+                                         management_ranges_default,
+                                         storage_ranges_custom,
+                                         management_ranges_custom))
+
+            # need to push to remote
+            self.show_step(7)
+            utils.put_json_on_remote_from_dict(
+                remote, current_net, cluster_id)
+
+            check_update_network_data_over_cli(remote, cluster_id,
+                                               '/var/log/')
+
+        self.show_step(8)
+        with self.env.d_env.get_admin_remote() as remote:
+            check_get_network_data_over_cli(remote, cluster_id, '/var/log/')
+            latest_net = json.loads(remote.open(
+                '/var/log/network_1.json').read())
+            updated_storage_default = [net['ip_ranges']
+                                       for net in latest_net['networks']
+                                       if 'storage' in net['name'] and
+                                          default_group_id == net['group_id']][0]
+
+            updated_storage_custom = [net['ip_ranges']
+                                      for net in latest_net['networks']
+                                      if 'storage' in net['name'] and
+                                         custom_group_id == net['group_id']][0]
+
+            updated_mgmt_default = [net['ip_ranges'] for net
+                                    in latest_net['networks']
+                                    if 'management' in net['name'] and
+                                       default_group_id == net['group_id']][0]
+
+            updated_mgmt_custom = [net['ip_ranges'] for net
+                                   in latest_net['networks']
+                                   if 'management' in net['name'] and
+                                      custom_group_id == net['group_id']][0]
+
+            asserts.assert_equal(
+                updated_storage_default, storage_ranges_default,
+                'Looks like storage range for default nodegroup '
+                'was not updated. Expected {0}, Actual: {1}'.format(
+                    storage_ranges_default, updated_storage_default))
+
+            asserts.assert_equal(
+                updated_storage_custom, storage_ranges_custom,
+                'Looks like storage range for custom nodegroup '
+                'was not updated. Expected {0}, Actual: {1}'.format(
+                    storage_ranges_custom, updated_storage_custom))
+
+            asserts.assert_equal(
+                updated_mgmt_default, management_ranges_default,
+                'Looks like management range fro default nodegroup was '
+                'not updated. Expected {0}, Actual: {1}'.format(
+                    management_ranges_default, updated_mgmt_default))
+
+            asserts.assert_equal(
+                updated_mgmt_custom, management_ranges_custom,
+                'Looks like management range fro custom nodegroup was '
+                'not updated. Expected {0}, Actual: {1}'.format(
+                    management_ranges_custom, updated_mgmt_custom))
+
         nodegroup_default = NODEGROUPS[0]['name']
         nodegroup_custom = NODEGROUPS[1]['name']
         self.fuel_web.update_nodes(
@@ -135,13 +272,70 @@ class TestMultipleClusterNets(TestBasic):
             }
         )
 
-        self.show_step(7)
+        self.show_step(11)
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        self.show_step(8)
+        self.show_step(12)
         self.fuel_web.verify_network(cluster_id)
 
-        self.show_step(9)
+        self.show_step(13)
+        net_data_default_group = [
+            data['network_data'] for data
+            in self.fuel_web.client.list_cluster_nodes(
+                cluster_id) if data['group_id'] == default_group_id]
+
+        for net_node in net_data_default_group:
+            logger.info('net_node {0}'.format(net_node))
+            for net in net_node:
+                logger.info('net is {0}'.format(net))
+                if 'storage' in net['name']:
+                    asserts.assert_true(
+                        self.is_ip_in_range(
+                            net['ip'],
+                            updated_storage_default[0],
+                            updated_storage_default[-1]))
+                if 'management' in net['name']:
+                    asserts.assert_true(
+                        self.is_ip_in_range(
+                            net['ip'],
+                            updated_mgmt_default[0],
+                            updated_mgmt_default[-1]))
+
+        net_data_custom_group = [
+            data for data in self.fuel_web.client.list_cluster_nodes(
+                cluster_id) if data['group_id'] == custom_group_id]
+
+        for net_node in net_data_custom_group:
+            logger.info('net_node {0}'.format(net_node))
+            for net in net_node:
+                logger.info('net is {0}'.format(net))
+                if 'storage' in net['name']:
+                    asserts.assert_true(
+                        self.is_ip_in_range(
+                            net['ip'],
+                            updated_storage_custom[0],
+                            updated_storage_custom[-1]))
+                if 'management' in net['name']:
+                    asserts.assert_true(
+                        self.is_ip_in_range(
+                            net['ip'],
+                            updated_mgmt_custom[0],
+                            updated_mgmt_custom[-1]))
+
+        mgmt_vrouter_vip = self.fuel_web.get_management_vrouter_vip(
+            cluster_id)
+        logger.debug('Management vrouter vips is {0}'.format(
+            mgmt_vrouter_vip))
+        mgmt_vip = self.fuel_web.get_mgmt_vip(cluster_id)
+        logger.debug('Management vips is {0}'.format(mgmt_vip))
+        #check for defaults
+        asserts.assert_true(self.is_ip_in_range(mgmt_vrouter_vip,
+                                                updated_mgmt_default[0],
+                                                updated_mgmt_default[-1]))
+        asserts.assert_true(self.is_ip_in_range(mgmt_vip,
+                                                updated_mgmt_default[0],
+                                                updated_mgmt_default[-1]))
+        self.show_step(14)
         self.fuel_web.run_ostf(cluster_id=cluster_id)
 
         self.env.make_snapshot("deploy_neutron_tun_ha_nodegroups")
@@ -178,7 +372,9 @@ class TestMultipleClusterNets(TestBasic):
         self.env.revert_snapshot("ready")
 
         self.show_step(2)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[0:5:2])
 
+        self.show_step(3)
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
             mode=DEPLOYMENT_MODE_HA,
@@ -207,17 +403,19 @@ class TestMultipleClusterNets(TestBasic):
                     " {0}".format(new_admin_range))
 
         self.show_step(4)
+        self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[1:5:2])
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[0:5])
 
         self.show_step(5)
         default_ng_nodes = [self.fuel_web.get_nailgun_node_by_devops_node(node)
                             for node in self.env.d_env.nodes().slaves[0:5:2]]
         for node in default_ng_nodes:
-            assert_true(self.is_ip_in_range(node['ip'], *new_admin_range),
-                        "Node '{0}' has IP address '{1}' which "
-                        "is not from defined IP addresses range:"
-                        " {2}!".format(node['fqdn'], node['ip'],
-                                       new_admin_range))
+            asserts.assert_true(
+                self.is_ip_in_range(node['ip'], *new_admin_range),
+                "Node '{0}' has IP address '{1}' which "
+                "is not from defined IP addresses range:"
+                " {2}!".format(node['fqdn'], node['ip'],
+                new_admin_range))
 
         self.show_step(6)
         self.show_step(7)
@@ -247,11 +445,12 @@ class TestMultipleClusterNets(TestBasic):
                             self.fuel_web.client.list_cluster_nodes(cluster_id)
                             if node['group_id'] == group_id]
         for node in default_ng_nodes:
-            assert_true(self.is_ip_in_range(node['ip'], *new_admin_range),
-                        "Node '{0}' has IP address '{1}' which "
-                        "is not from defined IP addresses range:"
-                        " {2}!".format(node['fqdn'], node['ip'],
-                                       new_admin_range))
+            asserts.assert_true(
+                self.is_ip_in_range(node['ip'], *new_admin_range),
+                "Node '{0}' has IP address '{1}' which "
+                "is not from defined IP addresses range:"
+                " {2}!".format(node['fqdn'], node['ip'],
+                new_admin_range))
 
         self.env.make_snapshot("deploy_ceph_ha_nodegroups")
 
@@ -349,10 +548,11 @@ class TestMultipleClusterNets(TestBasic):
         }
 
         for k in check:
-            vip = IPAddress(current_settings['vips'][k]['ipaddr'])
+            vip = netaddr.IPAddress(current_settings['vips'][k]['ipaddr'])
             custom_net = self.env.d_env.get_network(name=check[k]).ip
-            assert_true(vip in custom_net,
-                        '{0} is not from {1} network'.format(k, check[k]))
+            asserts.assert_true(
+                vip in custom_net,
+                '{0} is not from {1} network'.format(k, check[k]))
             logger.info('{0} is from {1} network'.format(k, check[k]))
 
         self.show_step(9)
