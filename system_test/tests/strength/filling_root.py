d6288e937a383ccf415f063c6015d6d20c1da3bb
Artem Grechanichenko <agrechanichenko@mirantis.com>
2015-11-10 16:15:32 +0200

    Test for pacemaker resources when disk is full
     Adding test case for covering filling root situation on primary controller.
     Add health_check_all to ostf_base_actions.py.
     Move health_check to ostf_base_actions.py
    
     Closes-Bug: #1500446
    
    Change-Id: I17055527e99c72b790544e7baef997eab20258e0

---
+++ system_test/tests/strength/filling_root.py
@@ -0,0 +1,82 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from system_test.helpers.utils import case_factory
+from proboscis import factory
+
+from system_test.tests.strength import strenght_base
+
+
+class FillRootPrimaryController(
+    strenght_base.FillRootBaseActions
+):
+    """Fill root filesystem on primary controller and check pacemaker
+
+    Scenario:
+        1. Setup master node
+        2. config default repositories for release
+        3. Bootstap slaves and make snapshot ready
+        4. Revert snapshot ready
+        5. Create Environment
+        6. Add nodes to Environment
+        7. Run network checker
+        8. Deploy Environment
+        9. Run network checker
+        10. Run OSTF
+        11. Make or use existing snapshot of ready Environment
+        12. Get pcs initial state
+        13. Fill root filesystem on primary controller
+           and check pacemaker and cibadmin status
+        14. Run Sanity and Smoke OSTF tests
+        15. Resolve the full space on root an check for restarting services
+        16. Run Sanity and Smoke OSTF tests
+        17. Run HA OSTF tests
+
+    """
+
+    base_group = ['system_test',
+                  'system_test.failover',
+                  'system_test.failover.filling_root'
+                  ]
+
+    actions_order = [
+        'setup_master',
+        'config_release',
+        'make_slaves',
+        'revert_slaves',
+        'create_env',
+        'add_nodes',
+        'network_check',
+        'deploy_cluster',
+        'network_check',
+        'health_check',
+        'save_load_environment',
+        'get_pcs_initial_state',
+        'fill_root_above_rabbit_disk_free_limit',
+        'check_stopping_resources',
+        'health_check',
+        'check_failed_ostf_ha',
+        'fill_root_below_rabbit_disk_free_limit',
+        'check_stopping_resources',
+        'health_check',
+        'check_failed_ostf_ha',
+        'resolve_space_on_root',
+        'check_starting_resources',
+        'health_check_all',
+    ]
+
+
+@factory
+def cases():
+    return (case_factory(FillRootPrimaryController))
