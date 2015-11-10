b0dbe1630dae2364d0e21a62d74d3174f7842f34
Maksym Strukov <mstrukov@mirantis.com>
2015-11-10 16:27:58 +0200

    Add test with adding nodegroup to operational environment
    Scenario:
    1. Revert snapshot with ready master node
    2. Bootstrap slaves from default nodegroup
    3. Create environment with Neutron VXLAN and default nodegroup
    4. Add nodes from 'default' nodegroup
    5. Run network verification
    6. Deploy environment
    7. Run network verification
    8. Run OSTF
    9. Bootstrap slaves from custom nodegroup
    10. Add nodes from 'custom' nodegroup to the environment
    11. Run network verification
    12. Deploy changes
    13. Run network verification
    14. Run OSTF
    15. Check that nodes from 'default' nodegroup can reach nodes from
        'custom' nodegroup via management and storage networks
    
    Change-Id: Ibabecb485f794692c0f190aa45066f26f67c137f
    Implements: blueprint test-nodegroup-add

--- fuelweb_test/helpers/granular_deployment_checkers.py
+++ fuelweb_test/helpers/granular_deployment_checkers.py
@@ -18,6 +18,7 @@ from proboscis.asserts import assert_equal
 from proboscis.asserts import assert_true
 
 from fuelweb_test import logger
+from fuelweb_test.helpers.utils import run_on_remote
 
 
 def check_hiera_resources(remote, file_name=None):
@@ -42,6 +43,14 @@ def get_hiera_data(remote, data):
     cmd = 'hiera {}'.format(data)
     res = remote.execute(cmd)['stdout']
     return res
+
+
+def get_hiera_data_as_json(remote, data):
+    cmd = "ruby -rhiera -rjson -e \"h = Hiera.new(); " \
+          "Hiera.logger = 'noop'; puts JSON.dump " \
+          "(h.lookup('{}', " \
+          "[], {{}}, nil, nil))\"".format(data)
+    return run_on_remote(remote, cmd, jsonify=True)
 
 
 def check_interface_status(remote, iname):
