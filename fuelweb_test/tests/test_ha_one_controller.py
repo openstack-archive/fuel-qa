f6b15771f1543ffa44bf06c8140c422663996db9
Alexandr Kostrikov <akostrikov@mirantis.com>
2015-11-17 15:10:00 +0300

    [WIP][Do not merge] Temporary fix to run on CI.
    Update interfaces  are passing now
    
    Change-Id: I0d23f270bebc15492315c38c5e604ccb4002d165

--- fuelweb_test/tests/test_ha_one_controller.py
+++ fuelweb_test/tests/test_ha_one_controller.py
@@ -28,6 +28,7 @@ from fuelweb_test.settings import NEUTRON_SEGMENT
 from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
 from fuelweb_test.settings import OPENSTACK_RELEASE
 from fuelweb_test.settings import OPENSTACK_RELEASE_UBUNTU
+from fuelweb_test.settings import PREDICTABLE_INTERFACE_NAMES
 from fuelweb_test.helpers.utils import run_on_remote_get_results
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
@@ -826,6 +827,13 @@ class UntaggedNetworksNegative(TestBasic):
             'eth2': ["management", "storage"],
             'eth3': []
         }
+        if PREDICTABLE_INTERFACE_NAMES:
+            interfaces = {
+                'enp0s3': ["fixed"],
+                'enp0s4': ["public"],
+                'enp0s5': ["management", "storage"],
+                'enp0s6': []
+            }
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
