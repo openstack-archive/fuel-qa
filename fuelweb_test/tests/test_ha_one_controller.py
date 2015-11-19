ff7ed26a9a2b0dbb6debb11b10fdfb8989a8898c
Maksym Strukov <mstrukov@mirantis.com>
2015-11-20 00:07:27 +0200

    Improve 'delete_environment' system test
    Test removes cluster and wait until nodes returns to unallocated
    pool. From time to time it fails due to timeout. Let's improve
    test:
    * add method for cluster deletion. It allows create 'cluster_deletion'
    task and wait until cluster deleted
    * increase timeout for nodes bootstrap
    
    Change-Id: I1aea90b4afe0453d464e7d1ce75eaf3c274482d7
    Closes-Bug: #1518099

--- fuelweb_test/tests/test_ha_one_controller.py
+++ fuelweb_test/tests/test_ha_one_controller.py
@@ -318,12 +318,8 @@ class HAOneControllerNeutron(HAOneControllerNeutronBase):
         self.env.revert_snapshot("deploy_ha_one_controller_neutron")
 
         cluster_id = self.fuel_web.get_last_created_cluster()
-        self.fuel_web.client.delete_cluster(cluster_id)
-        nailgun_nodes = self.fuel_web.client.list_nodes()
-        nodes = filter(lambda x: x["pending_deletion"] is True, nailgun_nodes)
-        assert_true(
-            len(nodes) == 2, "Verify 2 node has pending deletion status"
-        )
+        self.fuel_web.delete_env_wait(cluster_id, timeout=10 * 60)
+        nodes = self.env.d_env.nodes().slaves[0:2]
         wait(
             lambda:
             self.fuel_web.is_node_discovered(nodes[0]) and
