8a08cfa0079b21ecdb16715f4b48ba6066ad7cbc
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2015-12-01 03:11:41 +0300

    Initial implementation of predeployed RHEL computes
    - Add system test which deploy configuration with RHEL-based
    compute.
    - Add verification steps for it and log snapshot after test
    copmleted positively.
    
    Change-Id: I08437f11a3f0e14fcf46f1c8293a5d8da882dd97

---
+++ fuelweb_test/tests/test_rhel_compute.py
@@ -0,0 +1,303 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import division
+from devops.helpers.helpers import _wait
+from devops.helpers.helpers import _tcp_ping
+from proboscis import asserts
+from proboscis import test
+from fuelweb_test.helpers import checkers
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.helpers import os_actions
+from fuelweb_test import settings
+from fuelweb_test import logger as LOGGER
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+
+
+@test(groups=["rhel", "rhel_ha", "rhel.basic"])
+class RhelHA(TestBasic):
+    """RHEL-based compute tests"""
+
+    def wait_for_slave_provision(self, slave_name, timeout=10 * 60):
+        _wait(lambda: _tcp_ping(
+            self.env.d_env.get_node(
+                name=slave_name).get_ip_address_by_network_name
+            (self.env.d_env.admin_net), 22), timeout=timeout)
+
+    def connect_rhel_image(self, slave):
+        """Upload RHEL image into target node
+
+        :param slave: Target node name
+        """
+        path = settings.RHEL_IMAGE_PATH + settings.RHEL_IMAGE
+        if settings.COMPUTE_BOOT_STRATEGY == 'usb':
+            slave.disk_devices.get(
+                device='disk', bus='usb').volume.upload(path)
+        elif settings.COMPUTE_BOOT_STRATEGY == 'cdrom':
+            slave.disk_devices.get(
+                device='cdrom').volume.upload(path)
+        else:
+            slave.disk_devices.get(
+                name='system').volume.upload(path)
+
+    def register_rhel_subscription(self, ip):
+        reg_command = (
+            "/usr/sbin/subscription-manager register "
+            "--username={0} --password={1} --serverurl={2} "
+            "--org={3} --release={4}".format(
+                settings.RHEL_LICENSE_USERNAME,
+                settings.RHEL_LICENSE_PASSWORD,
+                settings.RHEL_SERVER_URL,
+                settings.RHEL_REGISTERED_ORG_NAME,
+                settings.RHEL_RELEASE)
+        )
+
+        if settings.RHEL_ACTIVATION_KEY:
+            reg_command = reg_command + "--activationkey={0}".format(
+                settings.RHEL_ACTIVATION_KEY)
+
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            result = remote.execute(reg_command)
+        LOGGER.debug(result)
+
+    def enable_rhel_repos(self, ip):
+        cmd = ("yum-config-manager --enable rhel-7-server-optional-rpms && "
+               "yum-config-manager --enable rhel-7-server-extras-rpms &&"
+               "yum-config-manager --enable rhel-7-server-rh-common-rpms")
+
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+
+    def set_hostname(self, ip):
+        cmd = "sysctl kernel.hostname=rhel.domain.local"
+
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+
+    def apply_compute_puppet(self, ip):
+        puppet_path = [
+            "/etc/puppet/modules/osnailyfacter/modular/hiera/hiera.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/globals/globals.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/firewall/firewall.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/tools/tools.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/netconfig/netconfig.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/roles/compute.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/"
+            "openstack-network/common-config.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/"
+            "openstack-network/plugins/ml2.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/"
+            "openstack-network/agents/l3.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/"
+            "openstack-network/agents/metadata.pp",
+            "/etc/puppet/modules/osnailyfacter/modular/"
+            "openstack-network/compute-nova.pp"]
+
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            for puppet in puppet_path:
+                result = remote.execute("puppet apply -vd {0}".format(puppet))
+                LOGGER.debug(result)
+
+    def backup_required_information(self, target_node_name):
+        cmd = ("cd ~/ && mkdir rhel_backup && "
+               "scp -r {0}:/root/.ssh rhel_backup/. && "
+               "scp {0}:/etc/astute.yaml rhel_backup/ && "
+               "scp -r {0}:/var/lib/astute/nova .").format(target_node_name)
+        with self.env.d_env.get_ssh_to_remote(self.env.admin_node_ip()) as \
+                remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+        LOGGER.debug("Backed up ssh-keys and astute.yaml")
+
+    def restore_information(self, ip):
+        cmd = (
+            "cd ~/rhel_backup && "
+            "scp -r .ssh {0}:{1}@{2}:/root/. && "
+            "scp astute.yaml {0}:{1}@{2}:/etc/.".format(
+                settings.RHEL_IMAGE_USER,
+                settings.RHEL_IMAGE_PASSWORD,
+                ip)
+        )
+
+        with self.env.d_env.get_ssh_to_remote(self.env.admin_node_ip()) as \
+                remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+
+        cmd = "mkdir -p /var/lib/astute"
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+
+        cmd = ("cd ~/rhel_backup && "
+               "scp -r nova {0}:{1}@{2}:/var/lib/astute/")
+
+        with self.env.d_env.get_ssh_to_remote(self.env.admin_node_ip()) as \
+                remote:
+            result = remote.execute(cmd)
+        LOGGER.debug(result)
+
+    def set_repo_for_perestroika(self, ip):
+        cmd = ("curl https://perestroika-repo-tst.infra.mirantis.net/"
+               "mos-repos/centos/mos8.0-centos7-fuel/os/x86_64/")
+
+        with self.env.d_env.get_ssh_to_remote(ip) as remote:
+            result = remote.execute(cmd)
+            if result['exit_code'] != 0:
+                LOGGER.warning(result)
+                msg = "Perestroika repos unavailable from node. Exiting."
+                LOGGER.error(msg)
+                raise Exception("msg")
+            else:
+                cmd = ("echo '[mos-8.0]\n"
+                       "name=mos-8.0\n"
+                       "type=rpm-md\n"
+                       "baseurl="
+                       "http://perestroika-repo-tst.infra.mirantis.net/"
+                       "mos-repos/centos/mos8.0-centos7-fuel/os/x86_64/\n"
+                       "gpgcheck=1\n"
+                       "enabled=1\n"
+                       "priority=5' >>"
+                       "/etc/yum.repos.d/mos8.0.repo && "
+                       "yum install yum-utils -y")
+                result = remote.execute(cmd)
+                LOGGER.debug(result)
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["deploy_rhel_compute_ha_tun"])
+    @log_snapshot_after_test
+    def deploy_rhel_based_compute(self):
+        """Deploy RHEL-based compute in HA mode with Neutron VXLAN
+
+        Scenario:
+            1. Check required image.
+            2. Revert snapshot 'ready_with_5_slaves'.
+            3. Create a Fuel cluster.
+            4. Update cluster nodes with required roles.
+            5. Deploy the Fuel cluster.
+            6. Run OSTF.
+            7. Backup astute.yaml and ssh keys from compute.
+            8. Boot compute with RHEL image.
+            9. Prepare node for Puppet run.
+            10. Execute modular tasks for compute.
+            11. Run OSTF.
+
+        Duration: 150m
+        Snapshot: deploy_rhel_compute_ha_tun
+
+        """
+        self.show_step(1)
+        LOGGER.debug('Check MD5 sum of RHEL 7 image')
+        check_image = checkers.check_image(
+            settings.RHEL_IMAGE,
+            settings.RHEL_IMAGE_MD5,
+            settings.RHEL_IMAGE_PATH)
+        asserts.assert_true(check_image)
+
+        self.show_step(2)
+        self.env.revert_snapshot("ready_with_5_slaves")
+
+        self.show_step(3)
+        LOGGER.debug('Create Fuel cluster RHEL-based compute tests')
+        data = {
+            'net_provider': 'neutron',
+            'net_segment_type': settings.NEUTRON_SEGMENT['tun'],
+            'tenant': 'RhelHA',
+            'user': 'RhelHA',
+            'password': 'RhelHA'
+        }
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE,
+            settings=data
+        )
+
+        self.show_step(4)
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute']
+            }
+        )
+
+        self.show_step(5)
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        cluster_vip = self.fuel_web.get_public_vip(cluster_id)
+        os_conn = os_actions.OpenStackActions(
+            cluster_vip, data['user'], data['password'], data['tenant'])
+        self.fuel_web.assert_cluster_ready(os_conn, smiles_count=13)
+
+        self.show_step(6)
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['ha', 'smoke', 'sanity'])
+
+        self.show_step(7)
+        target_node_name = "slave-04"
+        self.backup_required_information(target_node_name)
+
+        self.show_step(8)
+        target_node = self.env.d_env.get_node(name=target_node_name)
+        target_node.destroy()
+        self.connect_rhel_image(target_node)
+        target_node.start()
+        self.wait_for_slave_provision(target_node_name)
+
+        self.show_step(9)
+        target_node_ip = target_node.get_ip_address_by_network_name('admin')
+        self.restore_information(target_node_ip)
+        self.set_hostname(target_node_ip)
+        self.set_repo_for_perestroika(target_node_ip)
+
+        # Remove this parameter after infrastructure and tests will be ready.
+        if not settings.CENTOS_DUMMY_DEPLOY:
+            self.register_rhel_subscription(target_node_ip)
+            self.enable_rhel_repos(target_node_ip)
+
+        puppet_install_cmd = "sudo yum install puppet ruby -y"
+        with self.env.d_env.get_ssh_to_remote(target_node_ip) as remote:
+            result = remote.execute(puppet_install_cmd)
+            LOGGER.debug(result)
+
+        with self.env.d_env.get_ssh_to_remote(target_node_ip) as remote:
+            cmd = ("rm -f /etc/selinux/config; "
+                   "echo 'SELINUX=disabled\n"
+                   "SELINUXTYPE=targeted\n"
+                   "SETLOCALDEFS=0' > /etc/selinux/config")
+            result = remote.execute(cmd)
+            LOGGER.debug(result)
+
+        cmd = ("rsync -avz /etc/puppet/modules/* "
+               "{0}@{1}:/etc/puppet/modules/".format(settings.RHEL_IMAGE_USER,
+                                                     target_node_ip))
+        with self.env.d_env.get_ssh_to_remote(
+                self.env.get_admin_node_ip()) as remote:
+            remote.execute(cmd)
+            LOGGER.debug(cmd)
+
+        self.show_step(10)
+        self.apply_compute_puppet(target_node_ip)
+        self.fuel_web.assert_cluster_ready(os_conn, smiles_count=13)
+
+        self.show_step(11)
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['ha', 'smoke', 'sanity'])
+
+        self.env.make_snapshot("ready_ha_with_rhel_compute", is_make=True)
