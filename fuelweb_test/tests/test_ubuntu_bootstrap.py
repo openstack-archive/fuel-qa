ca15b0b25461eff906ffb341f43a2568660c66c4
Dmitry Kalashnik <dkalashnik@mirantis.com>
2015-12-01 11:56:50 +0300

    under construction. Ubuntu bootstrap
    Change-Id: Ie19178e2c7d7f636e9a2c94deff2054573e474b2
    Implements: blueprint dynamically-build-bootstrap-tests

--- fuelweb_test/tests/test_ubuntu_bootstrap.py
+++ fuelweb_test/tests/test_ubuntu_bootstrap.py
@@ -11,77 +11,302 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import tempfile
+import textwrap
 
 from devops.helpers.helpers import wait
-
 from proboscis import SkipTest
 from proboscis import test
-from proboscis.asserts import assert_true, assert_equal
+from proboscis.asserts import assert_true, assert_equal, assert_not_equal
 
+from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
-from fuelweb_test.helpers.utils import run_on_remote
+from fuelweb_test.helpers import fuel_actions
 from fuelweb_test import settings
 from fuelweb_test import logger
-from fuelweb_test.tests.base_test_case import SetupEnvironment
-from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
+from fuelweb_test.tests import base_test_case
 
 
 @test(groups=["bvt_ubuntu_bootstrap"])
-class UbuntuBootstrap(TestBasic):
-    # TODO documentation
-
-    # Verify version of bootstrap on slaves function
-    def verify_bootstrap_on_slaves(self, slaves):
-        logger.info("Verify bootstrap on slaves")
-        for slave in slaves:
-            with self.fuel_web.get_ssh_for_node(
-                    slave.name) as slave_remote:
-                cmd = 'cat /etc/*release'
-                output = run_on_remote(
-                    slave_remote, cmd)[0].lower()
-                assert_true(
-                    "ubuntu" in output,
-                    "Slave {0} doesn't use Ubuntu image for\
-                    bootstrap after Ubuntu images \
-                    were enabled".format(slave.name))
-
-    @test(depends_on=[SetupEnvironment.prepare_release],
-          groups=["prepare_ubuntu_bootstrap"])
+class UbuntuBootstrapBuild(base_test_case.TestBasic):
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
+          groups=["prepare_default_ubuntu_bootstrap"])
     @log_snapshot_after_test
-    def prepare_ubuntu_bootstrap(self):
+    def build_default_bootstrap(self):
         """Verify than slaves retrieved ubuntu bootstrap instead CentOS
 
         Scenario:
             1. Revert snapshot ready
-            2. Choose Ubuntu bootstrap on master node
+            2. Build and activate Ubuntu bootstrap with default settings
             3. Bootstrap slaves
-            4. Verify bootstrap on slaves
+            4. Verify Ubuntu bootstrap on slaves
 
         Duration 15m
-        Snapshot: prepare_ubuntu_bootstrap
         """
-
         self.env.revert_snapshot("ready")
 
-        # Run script on master node to change bootstrap to Ubuntu
         with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            uuid, bootstrap_location = fuel_bootstrap.build_bootstrap_image()
+            fuel_bootstrap.import_bootstrap_image(bootstrap_location)
+            fuel_bootstrap.activate_bootstrap_image(uuid, notify_webui=True)
 
-            cmd = 'fuel-bootstrap-image-set ubuntu'
-            run_on_remote(remote, cmd)
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02", "slave-03"])
+        self.env.bootstrap_nodes(nodes)
 
-            # Should be removed after Bug#1482242 will be fixed
-            cmd = 'dockerctl shell cobbler service dnsmasq restart'
-            run_on_remote(remote, cmd)
+        for node in nodes:
+            with self.fuel_web.get_ssh_for_node(node.name) as slave_remote:
+                checkers.verify_bootstrap_on_node(slave_remote,
+                                                  os_type="ubuntu",
+                                                  uuid=uuid)
 
-        self.env.bootstrap_nodes(
-            self.env.d_env.nodes().slaves[:3])
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
+          groups=["prepare_simple_ubuntu_bootstrap"])
+    @log_snapshot_after_test
+    def build_simple_bootstrap(self):
+        """Verify than slaves retrieved ubuntu bootstrap instead CentOS
 
-        # Verify version of bootstrap on slaves
-        self.verify_bootstrap_on_slaves(self.env.d_env.nodes().slaves[:3])
+        Scenario:
+            1. Revert snapshot ready
+            2. Build and activate Ubuntu bootstrap with extra package
+            3. Bootstrap slaves
+            4. Verify Ubuntu bootstrap on slaves
 
-        self.env.make_snapshot("prepare_ubuntu_bootstrap", is_make=True)
+        Duration 15m
+        """
+        self.env.revert_snapshot("ready")
 
-    @test(depends_on_groups=['prepare_ubuntu_bootstrap'],
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            bootstrap_default_params = \
+                fuel_bootstrap.get_bootstrap_default_config()
+
+            ubuntu_repo = bootstrap_default_params["ubuntu_repos"][0]["uri"]
+            mos_repo = bootstrap_default_params["mos_repos"][0]["uri"]
+
+            bootstrap_params = {
+                "ubuntu-release": "trusty",
+                "ubuntu-repo": "'{0} trusty'".format(ubuntu_repo),
+                "mos-repo": "'{0} mos8.0'".format(mos_repo),
+                "label": "UbuntuBootstrap",
+                "output-dir": "/tmp",
+                "package": ["ipython"]
+            }
+
+            uuid, bootstrap_location = \
+                fuel_bootstrap.build_bootstrap_image(**bootstrap_params)
+            fuel_bootstrap.import_bootstrap_image(bootstrap_location)
+            fuel_bootstrap.activate_bootstrap_image(uuid, notify_webui=True)
+
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02", "slave-03"])
+        self.env.bootstrap_nodes(nodes)
+
+        for node in nodes:
+            with self.fuel_web.get_ssh_for_node(node.name) as slave_remote:
+                checkers.verify_bootstrap_on_node(slave_remote,
+                                                  os_type="ubuntu",
+                                                  uuid=uuid)
+
+                ipython_version = checkers.get_package_versions_from_node(
+                    slave_remote, name="ipython", os_type="Ubuntu")
+                assert_not_equal(ipython_version, "")
+
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
+          groups=["prepare_full_ubuntu_bootstrap"])
+    @log_snapshot_after_test
+    def build_full_bootstrap(self):
+        """Verify than slaves retrieved ubuntu bootstrap instead CentOS
+
+        Scenario:
+            1. Revert snapshot ready
+            2. Build and activate Ubuntu bootstrap with extra settings
+            3. Bootstrap slaves
+            4. Verify Ubuntu bootstrap on slaves
+
+        Duration 15m
+        """
+        self.env.revert_snapshot("ready")
+
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            bootstrap_default_params = \
+                fuel_bootstrap.get_bootstrap_default_config()
+
+            ubuntu_repo = bootstrap_default_params["ubuntu_repos"][0]["uri"]
+            mos_repo = bootstrap_default_params["mos_repos"][0]["uri"]
+
+            bootstrap_script = '''\
+                #!/bin/bash
+
+                echo "testdata" > /test_bootstrap_script
+                apt-get install ipython -y'''
+
+            with tempfile.NamedTemporaryFile() as temp_file:
+                temp_file.write(textwrap.dedent(bootstrap_script))
+                temp_file.flush()
+                remote.mkdir("/root/bin")
+                remote.upload(temp_file.name, "/root/bin/bootstrap_script.sh")
+
+            remote.mkdir("/root/inject/var/lib/testdir")
+            remote.mkdir("/root/inject/var/www/testdir2")
+
+            kernel_cmdline = ["biosdevname=0", "net.ifnames=1", "debug",
+                              "ignore_loglevel", "log_buf_len=10M"]
+
+            bootstrap_params = {
+                "ubuntu-release": "trusty",
+                "ubuntu-repo": "'{0} trusty'".format(ubuntu_repo),
+                "mos-repo": "'{0} mos8.0'".format(mos_repo),
+                # "http-proxy": "",
+                # "https-proxy": "",
+                # "direct-repo-addr": "",
+                "script": "/root/bin/bootstrap_script.sh",
+                "include-kernel-module": "core",
+                "blacklist-kernel-module": "fuse",
+                "label": "UbuntuBootstrap",
+                "inject-files-from": "/root/include/",
+                "extend-kopts": "'{0}'".format(" ".join(kernel_cmdline)),
+                # "kernel-flavor": "",
+                "output-dir": "/tmp",
+                # "repo": [],
+                "package": ["fuse", "sshfs"],
+            }
+
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            uuid, bootstrap_location = \
+                fuel_bootstrap.build_bootstrap_image(**bootstrap_params)
+            fuel_bootstrap.import_bootstrap_image(bootstrap_location)
+            fuel_bootstrap.activate_bootstrap_image(uuid, notify_webui=True)
+
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02", "slave-03"])
+        self.env.bootstrap_nodes(nodes)
+
+        for node in nodes:
+            with self.fuel_web.get_ssh_for_node(node.name) as slave_remote:
+                checkers.verify_bootstrap_on_node(slave_remote,
+                                                  os_type="ubuntu",
+                                                  uuid=uuid)
+
+                for package in ['ipython', 'fuse', 'sshfs']:
+                    package_version = checkers.get_package_versions_from_node(
+                        slave_remote, name=package, os_type="Ubuntu")
+                    assert_not_equal(package_version, "",
+                                     "Package {0} is not installed on slave "
+                                     "{1}".format(package, node.name))
+
+                for injected_dir in ["/var/lib/testdir", "/var/www/testdir2"]:
+                    checkers.check_file_exists(slave_remote, injected_dir)
+
+                file_content = remote.execute("cat /test_bootstrap_script")
+                assert_equal("".join(file_content["stdout"]), "testdata",
+                             "REPLACEME")
+
+                actual_kernel_cmdline = \
+                    "".join(remote.execute("cat /proc/cmdline")["stdout"])
+                for kernel_opt in kernel_cmdline:
+                    assert_true(kernel_opt in actual_kernel_cmdline,
+                                "REPLACEME")
+
+                modules_loaded = remote.execute("lsmod")
+                core_mod_loaded = any(["core" in line
+                                       for line in modules_loaded["stdout"]])
+                logger.warning("Core module is {0}loaded, loadad modules: {1}"
+                               .format("" if core_mod_loaded else "not ",
+                                       "\n".join(modules_loaded)))
+
+                fuse_blacklisted = remote.execute(
+                    "grep -r fuse /etc/modprobe.d/*blacklist*")["exit_code"]
+                logger.warning("{0}".format(fuse_blacklisted == 0))
+
+    @test(depends_on_groups=["prepare_default_ubuntu_bootstrap"],
+          groups=[""])
+    @log_snapshot_after_test
+    def create_list_import_delete_bootstrap_image(self):
+        self.env.revert_snapshot("prepare_default_ubuntu_bootstrap")
+
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            bootstrap_location, uuid = \
+                fuel_bootstrap.build_bootstrap_image()
+            fuel_bootstrap.import_bootstrap_image(bootstrap_location)
+
+            assert_true(uuid in fuel_bootstrap.list_bootstrap_images(), "")
+            assert_equal(3, fuel_bootstrap.list_bootstrap_images_uuids(), "")
+
+            fuel_bootstrap.delete_bootstrap_image(uuid)
+            assert_true(uuid not in fuel_bootstrap.list_bootstrap_images())
+            assert_equal(2, fuel_bootstrap.list_bootstrap_images_uuids())
+
+            uuid = fuel_bootstrap.list_bootstrap_images()
+            assert_true("error" in fuel_bootstrap.delete_bootstrap_image(uuid))
+
+
+@test(groups=["bvt_ubuntu_bootstrap"])
+class UbuntuBootstrap(base_test_case.TestBasic):
+    @test(depends_on=[base_test_case.SetupEnvironment.ready],
+          groups=["deploy_with_two_ubuntu_bootstraps"])
+    @log_snapshot_after_test
+    def deploy_with_two_ubuntu_bootstraps(self):
+        if not self.env.revert_snapshot('ready'):
+            raise SkipTest()
+
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            uuid = fuel_bootstrap.get_active_bootstrap_uuid()
+
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02"])
+
+        self.env.bootstrap_nodes(nodes)
+        for node in nodes:
+            with self.fuel_web.get_ssh_for_node(node.name) as slave_remote:
+                checkers.verify_bootstrap_on_node(slave_remote,
+                                                  os_type="ubuntu",
+                                                  uuid=uuid)
+
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            _, new_uuid = fuel_bootstrap.build_bootstrap_image(activate=True)
+
+        new_node = self.env.d_env.get_node(name="slave-03")
+        self.env.bootstrap_nodes([new_node])
+        with self.fuel_web.get_ssh_for_node(new_node.name) as slave_remote:
+            checkers.verify_bootstrap_on_node(slave_remote,
+                                              os_type="ubuntu",
+                                              uuid=new_uuid)
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE,
+            settings={
+                'tenant': 'stop_deploy',
+                'user': 'stop_deploy',
+                'password': 'stop_deploy',
+            }
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+                'slave-03': ['cinder']
+            }
+        )
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        assert_equal(
+            3, len(self.fuel_web.client.list_cluster_nodes(cluster_id)))
+
+        # Run ostf
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['smoke'])
+
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves_3],
           groups=["deploy_stop_on_deploying_ubuntu_bootstrap"])
     @log_snapshot_after_test
     def deploy_stop_on_deploying_ubuntu_bootstrap(self):
@@ -104,7 +329,7 @@ class UbuntuBootstrap(TestBasic):
         Snapshot: deploy_stop_on_deploying_ubuntu_bootstrap
         """
 
-        if not self.env.revert_snapshot('prepare_ubuntu_bootstrap'):
+        if not self.env.revert_snapshot('ready_with_3_slaves'):
             raise SkipTest()
 
         cluster_id = self.fuel_web.create_cluster(
@@ -128,11 +353,28 @@ class UbuntuBootstrap(TestBasic):
 
         # Deploy cluster and stop deployment, then verify bootstrap on slaves
         self.fuel_web.provisioning_cluster_wait(cluster_id)
+
+        # Build another bootstrap image
+        with self.env.d_env.get_admin_remote() as remote:
+            fuel_bootstrap = fuel_actions.FuelBootstrapCliActions(remote)
+            _, uuid = \
+                fuel_bootstrap.build_bootstrap_image(activate=True)
+
         self.fuel_web.deploy_task_wait(cluster_id=cluster_id, progress=10)
         self.fuel_web.stop_deployment_wait(cluster_id)
+
         self.fuel_web.wait_nodes_get_online_state(
-            self.env.d_env.nodes().slaves[:2], timeout=10 * 60)
-        self.verify_bootstrap_on_slaves(self.env.d_env.nodes().slaves[:3])
+            self.env.d_env.get_nodes(name__in=['slave-01', 'slave-02']),
+            timeout=10 * 60)
+
+        # Verify new bootstrap image
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02", "slave-03"])
+        for node in nodes:
+            with self.fuel_web.get_ssh_for_node(node.name) as slave_remote:
+                checkers.verify_bootstrap_on_node(slave_remote,
+                                                  os_type="ubuntu",
+                                                  uuid=uuid)
 
         # Network verification
         self.fuel_web.verify_network(cluster_id)
@@ -182,9 +424,13 @@ class UbuntuBootstrap(TestBasic):
         # Reset environment,
         # then verify bootstrap on slaves and re-deploy cluster
         self.fuel_web.stop_reset_env_wait(cluster_id)
-        self.fuel_web.wait_nodes_get_online_state(
-            self.env.d_env.nodes().slaves[:3], timeout=10 * 60)
-        self.verify_bootstrap_on_slaves(self.env.d_env.nodes().slaves[:3])
+
+        nodes = self.env.d_env.get_nodes(
+            name__in=["slave-01", "slave-02", "slave-03"])
+
+        self.fuel_web.wait_nodes_get_online_state(nodes, timeout=10 * 60)
+        for node in nodes:
+            self.verify_bootstrap_on_node(node)
 
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
@@ -225,8 +471,50 @@ class UbuntuBootstrap(TestBasic):
         wait(lambda: len(self.fuel_web.client.list_nodes()) == 3,
              timeout=10 * 60)
 
-        self.verify_bootstrap_on_slaves(self.env.d_env.nodes().slaves[:3])
+        for node in self.env.d_env.get_nodes(
+                name__in=['slave-01', 'slave-02', 'slave-03']):
+            self.verify_bootstrap_on_node(node)
 
         self.env.make_snapshot(
             "delete_on_ready_ubuntu_bootstrap",
             is_make=True)
+
+    @test(depends_on_groups=['deploy_stop_on_deploying_ubuntu_bootstrap'],
+          groups=["delete_node_on_ready_ubuntu_bootstrap"])
+    @log_snapshot_after_test
+    def delete_node_on_ready_ubuntu_bootstrap(self):
+        """Delete node from cluster in HA mode\
+        with 1 controller on Ubuntu Bootstrap
+
+        Scenario:
+            1. Delete node
+            2. Verify bootstrap on slaves
+
+        Duration 30m
+        Snapshot: delete_on_ready_ubuntu_bootstrap
+        """
+        if not self.env.revert_snapshot(
+                'deploy_stop_on_deploying_ubuntu_bootstrap'):
+            raise SkipTest()
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        # Delete cluster, then verify bootstrap on slaves
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-03': ['cinder']
+            },
+            pending_addition=False,
+            pending_deletion=True
+        )
+
+        self.fuel_web.run_network_verify(cluster_id)
+        self.fuel_web.deploy_cluster_wait(cluster_id, check_services=False)
+
+        # wait for nodes to appear after bootstrap
+        wait(lambda: len(self.fuel_web.client.list_nodes()) == 3,
+             timeout=10 * 60)
+
+        self.verify_bootstrap_on_node(
+            self.env.d_env.get_node(name="slave-03"))
