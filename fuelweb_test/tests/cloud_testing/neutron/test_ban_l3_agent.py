9543506ec88a0407ede12ac970dbd0bd3f065274
Yury Tregubov <ytregubov@mirantis.com>
2015-12-07 07:52:28 +0000

    Networking automated tests
    Author: Tregubov Yury
    Added tests for controller restart nad shutdown in scope of  neutron networking automation.
    Accidentely some letters were converted to lowercase in new tets. Fixed
    in patchset2.
    
    Change-Id: I17e88ead1a359e32a8a6cac497b6da9df14c3ce3

--- fuelweb_test/tests/cloud_testing/neutron/test_ban_l3_agent.py
+++ fuelweb_test/tests/cloud_testing/neutron/test_ban_l3_agent.py
@@ -142,3 +142,268 @@ class TestL3AgentVxlan(base.TestNeutronBase):
         """
         self.env.revert_snapshot("deploy_ha_neutron_tun")
         self.check_ban_one_l3_agent()
+
+
+@test(groups=['networking', 'networking_vlan'])
+class TestControllerRestartVlan(base.TestNeutronBase):
+    """test l3 agent migration on failure"""
+
+    segment_type = settings.NEUTRON_SEGMENT['vlan']
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVlan.deploy_ha_neutron_vlan],
+        groups=['shutdown_primary_controller_check_l3_agt_vlan'])
+    @log_snapshot_after_test
+    def shut_down_primary_controller_check_l3_agt(self):
+        """
+        scenario:
+
+            1. create network1, subnet1, router1
+            2. create network2, subnet2, router2
+            3. launch 2 instances (vm1 and vm2) and associate floating ips
+            4. add rules for ping
+            5. find primary controller, run command on controllers:
+                hiera role
+            6. check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. if there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. destroy primary controller
+                virsh destroy <primary_controller>
+            11. wait some time until all agents are up
+                neutron-agent-list
+            12. check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        duration 20m
+        snapshot deploy_ha_neutron_vlan
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_vlan")
+        self.check_prime_controller_shutdown()
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVlan.deploy_ha_neutron_vlan],
+        groups=['restart_primary_controller_check_l3_agt_vlan'])
+    @log_snapshot_after_test
+    def restart_primary_controller_check_l3_agt(self):
+        """
+        scenario:
+
+            1. create network1, subnet1, router1
+            2. create network2, subnet2, router2
+            3. launch 2 instances (vm1 and vm2) and associate floating ips
+            4. add rules for ping
+            5. find primary controller, run command on controllers:
+                hiera role
+            6. check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. if there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. restart primary controller
+                reboot on <primary_controller>
+            11. wait some time until all agents are up
+                neutron-agent-list
+            12. check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        duration 20m
+        snapshot deploy_ha_neutron_vlan
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_vlan")
+        self.check_prime_controller_restart()
+
+
+@test(groups=['networking', 'networking_gre'])
+class TestControllerRestartGre(base.TestNeutronBase):
+    """test l3 agent migration on failure"""
+
+    segment_type = settings.NEUTRON_SEGMENT['gre']
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVxlan.deploy_ha_neutron_vxlan],
+        groups=['shutdown_primary_controller_check_l3_agt_gre'])
+    @log_snapshot_after_test
+    def shut_down_primary_controller_check_l3_agt(self):
+        """
+        scenario:
+
+            1. create network1, subnet1, router1
+            2. create network2, subnet2, router2
+            3. launch 2 instances (vm1 and vm2) and associate floating ips
+            4. add rules for ping
+            5. find primary controller, run command on controllers:
+                hiera role
+            6. check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. if there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. destroy primary controller
+                virsh destroy <primary_controller>
+            11. wait some time until all agents are up
+                neutron-agent-list
+            12. check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        duration 20m
+        snapshot deploy_ha_neutron_gre
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_gre")
+        self.check_prime_controller_shutdown()
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVxlan.deploy_ha_neutron_vxlan],
+        groups=['restart_primary_controller_check_l3_agt_gre'])
+    @log_snapshot_after_test
+    def restart_primary_controller_check_l3_agt(self):
+        """
+        scenario:
+
+            1. create network1, subnet1, router1
+            2. create network2, subnet2, router2
+            3. launch 2 instances (vm1 and vm2) and associate floating ips
+            4. add rules for ping
+            5. find primary controller, run command on controllers:
+                hiera role
+            6. check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. if there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. restart primary controller
+                reboot on <primary_controller>
+            11. wait some time until all agents are up
+                neutron-agent-list
+            12. check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        duration 20m
+        snapshot deploy_ha_neutron_gre
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_gre")
+        self.check_prime_controller_restart()
+
+
+@test(groups=['networking', 'networking_vxlan'])
+class TestControllerRestartVxlan(base.TestNeutronBase):
+    """Test L3 agent migration on failure"""
+
+    segment_type = settings.NEUTRON_SEGMENT['tun']
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVxlan.deploy_ha_neutron_vxlan],
+        groups=['shutdown_primary_controller_check_l3_agt_vxlan'])
+    @log_snapshot_after_test
+    def shut_down_primary_controller_check_l3_agt(self):
+        """
+        Scenario:
+
+            1. Create network1, subnet1, router1
+            2. Create network2, subnet2, router2
+            3. Launch 2 instances (vm1 and vm2) and associate floating ips
+            4. Add rules for ping
+            5. Find primary controller, run command on controllers:
+                hiera role
+            6. Check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. If there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. Destroy primary controller
+                virsh destroy <primary_controller>
+            11. Wait some time until all agents are up
+                neutron-agent-list
+            12. Check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. Boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        Duration 20m
+        Snapshot deploy_ha_neutron_tun
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_tun")
+        self.check_prime_controller_shutdown()
+
+    @test(
+        depends_on=[
+            test_neutron.TestNeutronFailoverVxlan.deploy_ha_neutron_vxlan],
+        groups=['restart_primary_controller_check_l3_agt_vxlan'])
+    @log_snapshot_after_test
+    def restart_primary_controller_check_l3_agt(self):
+        """
+        Precondition:
+            Cluster is deployed in HA mode
+            Neutron with VLAN segmentation set up
+
+        Scenario:
+
+            1. Create network1, subnet1, router1
+            2. Create network2, subnet2, router2
+            3. Launch 2 instances (vm1 and vm2) and associate floating ips
+            4. Add rules for ping
+            5. Find primary controller, run command on controllers:
+                hiera role
+            6. Check on what agents is router1:
+                neutron l3-agent-list-hosting-router router1
+            7. If there isn't agent on the primary controller:
+                neutron l3-agent-router-remove non_on_primary_agent_id router1
+                neutron l3-agent-router-add on_primary_agent_id router1
+            8. ping 8.8.8.8 from vm2
+            9. ping vm1 from vm2 and vm1 from vm2
+            10. Restart primary controller
+                reboot on <primary_controller>
+            11. Wait some time until all agents are up
+                neutron-agent-list
+            12. Check that all routers reschedule from primary controller:
+                neutron router-list-on-l3-agent <on_primary_agent_id>
+            13. Boot vm3 in network1
+            14. ping 8.8.8.8 from vm3
+            15. ping between vm1 and vm3 by internal ip
+            16. ping between vm1 and vm2 by floating ip
+
+        Duration 20m
+        Snapshot deploy_ha_neutron_tun
+
+        """
+        self.env.revert_snapshot("deploy_ha_neutron_tun")
+        self.check_prime_controller_restart()
