2b85fd38d6e2ddd2888179bdaf228f4777847cca
Tatyana Leontovich <tleontovich@mirantis.com>
2015-12-09 19:31:07 +0200

    Add package version check
    To avoid false positive result in gate fuel library test add nect assertion:
    
    * We have packages that should be updated - if not raise error
    * Packages from review is higer version that installed - if not raise error
    
    Change-Id: I065bef8f73d2abdb1b3a905a2720a2db5277e4fc
    Closes-Bug: #1523466

--- gates_tests/helpers/utils.py
+++ gates_tests/helpers/utils.py
@@ -12,7 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
 from proboscis.asserts import assert_equal
 
 from fuelweb_test.helpers import checkers
