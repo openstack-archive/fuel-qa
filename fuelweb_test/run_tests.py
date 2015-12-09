6572c1cdb3a4382f6007f80029c6cb2d1b54364f
Egor Kotko <ykotko@mirantis.com>
2015-12-09 14:50:44 +0100

    Update decorator "update_ostf"
    - make updating of ostf-server code via package
    not via the patch
    - add test gate_ostf_update
    
    Next variables should be exported:
    UPDATE_FUEL=True
    UPDATE_FUEL_PATH=/path/to/dir/with/package
    
    Change-Id: I53a768dfb4f755ab1b59db822ce571e00a0b2cb0
    Closes-Bug: #1517487

--- fuelweb_test/run_tests.py
+++ fuelweb_test/run_tests.py
@@ -142,6 +142,7 @@ def import_tests():
     from gates_tests.tests import test_review_in_fuel_agent  # noqa
     from tests.tests_strength import test_load  # noqa
     from tests import test_services_reconfiguration  # noqa
+    from gates_tests.tests import test_review_in_ostf  # noqa
 
 
 def run_tests():
