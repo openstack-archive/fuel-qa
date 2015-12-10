044ad2df782cca4074f353bd0a3a4085c6201535
Alexandr Kostrikov <akostrikov@mirantis.com>
2015-12-10 15:33:59 +0300

    [wip] Test mirror. Try on CI.
    Change-Id: I5f8b0af504ebba800bd244c16a893654cb217b02

--- fuelweb_test/tests/tests_mirrors/test_use_mirror.py
+++ fuelweb_test/tests/tests_mirrors/test_use_mirror.py
@@ -16,12 +16,17 @@ from proboscis import test
 from proboscis import SkipTest
 
 from fuelweb_test.settings import DEPLOYMENT_MODE
+from fuelweb_test.settings import MIRROR_UBUNTU
 from fuelweb_test.settings import NEUTRON_SEGMENT
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test.helpers.utils import run_on_remote
 
 
+# TODO ssh manager
+# TODO log file
+# TODO remove and use default instead of 'deb http://mirror.seed-cz1.fuel-infra.org/pkgs/ubuntu-2016-01-14-170104 trusty main universe multiverse'?
 @test(groups=['fuel-mirror'])
 class TestUseMirror(TestBasic):
     """Tests custom mirrors to deploy environment.
@@ -35,8 +40,10 @@ class TestUseMirror(TestBasic):
     that is not reflected in our mirror.
     """
 
-    @test(groups=['fuel-mirror', 'use-mirror'],
+    @test(groups=['fuel-mirror', 'use-mirror',
+                  'deploy_multiple_services_local_mirror'],
           depends_on=[SetupEnvironment.prepare_slaves_5])
+    @log_snapshot_after_test
     def deploy_with_custom_mirror(self):
         """Create mirror for deployment without internet dependencies.
 
@@ -44,21 +51,18 @@ class TestUseMirror(TestBasic):
         Good package covers steps from 1 to 7.
 
         Scenario:
-            1. Install packages to build fuel-mirror and packetary
-            2. Download packetary from github
-            3. Checkout to packetary3 version of packetary
-            4. Install packetary and fuel-mirror
-            5. Create configuration directory
-            6. Copy configuration file to configuration directory
-            7. Update config file with real master ip.
-            8. Run create mirror command
-            9. Create cluster with neutron networking
-            10. Add 3 nodes with controller role
-            11. Add 1 node with compute role and 1 node with cinder role
-            12. Run network verification
-            13. Deploy the cluster
-            14. Run OSTF
-            15. Create snapshot
+            1. Verify that tool to create mirrors present
+            2. Copy configuration file to configuration directory
+            3. Update config file with real master ip.
+            4. Run create mirror command
+            5. Apply mirror changes on
+            6. Create cluster with neutron networking
+            7. Add 3 nodes with controller role
+            8. Add 1 node with compute role and 1 node with cinder role
+            9. Run network verification
+            10. Deploy the cluster
+            11. Run OSTF
+            12. Create snapshot
 
         Duration 90m
         Snapshot deploy_with_custom_mirror
@@ -66,44 +70,23 @@ class TestUseMirror(TestBasic):
         self.env.revert_snapshot('ready_with_5_slaves')
 
         with self.env.d_env.get_admin_remote() as remote:
-            # FIXME(akostrikov) This should be removed with correct install.
-            # All that above is a hack.
-            self.show_step(1, initialize=True)
-            run_on_remote(remote,
-                          'yum install git python-lxml.x86_64 '
-                          'python-eventlet -y')
+            self.show_step(1)
             self.show_step(2)
-            run_on_remote(remote,
-                          'cd /opt && rm -rf packetary && '
-                          'git clone https://github.com/bgaifullin/packetary')
             self.show_step(3)
-            run_on_remote(remote,
-                          'cd /opt/packetary && git checkout packetary3')
             self.show_step(4)
-            run_on_remote(remote, 'cd /opt/packetary && pip install -e .')
-            run_on_remote(remote,
-                          'cd /opt/packetary/contrib/fuel_mirror/ && '
-                          'pip install -e .')
+            if MIRROR_UBUNTU != '':
+                ubuntu_url = MIRROR_UBUNTU.split()[1]
+                replace_cmd = "sed -i 's,http://archive.ubuntu.com/ubuntu,{0},g' /usr/share/fuel-mirror/ubuntu.yaml".format(ubuntu_url)
+                run_on_remote(remote, replace_cmd)
+            run_on_remote(remote, """sed -i '/     - "debconf-utils"/a\     - "debconf"' /usr/share/fuel-mirror/ubuntu.yaml""")
+            run_on_remote(remote, 'rm -rf /var/www/nailgun/2015.1.0-8.0/ubuntu/x86_64/')
+            run_on_remote(remote, 'rm -rf /var/www/nailgun/2015.1.0-8.0/centos/x86_64/')
+            run_on_remote(remote, 'rm -rf /var/www/nailgun/2015.1.0-8.0/mos-centos/')
+            run_on_remote(remote, 'fuel-mirror create -P ubuntu -G mos ubuntu')
             self.show_step(5)
-            run_on_remote(remote, 'mkdir -p /etc/fuel-mirror/')
-            self.show_step(6)
             run_on_remote(remote,
-                          'cp /opt/packetary/contrib/fuel_mirror/'
-                          'etc/config.yaml /etc/fuel-mirror/config.yaml')
-            self.show_step(7)
-            admin_ip = str(
-                self.env.d_env.nodes().admin.get_ip_address_by_network_name(
-                    'admin'))
-            cmd = "sed -r 's/{prev_ip}'/{admin_ip}/ -i'' {config_path}".format(
-                prev_ip='10.20.0.2',
-                admin_ip=admin_ip,
-                config_path='/etc/fuel-mirror/config.yaml'
-            )
-            run_on_remote(remote, cmd)
-            self.show_step(8)
-            run_on_remote(remote, 'fuel-mirror create --ubuntu')
-
-        self.show_step(9)
+                          'fuel-mirror apply -P ubuntu -G mos ubuntu --default')
+        self.show_step(6)
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
@@ -116,8 +99,9 @@ class TestUseMirror(TestBasic):
                 'password': 'packetary'
             }
         )
-        self.show_step(10)
-        self.show_step(11)
+        self.show_step(7)
+        self.show_step(8)
+
         self.fuel_web.update_nodes(
             cluster_id,
             {
@@ -128,16 +112,16 @@ class TestUseMirror(TestBasic):
                 'slave-05': ['cinder']
             }
         )
-        self.show_step(12)
+        self.show_step(9)
         self.fuel_web.verify_network(cluster_id)
-        self.show_step(13)
+        self.show_step(10)
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        self.show_step(14)
+        self.show_step(11)
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
             test_sets=['ha', 'smoke', 'sanity'])
-
+        self.show_step(12)
         self.env.make_snapshot('deploy_with_custom_mirror')
 
     @test(groups=['fuel-mirror', 'use-mirror'])
