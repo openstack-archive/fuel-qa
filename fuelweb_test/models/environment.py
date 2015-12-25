203657a81ebe4e53de0b9dafa12faa1f71b5f4dc
Ivan Suzdal <isuzdal@mirantis.com>
2015-12-25 16:06:12 +0300

    Disable gso/gro/tso on admin_node
    Change-Id: If20f3acb7961ee4c8c6fde19653cbe9854d81583

--- fuelweb_test/models/environment.py
+++ fuelweb_test/models/environment.py
@@ -291,6 +291,8 @@ class EnvironmentModel(object):
             logger.info('Admin node started second time.')
             self.d_env.nodes().admin.await(self.d_env.admin_net)
             self.set_admin_ssh_password()
+            if MASTER_IS_CENTOS7:
+                self.disable_offloading_on_admin_node()
             self.docker_actions.wait_for_ready_containers(timeout=600)
 
             # set collector address in case of admin node destroy
@@ -306,6 +308,8 @@ class EnvironmentModel(object):
                     settings.FUEL_STATS_HOST, settings.FUEL_STATS_PORT
                 ))
         self.set_admin_ssh_password()
+        if MASTER_IS_CENTOS7:
+            self.disable_offloading_on_admin_node()
         self.docker_actions.wait_for_ready_containers()
 
     def make_snapshot(self, snapshot_name, description="", is_make=False):
@@ -405,6 +409,12 @@ class EnvironmentModel(object):
                 .format(settings.KEYSTONE_CREDS['username'],
                         settings.KEYSTONE_CREDS['password']))
 
+    def disable_offloading_on_admin_node(self, iface='enp0s3'):
+        logger.info("Disabling offloading on admin node")
+        cmd = 'ethtool -K {0} gso off gro off tso off'.format(iface)
+        with self.d_env.get_admin_remote() as remote:
+            self.execute_remote_cmd(remote, cmd)
+
     def setup_environment(self, custom=settings.CUSTOM_ENV,
                           build_images=settings.BUILD_IMAGES,
                           iso_connect_as=settings.ADMIN_BOOT_DEVICE,
@@ -427,6 +437,8 @@ class EnvironmentModel(object):
                                       iso_connect_as=iso_connect_as))
         self.wait_for_provisioning()
         self.set_admin_ssh_password()
+        if MASTER_IS_CENTOS7:
+            self.disable_offloading_on_admin_node()
         self.wait_for_external_config()
         if custom:
             self.setup_customisation()
