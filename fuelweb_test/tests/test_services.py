210069d55b37f4bdadcc94953e4f2ed9f2a58bfd
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2015-12-29 10:55:26 +0300

    Fixed Ceilometer and Heat SWARM tests
    We need to fix checks for ceilometer-api processes
    because we shouldn't hardcode count of processes in
    automated tests.
    
    Co-Authored-By: Vitaly Gusev <vgusev@mirantis.com>
    
    Change-Id: Id3117459e273fcb86a44fe3972e9ede0b8d43354
    Closes-Bug: #1529072

--- fuelweb_test/tests/test_services.py
+++ fuelweb_test/tests/test_services.py
@@ -551,9 +551,9 @@ class CeilometerHAOneControllerMongo(OSTFCeilometerHelper):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-03")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
@@ -608,9 +608,9 @@ class CeilometerHAOneControllerMongo(OSTFCeilometerHelper):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_one_controller_multirole")
@@ -666,9 +666,9 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id,
                        skip_tests=['test_check_volume_notifications'])
@@ -718,9 +718,9 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_multirole", is_make=True)
@@ -813,9 +813,9 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_with_external_mongo")
@@ -883,9 +883,9 @@ class HeatHAOneController(TestBasic):
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         with self.env.d_env.get_ssh_to_remote(_ip) as remote:
-            checkers.verify_service(
-                remote,
-                service_name='ceilometer-api')
+            checkers.verify_service(remote,
+                                    service_name='ceilometer-api',
+                                    ignore_count_of_proccesses=True)
 
         LOGGER.debug('Run Heat OSTF platform tests')
 
@@ -975,9 +975,9 @@ class HeatHA(TestBasic):
                 checkers.verify_service(
                     remote,
                     service_name='heat-api', count=3)
-                checkers.verify_service(
-                    remote,
-                    service_name='ceilometer-api')
+                checkers.verify_service(remote,
+                                        service_name='ceilometer-api',
+                                        ignore_count_of_proccesses=True)
 
         LOGGER.debug('Run Heat OSTF platform tests')
 
