6451b36c1e92d1fb1cce7c59474d8dc09c321faa
Artem Grechanichenko <agrechanichenko@mirantis.com>
2016-01-11 17:15:20 +0200

    Additional variable in fuel-agent ci gate test
    Using FUEL_AGENT_REPO_PATH to having access
    to fuel-agent review code in patch_and_assemble_ubuntu_bootstrap
    and patch_centos_boostrap methods
    
    Change-Id: If436e3fa7ace8aa5d607b06d5566a543d23ac30e

--- gates_tests/helpers/utils.py
+++ gates_tests/helpers/utils.py
@@ -89,7 +89,7 @@ def patch_centos_bootstrap(environment):
     try:
         pack_path = '/var/www/nailgun/fuel-agent-review/'
         with environment.d_env.get_admin_remote() as remote:
-            remote.upload(settings.UPDATE_FUEL_PATH.rstrip('/'),
+            remote.upload(settings.FUEL_AGENT_REPO_PATH.rstrip('/'),
                           pack_path)
             # renew code in bootstrap
 
@@ -116,7 +116,7 @@ def patch_centos_bootstrap(environment):
                 "gzip -9 > /var/initramfs.img.updated")
 
             cmd = ("rm -rf {0}/initramfs.img;"
-                   "rsync -r {2}fuel-agent/fuel_agent/* {0}{1}/;"
+                   "rsync -r {2}fuel_agent/* {0}{1}/;"
                    "cd {0}/;"
                    "{3};"
                    ).format(
@@ -145,7 +145,7 @@ def patch_and_assemble_ubuntu_bootstrap(environment):
     try:
         pack_path = '/var/www/nailgun/fuel-agent-review/'
         with environment.d_env.get_admin_remote() as remote:
-            remote.upload(settings.UPDATE_FUEL_PATH.rstrip('/'),
+            remote.upload(settings.FUEL_AGENT_REPO_PATH.rstrip('/'),
                           pack_path)
             # renew code in bootstrap
 
@@ -170,7 +170,7 @@ def patch_and_assemble_ubuntu_bootstrap(environment):
             # Step 3 - replace fuel-agent code in unpacked bootstrap
             agent_path = "/usr/lib/python2.7/dist-packages/fuel_agent"
             bootstrap_file = bootstrap + "/root.squashfs"
-            cmd = ("rsync -r {2}fuel-agent/fuel_agent/* {0}{1}/;"
+            cmd = ("rsync -r {2}fuel_agent/* {0}{1}/;"
                    "mv {3} /var/root.squashfs.old;"
                    ).format(
                 bootstrap_var,
