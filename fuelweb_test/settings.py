f13b130387bf134aa96d22366af2f0ee09d45cd2
Alexander Kurenyshev <akurenyshev@mirantis.com>
2016-01-29 13:52:01 +0300

    Fix adds insecure option to the call to MOS
    We couldn't use public vip ip before start deploying cluster.
    So, we need to add option to skip SSL cert verification
    when connecting to the MOS services
    
    Closes-Bug:1537833
    
    Change-Id: I53621022a3ae55cb30ab224800fc812ce2b0a79b

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -54,7 +54,9 @@ ADMIN_BOOT_DEVICE = os.environ.get('ADMIN_BOOT_DEVICE', 'cdrom')
 DNS = os.environ.get('DNS', '8.8.8.8')
 PUBLIC_TEST_IP = os.environ.get('PUBLIC_TEST_IP', '8.8.8.8')
 
-DISABLE_SSL = get_var_as_bool('DISABLE_SSL', False)
+DISABLE_SSL = get_var_as_bool('DISABLE_SSL', True)
+VERIFY_SSL = get_var_as_bool('VERIFY_SSL', False)
+SSL_CN = os.environ.get('SSL_CN', 'public.fuel.local')
 SSL_CERTS_DIR = os.environ.get('SSL_CERTS_DIR', os.getcwd())
 if not os.path.exists(SSL_CERTS_DIR):
     os.makedirs(SSL_CERTS_DIR)
