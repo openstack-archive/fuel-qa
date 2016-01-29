4f2f419aa6ad6dd7fac1a1c2be92ac7cd3959572
Dmitry Teselkin <dteselkin@mirantis.com>
2016-01-29 18:33:02 +0300

    [WIP] Run distro-sync during master node setup
    Based on https://review.openstack.org/#/c/274110
    
    Change-Id: Ic905a62deae811e0aa93bff0713b34683e3f0cdc

--- fuelweb_test/models/environment.py
+++ fuelweb_test/models/environment.py
@@ -409,6 +409,22 @@ class EnvironmentModel(object):
                   " -regex '.*/mos[0-9,\.]+\-(updates|security).repo' | " \
                   "xargs -n1 -i sed '$aenabled=0' -i {}"
             self.execute_remote_cmd(remote, cmd)
+        if settings.DO_DISTRO_SYNC:
+            logger.info('Entering distro-sync')
+            self.wait_for_puppet_finished()
+            remote = self.d_env.get_admin_remote()
+            git_url = "git://172.18.10.105/mos7-centos6-update.git"
+            cmd = "yum install -y git && " \
+                  "tmpdir=$(mktemp -d) && cd $tmpdir && " \
+                  "git clone %s . && " \
+                  "/bin/bash ./update-master-node.sh" % git_url
+            logger.info("Executing distro-sync sequence '{}'".format(
+                cmd
+            ))
+            self.execute_remote_cmd(remote, cmd)
+            self.reboot_master_node()
+            self.wait_for_puppet_finished()
+            logger.info('Exiting distro-sync')
 
     @update_rpm_packages
     @upload_manifests
@@ -417,6 +433,56 @@ class EnvironmentModel(object):
             self.d_env.nodes(
             ).admin.get_ip_address_by_network_name
             (self.d_env.admin_net), 22), timeout=7 * 60)
+
+    def wait_for_puppet_finished(self, timeout=120):
+        self.wait_for_provisioning()
+        try:
+            remote = self.d_env.get_admin_remote()
+            cmd = 'c=0; ' \
+                  'while test $c -gt 3; do ' \
+                  '(pkill -0 puppet && c=$((c + 1))); ' \
+                  'sleep 1; ' \
+                  'done'
+            logger.info('Waiting for puppet finishes to run ...')
+            wait(lambda: remote.execute(cmd)['exit_code'] == 0,
+                 timeout=timeout)
+        except Exception:
+            logger.error('Failed to wait for master node'
+                         ' readiness after reboot')
+            raise
+
+    def reboot_master_node(self, timeout=600):
+        last_uptime = self.get_master_node_uptime()
+        logger.info('Rebooting master node, uptime %s ...' % last_uptime)
+        remote = self.d_env.get_admin_remote()
+        remote.execute('reboot &')
+        logger.info('Waiting for master node to reboot ...')
+        try:
+            wait(lambda: self.check_master_node_rebooted(last_uptime),
+                 timeout=timeout)
+        except Exception:
+            logger.error('Failed to wait for master node to reboot')
+            raise
+
+    def get_master_node_uptime(self):
+        self.wait_for_provisioning()
+        try:
+            remote = self.d_env.get_admin_remote()
+            result = remote.execute('cat /proc/uptime')
+            return float(result['stdout'][0].split()[0])
+        except Exception:
+            return None
+
+    def check_master_node_rebooted(self, last_uptime=None):
+        if not last_uptime:
+            return False
+        uptime = self.get_master_node_uptime()
+        if not uptime:
+            return False
+        if uptime >= last_uptime:
+            return False
+        logger.info('Node has been rebooted, uptime %s' % uptime)
+        return True
 
     def setup_customisation(self):
         self.wait_for_provisioning()
