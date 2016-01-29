f5794d55ceb5dfa11569e109d13cdc09cf23ad2c
Alexander Kurenyshev <akurenyshev@mirantis.com>
2016-01-29 13:52:01 +0300

    Fix changes domain name in MOS services endpoints
    We couldn't use public vip ip before start deploying cluster.
    So, we need to add option to skip SSL cert verification
    when connecting to the MOS services, and replace unresolvable
    domain name with public vip ip after deployment
    
    Closes-Bug:1537833
    
    Change-Id: I53621022a3ae55cb30ab224800fc812ce2b0a79b

--- fuelweb_test/tests/test_cli_base.py
+++ fuelweb_test/tests/test_cli_base.py
@@ -24,6 +24,7 @@ from fuelweb_test.helpers.ssl import change_cluster_ssl_config
 from fuelweb_test.tests.base_test_case import TestBasic
 from fuelweb_test import logwrap
 from fuelweb_test import logger
+from fuelweb_test.settings import SSL_CN
 
 
 class CommandLine(TestBasic):
@@ -122,8 +123,7 @@ class CommandLine(TestBasic):
     @logwrap
     def update_ssl_configuration(self, cluster_id, remote):
         settings = self.download_settings(cluster_id, remote)
-        cn = self.get_public_vip(cluster_id, remote)
-        change_cluster_ssl_config(settings, cn)
+        change_cluster_ssl_config(settings, SSL_CN)
         self.upload_settings(cluster_id, remote, settings)
 
     def add_nodes_to_cluster(
