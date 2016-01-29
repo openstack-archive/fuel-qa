68cb59da3451badf0dfa032bd6974aae5a23c088
Dmitry Teselkin <dteselkin@mirantis.com>
2016-01-29 18:33:02 +0300

    Run distro-sync during master node setup
    This commit adds additional (optional) step to master node
    deployment phase that executes distro-sync script and
    enables upstream CentOS repositories on master node.
    
    When upstream repos enabled the script updates master
    node before a snapshot created, allowing all subsequent
    tests to use modified image.
    
    Epic link: https://mirantis.jira.com/browse/PROD-3278
    
    Based on https://review.openstack.org/#/c/274110
    
    Change-Id: Ic905a62deae811e0aa93bff0713b34683e3f0cdc

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -507,3 +507,9 @@ NESSUS_PORT = os.environ.get("NESSUS_PORT", 8834)
 NESSUS_USERNAME = os.environ.get("NESSUS_USERNAME")
 NESSUS_PASSWORD = os.environ.get("NESSUS_PASSWORD")
 NESSUS_SSL_VERIFY = get_var_as_bool("NESSUS_SSL_VERIFY", False)
+
+DISTRO_SYNC_STEP = os.environ.get('DISTRO_SYNC_STEP', '')
+DISTRO_SYNC_URL = os.environ.get(
+    'DISTRO_SYNC_URL', 'git://172.18.10.105/mos-distro-sync.git')
+DISTRO_SYNC_BRANCH = os.environ.get('DISTRO_SYNC_BRANCH', 'master')
+DISTRO_SYNC_ROLLBACK = get_var_as_bool('DISTRO_SYNC_ROLLBACK', False)
