e71669201613374eae002fc53049405c63443a52
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2016-02-19 17:06:20 +0200

    Refactoring of system_test classes
    Some design of system_test packages:
        - move actions to separate package
        - move core fuctionality to core package
    
    Change-Id: I7b12380a687c9cccbd44d79819f842b1b2470cd5
    Implemets blueprint template-based-testcases

--- system_test/actions/__init__.py
+++ system_test/actions/__init__.py
@@ -4,7 +4,7 @@
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE_2.0
 #
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -12,12 +12,17 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
-from .factory import ActionsFactory
-from .repository import Repository
+from .base import BaseActions
+from .fuelmaster_actions import FuelMasterActions
+from .vcenter_actions import VMwareActions
+from .strength_actions import StrengthActions
+from .strength_actions import FillRootActions
 
 
 __all__ = [
-    ActionsFactory,
-    Repository,
+    BaseActions,
+    FuelMasterActions,
+    VMwareActions,
+    StrengthActions,
+    FillRootActions
 ]
