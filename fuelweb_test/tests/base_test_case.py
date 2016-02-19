bc29d9e5bc428b3c0575ec7540b183f5b227532a
Olivier Bourdon <obourdon@mirantis.com>
2016-02-19 09:26:12 +0100

    Add code to check number of available nodes for deployment
    Change-Id: I58fbf17d3bb1c0661181aa03d7a0431cf976137f
    Related-Bug: 1547381

--- fuelweb_test/tests/base_test_case.py
+++ fuelweb_test/tests/base_test_case.py
@@ -17,6 +17,7 @@ from proboscis import SkipTest
 from proboscis import test
 
 from fuelweb_test import logger
+from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test.helpers.utils import get_test_method_name
 from fuelweb_test.helpers.utils import TimeStat
@@ -234,6 +235,10 @@ class SetupEnvironment(TestBasic):
         self.show_step(2)
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[:1],
                                  skip_timesync=True)
+        checkers.validate_amount_nodes(
+            nodes=self.fuel_web.client.list_nodes(),
+            expected_amount=1
+        )
         self.env.make_snapshot("ready_with_1_slaves", is_make=True)
         self.current_log_step = 0
 
@@ -256,6 +261,10 @@ class SetupEnvironment(TestBasic):
         self.show_step(2)
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[:3],
                                  skip_timesync=True)
+        checkers.validate_amount_nodes(
+            nodes=self.fuel_web.client.list_nodes(),
+            expected_amount=3
+        )
         self.env.make_snapshot("ready_with_3_slaves", is_make=True)
         self.current_log_step = 0
 
@@ -278,6 +287,10 @@ class SetupEnvironment(TestBasic):
         self.show_step(2)
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[:5],
                                  skip_timesync=True)
+        checkers.validate_amount_nodes(
+            nodes=self.fuel_web.client.list_nodes(),
+            expected_amount=5
+        )
         self.env.make_snapshot("ready_with_5_slaves", is_make=True)
         self.current_log_step = 0
 
@@ -303,5 +316,9 @@ class SetupEnvironment(TestBasic):
                                  skip_timesync=True)
         self.env.bootstrap_nodes(self.env.d_env.nodes().slaves[5:9],
                                  skip_timesync=True)
+        checkers.validate_amount_nodes(
+            nodes=self.fuel_web.client.list_nodes(),
+            expected_amount=9
+        )
         self.env.make_snapshot("ready_with_9_slaves", is_make=True)
         self.current_log_step = 0
