fc9a619898f2b18b1c63cf8eca6b7e048091f74f
vgusev <vgusev@mirantis.com>
2016-02-24 16:52:53 +0300

    [StackLight] Add tests for HA mode for LMA Infrastructure Alerting plugin
    Added tests for HA mode and cluster scaling
    for LMA Infrastructure Alerting plugin
    
    Change-Id: Ic90c5f428ded02776d721a8aa5c6bf07eee48dcf

--- fuelweb_test/tests/plugins/plugin_lma_infra_alerting/test_plugin_lma_infra_alerting.py
+++ fuelweb_test/tests/plugins/plugin_lma_infra_alerting/test_plugin_lma_infra_alerting.py
@@ -21,7 +21,6 @@ import requests
 
 from fuelweb_test import logger
 from fuelweb_test import settings as conf
-from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
@@ -78,23 +77,96 @@ class TestLmaInfraAlertingPlugin(TestBasic):
             }
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
-        self._check_nagios(cluster_id)
+        self._check_nagios(cluster_id, nodes_count=1)
         self.fuel_web.run_ostf(cluster_id=cluster_id)
-        self.env.make_snapshot("deploy_lma_infra_alerting_ha")
+        self.env.make_snapshot("deploy_lma_infra_alerting_ha", is_make=True)
+
+    @test(depends_on=[deploy_lma_infra_alerting_ha],
+          groups=["scale_up_infra_alerting_cluster"])
+    @log_snapshot_after_test
+    def scale_up_infra_alerting_cluster(self):
+        """Scale up LMA Infrastructure Alerting cluster
+
+        Scenario:
+            1. Revert snapshot deploy_lma_infra_alerting_ha
+            2. Add 2 nodes with infrastructure_alerting role
+            3. Deploy the cluster
+            4. Check that the plugins work
+            5. Run OSTF
+
+        Duration 60m
+        Snapshot: scale_up_infra_alerting_cluster
+
+        """
+        self.env.revert_snapshot("deploy_lma_infra_alerting_ha")
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        self.env.bootstrap_nodes(
+            self.env.d_env.nodes().slaves[5:7])
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-06': [self._role_name],
+                'slave-07': [self._role_name],
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self._check_nagios(cluster_id, nodes_count=3)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+        self.env.make_snapshot("scale_up_infra_alerting_cluster")
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_9],
+          groups=["deploy_lma_infra_alerting_cluster_ha"])
+    @log_snapshot_after_test
+    def deploy_lma_infra_alerting_cluster_ha(self):
+        """Deploy cluster in HA with the LMA infrastructure alerting plugin
+           in HA mode
+
+        Scenario:
+            1. Upload plugin to the master node
+            2. Install plugins
+            3. Create cluster
+            4. Add 3 nodes with controller role
+            5. Add 1 node with compute + cinder roles
+            6. Add 3 nodes with infrastructure_alerting
+            7. Deploy the cluster
+            8. Check that the plugins work
+            9. Run OSTF
+
+        Duration 70m
+        Snapshot deploy_lma_infra_alerting_cluster_ha
+
+        """
+
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        cluster_id = self._bootstrap()
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                "slave-01": ["controller"],
+                "slave-02": ["controller"],
+                "slave-03": ["controller"],
+                "slave-04": ["compute", "cinder"],
+                "slave-05": [self._role_name],
+                "slave-06": [self._role_name],
+                "slave-07": [self._role_name],
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self._check_nagios(cluster_id, nodes_count=3)
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+        self.env.make_snapshot("deploy_lma_infra_alerting_cluster_ha")
 
     def _bootstrap(self):
 
-        with self.env.d_env.get_admin_remote() as remote:
+        self.env.admin_actions.upload_plugin(
+            plugin=conf.LMA_INFRA_ALERTING_PLUGIN_PATH)
 
-            # copy plugin to the master node
-            checkers.upload_tarball(
-                remote,
-                conf.LMA_INFRA_ALERTING_PLUGIN_PATH, "/var")
-
-            # install plugin
-            checkers.install_plugin_check_code(
-                remote,
-                plugin=os.path.basename(conf.LMA_INFRA_ALERTING_PLUGIN_PATH))
+        self.env.admin_actions.install_plugin(
+            plugin_file_name=os.path.basename(
+                conf.LMA_INFRA_ALERTING_PLUGIN_PATH))
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
@@ -113,19 +185,17 @@ class TestLmaInfraAlertingPlugin(TestBasic):
                     msg)
         logger.debug('%s (%s) plugin is installed' % (self._name,
                                                       self._version))
-        self.fuel_wb.update_plugin_settings(cluster_id,
-                                            self._name,
-                                            self._version,
-                                            plugin_options)
+        self.fuel_web.update_plugin_settings(cluster_id, self._name,
+                                             self._version, plugin_options)
 
         return cluster_id
 
-    def _check_nagios(self, cluster_id):
+    def _check_nagios(self, cluster_id, nodes_count):
         nagios_nodes = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
             cluster_id, [self._role_name])
 
         assert_true(
-            len(nagios_nodes) == 1,
+            len(nagios_nodes) == nodes_count,
             "One node with '{}' role must be present, found {}".format(
                 self._role_name, len(nagios_nodes)))
 
