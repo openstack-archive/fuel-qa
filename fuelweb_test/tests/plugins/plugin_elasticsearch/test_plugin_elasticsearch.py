f73b7d4284a1db5cd2779c84e257985a7c7902bc
vgusev <vgusev@mirantis.com>
2016-02-24 15:48:38 +0300

    [StackLight] Add tests for HA mode for Elasticsearch-Kibana plugin
    Added tests for HA mode and cluster scaling
    for Elasticsearch-Kibana plugin
    
    Change-Id: Idf276c699730e6610ac81bc5b9707e33f4b6eb80

--- fuelweb_test/tests/plugins/plugin_elasticsearch/test_plugin_elasticsearch.py
+++ fuelweb_test/tests/plugins/plugin_elasticsearch/test_plugin_elasticsearch.py
@@ -20,7 +20,6 @@ from proboscis import test
 
 from fuelweb_test import logger
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
-from fuelweb_test.helpers import checkers
 from fuelweb_test.settings import DEPLOYMENT_MODE
 from fuelweb_test.settings import ELASTICSEARCH_KIBANA_PLUGIN_PATH
 from fuelweb_test.tests.base_test_case import SetupEnvironment
@@ -40,6 +39,41 @@ class TestElasticsearchPlugin(TestBasic):
     def get_vip(self, cluster_id):
         networks = self.fuel_web.client.get_networks(cluster_id)
         return networks.get('vips').get('es_vip_mgmt', {}).get('ipaddr', None)
+
+    def upload_and_install_plugin(self):
+        self.env.admin_actions.upload_plugin(
+            plugin=ELASTICSEARCH_KIBANA_PLUGIN_PATH)
+        self.env.admin_actions.install_plugin(
+            plugin_file_name=os.path.basename(
+                ELASTICSEARCH_KIBANA_PLUGIN_PATH))
+
+    def update_plugin_settings(self, cluster_id):
+        msg = "Plugin couldn't be enabled. Check plugin version. Test aborted"
+        assert_true(
+            self.fuel_web.check_plugin_exists(cluster_id, self._name),
+            msg)
+
+        self.fuel_web.update_plugin_settings(cluster_id, self._name,
+                                             self._version, {})
+
+    def check_elasticsearch_plugin(self, cluster_id):
+        es_server_ip = self.get_vip(cluster_id)
+        assert_is_not_none(es_server_ip,
+                           "Failed to get the IP of Elasticsearch server")
+
+        logger.debug("Check that Elasticsearch is ready")
+
+        r = requests.get("http://{}:9200/".format(es_server_ip))
+        msg = "Elasticsearch responded with {}, expected 200".format(
+            r.status_code)
+        assert_equal(r.status_code, 200, msg)
+
+        logger.debug("Check that the HTTP server is running")
+
+        r = requests.get("http://{}/".format(es_server_ip))
+        msg = "HTTP server responded with {}, expected 200".format(
+            r.status_code)
+        assert_equal(r.status_code, 200, msg)
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
           groups=["deploy_elasticsearch_kibana"])
@@ -63,29 +97,14 @@ class TestElasticsearchPlugin(TestBasic):
         """
         self.env.revert_snapshot("ready_with_3_slaves")
 
-        with self.env.d_env.get_admin_remote() as remote:
-            # copy plugin to the master node
-            checkers.upload_tarball(
-                remote,
-                ELASTICSEARCH_KIBANA_PLUGIN_PATH, '/var')
-
-            # install plugin
-            checkers.install_plugin_check_code(
-                remote,
-                plugin=os.path.basename(ELASTICSEARCH_KIBANA_PLUGIN_PATH))
+        self.upload_and_install_plugin()
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
             mode=DEPLOYMENT_MODE,
         )
 
-        msg = "Plugin couldn't be enabled. Check plugin version. Test aborted"
-        assert_true(
-            self.fuel_web.check_plugin_exists(cluster_id, self._name),
-            msg)
-
-        self.fuel_web.update_plugin_settings(cluster_id, self._name,
-                                             self._version, {})
+        self.update_plugin_settings(cluster_id)
 
         self.fuel_web.update_nodes(
             cluster_id,
@@ -98,24 +117,98 @@ class TestElasticsearchPlugin(TestBasic):
 
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        es_server_ip = self.get_vip(cluster_id)
-        assert_is_not_none(es_server_ip,
-                           "Failed to get the IP of Elasticsearch server")
-
-        logger.debug("Check that Elasticsearch is ready")
-
-        r = requests.get("http://{}:9200/".format(es_server_ip))
-        msg = "Elasticsearch responded with {}".format(r.status_code)
-        msg += ", expected 200"
-        assert_equal(r.status_code, 200, msg)
-
-        logger.debug("Check that the HTTP server is running")
-
-        r = requests.get("http://{}/".format(es_server_ip))
-        msg = "HTTP server responded with {}".format(r.status_code)
-        msg += ", expected 200"
-        assert_equal(r.status_code, 200, msg)
+        self.check_elasticsearch_plugin(cluster_id)
 
         self.fuel_web.run_ostf(cluster_id=cluster_id)
 
-        self.env.make_snapshot("deploy_elasticsearch_kibana_plugin")
+        self.env.make_snapshot(
+            "deploy_elasticsearch_kibana_plugin", is_make=True)
+
+    @test(depends_on=[deploy_elasticsearch_kibana_plugin],
+          groups=["scale_up_elasticsearch_cluster"])
+    @log_snapshot_after_test
+    def scale_up_elasticsearch_cluster(self):
+        """Scale up Elasticsearch cluster
+
+        Scenario:
+            1. Revert snapshot deploy_elasticsearch_kibana_plugin
+            2. Add 2 node with elasticsearch_kibana role
+            3. Deploy the cluster
+            4. Check that plugin is working
+            5. Run OSTF
+
+        Duration 60m
+        Snapshot: scale_up_elasticsearch_cluster
+
+        """
+        self.env.revert_snapshot("deploy_elasticsearch_kibana_plugin")
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        self.env.bootstrap_nodes(
+            self.env.d_env.nodes().slaves[3:5])
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-04': [self._role_name],
+                'slave-05': [self._role_name]
+            }
+        )
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.check_elasticsearch_plugin(cluster_id)
+
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+
+        self.env.make_snapshot("scale_up_elasticsearch_cluster")
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["deploy_elasticsearch_kibana_ha_mode"])
+    @log_snapshot_after_test
+    def deploy_elasticsearch_kibana_plugin_ha_mode(self):
+        """Deploy a cluster with the Elasticsearch-Kibana plugin
+
+        Scenario:
+            1. Upload plugin to the master node
+            2. Install plugin
+            3. Create cluster
+            4. Add 1 node with controller role
+            5. Add 1 node with compute role
+            6. Add 3 nodes with elasticsearch_kibana role
+            7. Deploy the cluster
+            8. Check that plugin is working
+            9. Run OSTF
+
+        Duration 60m
+        Snapshot deploy_elasticsearch_kibana_plugin_ha_mode
+        """
+        self.env.revert_snapshot("ready_with_5_slaves")
+
+        self.upload_and_install_plugin()
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+        )
+
+        self.update_plugin_settings(cluster_id)
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+                'slave-03': [self._role_name],
+                'slave-04': [self._role_name],
+                'slave-05': [self._role_name]
+            }
+        )
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.check_elasticsearch_plugin(cluster_id)
+
+        self.fuel_web.run_ostf(cluster_id=cluster_id)
+
+        self.env.make_snapshot(
+            "deploy_elasticsearch_kibana_plugin_ha_mode")
