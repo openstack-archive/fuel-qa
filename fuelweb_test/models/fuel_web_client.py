2ced93a3895fdab5b9ba037e8114196b2a2e67c9
zatserklyany <zatserklyany@gmail.com>
2016-02-26 13:42:59 +0200

    Update test to plugin_attributes structure
    Closes-Bug #1550234
    
    Change-Id: I7426ca5308712443313a66bb45fc45e5594288a9

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -2365,9 +2365,22 @@ class FuelWebClient(object):
         for option, value in data.items():
             plugin_data = plugin_attributes['editable'][plugin_name]
             path = option.split("/")
-            for p in path[:-1]:
-                plugin_data = plugin_data[p]
-            plugin_data[path[-1]] = value
+            """Key 'metadata' can be in section
+            plugin_data['metadata']['versions']
+            For enable/disable plugin value must be set in
+            plugin_data['metadata']['enabled']
+            """
+            if 'metadata' in path:
+                plugin_data['metadata'][path[-1]] = value
+            elif 'versions' in plugin_data['metadata']:
+                for version in plugin_data['metadata']['versions']:
+                    for p in path[:-1]:
+                        version = version[p]
+                    version[path[-1]] = value
+            else:
+                for p in path[:-1]:
+                    plugin_data = plugin_data[p]
+                plugin_data[path[-1]] = value
         self.client.update_cluster_attributes(cluster_id, plugin_attributes)
 
     def get_plugin_data(self, cluster_id, plugin_name, version):
