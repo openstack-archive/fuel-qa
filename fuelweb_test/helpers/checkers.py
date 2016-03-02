826eb41b1a04cba2243bdc0513d4226c1713ac2e
Alexandr Kostrikov <akostrikov@mirantis.com>
2016-03-02 19:37:41 +0300

    Advanced features are enabled in nailgun
    Settings should be enabled not only in
    astute, but also in nailgun.
    That is done via application of puppet
    manifest of nailgun.
    
    Change-Id: I2a1ba27ee6c73fd32d0b318181c3c6b2a10e2d58
    Closes-bug: #1549447

--- fuelweb_test/helpers/checkers.py
+++ fuelweb_test/helpers/checkers.py
@@ -242,9 +242,16 @@ def get_package_versions_from_node(remote, name, os_type):
 @logwrap
 def enable_feature_group(env, group):
     fuel_settings = env.admin_actions.get_fuel_settings()
-    fuel_settings["FEATURE_GROUPS"].append(group)
+    if group not in fuel_settings["FEATURE_GROUPS"]:
+        fuel_settings["FEATURE_GROUPS"].append(group)
     env.admin_actions.save_fuel_settings(fuel_settings)
-    env.admin_actions.restart_service("nailgun")
+
+    # NOTE(akostrikov) We use FUEL_SETTINGS_YAML as primary source or truth and
+    # update nailgun configs via puppet from that value
+    ssh_manager.execute(
+        ip=ssh_manager.admin_ip,
+        cmd='puppet apply /etc/puppet/modules/nailgun/examples/nailgun-only.pp'
+    )
 
     def check_api_group_enabled():
         try:
