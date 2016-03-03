f61fcc3c480e1236d004468d00fde7dada55b3e7
Maksym Strukov <mstrukov@mirantis.com>
2016-03-03 21:39:26 +0200

    Use user-defined exception in execute_on_remote
    We need user-defined exception in execute_on_remote to
    catch it in failover tests. Base Exception isn't suitable
    as it mask all other exception types
    
    Change-Id: Ic48d73c2fabcf32eed8b57db047d60307e8ce63a
    Closes-Bug: #1552692

--- fuelweb_test/tests/test_ubuntu_bootstrap.py
+++ fuelweb_test/tests/test_ubuntu_bootstrap.py
@@ -28,6 +28,8 @@ from fuelweb_test.helpers import utils
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test import settings
 from fuelweb_test.tests import base_test_case
+from fuelweb_test.helpers.exception \
+    import ExecuteOnRemoteUnexpectedExitCodeError
 
 
 @test(groups=["ubuntu_bootstrap_builder", "bvt_ubuntu_bootstrap"])
@@ -290,9 +292,9 @@ class UbuntuBootstrapBuild(base_test_case.TestBasic):
                     "Bootstrap {0} was not deleted and still available: {1}"
                     .format(uuid, bootstrap_uuids))
 
-        assert_raises(error.DevopsCalledProcessError,
-                      self.env.fuel_bootstrap_actions.activate_bootstrap_image,
-                      uuid)
+        assert_true(ExecuteOnRemoteUnexpectedExitCodeError,
+                    self.env.fuel_bootstrap_actions.activate_bootstrap_image,
+                    uuid)
 
         assert_equal(2, len(bootstrap_uuids),
                      "Only two bootstrap images should be available, current"
