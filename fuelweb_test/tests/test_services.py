4102235eb6a225c7185161e134503244ef4beee6
Alexey Stepanov <astepanov@mirantis.com>
2016-03-15 15:18:54 +0300

    Python3 code compatibility: division, round
    Make division transparent-compatible (from __future__ import division, // for strict int result)
    Do not use round due to different types of result objects
    
    blueprint fuel-qa-python3-compatibility
    Related-bug: #1556791
    
    Change-Id: I7bb173c6f97ac046af45c3f52cc32846a93a8981

--- fuelweb_test/tests/test_services.py
+++ fuelweb_test/tests/test_services.py
@@ -528,7 +528,7 @@ class CeilometerHAOneControllerMongo(OSTFCeilometerHelper):
         logger.debug('disk size is {0}'.format(disk_mb))
         mongo_disk_mb = 11116
         os_disk_mb = disk_mb - mongo_disk_mb
-        mongo_disk_gb = ("{0}G".format(round(mongo_disk_mb / 1024, 1)))
+        mongo_disk_gb = "{0}G".format(mongo_disk_mb // 1024)
         disk_part = {
             "vda": {
                 "os": os_disk_mb,
