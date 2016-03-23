060e2a305a794a2a4020ba074762ea79d19f605f
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-03-23 16:03:26 +0200

    Deduplicate code in Neutron tests
    Move rebalance_swift_ring method to common helper.
    
    Change-Id: Id45217ce9f2f61f184db247bbf98d460144b9821

--- fuelweb_test/tests/base_test_case.py
+++ fuelweb_test/tests/base_test_case.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
 from proboscis import TestProgram
 from proboscis import SkipTest
 from proboscis import test
