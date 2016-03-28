46065432da14c73a1815570f8b32948312166ff7
Egor Kotko <ykotko@mirantis.com>
2016-03-28 18:49:49 +0200

    Fix output of HDD size
    Fix output of HDD size
    in check_nodes_notifications test
    
    Change-Id: I0eb9a23d67f8659e6ed3b7af5e557bd434eaf742
    Closes-bug: #1561687

--- fuelweb_test/tests/test_ha_one_controller.py
+++ fuelweb_test/tests/test_ha_one_controller.py
@@ -676,11 +676,9 @@ class NodeDiskSizes(TestBasic):
             for disk in node['meta']['disks']:
                 assert_equal(disk['size'], disk_size, 'Disk size')
 
-        # pylint: disable=round-builtin
-        hdd_size = "{} TB HDD".format(
-            round(((disk_size * 3 / (10 ** 9)) / 1000), 3))
-        # pylint: enable=round-builtin
+        hdd_size = "{0:.3} TB HDD".format((disk_size * 3 / (10 ** 9)) / 1000)
         notifications = self.fuel_web.client.get_notifications()
+
         for node in nailgun_nodes:
             # assert /api/notifications
             for notification in notifications:
@@ -688,16 +686,21 @@ class NodeDiskSizes(TestBasic):
                 current_node = notification['node_id'] == node['id']
                 if current_node and discover and \
                    "discovered" in notification['message']:
-                    assert_true(
-                        hdd_size in notification['message'],
-                        '"{size}" not found in notification message '
-                        '"{note}" for node {node} (hostname {host})!'.format(
-                            size=hdd_size,
-                            note=notification['message'],
-                            node=node['name'],
-                            host=node['hostname']
-                        )
-                    )
+                    assert_true(hdd_size in notification['message'],
+                                '"{size} not found in notification message '
+                                '"{note}" for node {node} '
+                                '(hostname {host})!'.format(
+                                    size=hdd_size,
+                                    note=notification['message'],
+                                    node=node['name'],
+                                    host=node['hostname']
+                                ))
+
+            # assert disks
+            disks = self.fuel_web.client.get_node_disks(node['id'])
+            for disk in disks:
+                assert_equal(disk['size'],
+                             NODE_VOLUME_SIZE * 1024 - 500, 'Disk size')
 
             # assert disks
             disks = self.fuel_web.client.get_node_disks(node['id'])
