198b6723f125cecd80efdd6f3a81befee7ba891a
Alexandr Kostrikov <akostrikov@mirantis.com>
2016-04-07 18:46:36 +0300

    Custom graphs deployment
    Check that graphs are isolated and generated
    from release and cluster tasks.
    
    Change-Id: Ie4454e742693dda8452ec9db90a6eafa8b97d42d
    Closes-bug: #1567519

---
+++ fuelweb_test/config_templates/prepare_release_image.py
@@ -0,0 +1,39 @@
+def execute():
+    import six
+
+    from nailgun.settings import NailgunSettings
+    from nailgun.objects.release import Release
+    from nailgun import consts
+    from nailgun.orchestrator import tasks_templates
+
+    settings = NailgunSettings()
+    master_ip = settings.config['MASTER_IP']
+    release_id = 2
+
+    release = Release.get_by_uid(release_id)
+
+    packages_str = \
+        release.attributes_metadata['editable']['provision']['packages']['value']
+    packages = list(
+        six.moves.filter(bool, (s.strip() for s in packages_str.split('\n'))))
+    task = tasks_templates.make_provisioning_images_task(
+        [consts.MASTER_NODE_UID],
+        release.attributes_metadata['editable']['repo_setup']['repos']['value'],
+        release.attributes_metadata['generated']['provision'],
+        'prepare_release_ubuntu',
+        packages)
+
+    release_str = 'release_{release_id}'.format(release_id=release_id)
+    with open('build_image.sh', 'w') as cmd_file:
+        cmd_file.write(task['parameters']['cmd'].replace(
+            "{cluster.release.environment_version}",
+            release.environment_version).replace(
+            '{cluster.release.version}',
+            release.version).replace(
+            '{settings.MASTER_IP}',
+            master_ip).replace(  # any clean way?
+            "{cluster.id}",
+            release_str))
+
+if __name__ == '__main__':
+    execute()
