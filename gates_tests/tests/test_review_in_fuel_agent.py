25ec9da2156bbcf8b2d8a1f218ee0e2c8666272b
Artem Grechanichenko <agrechanichenko@mirantis.com>
2016-04-08 18:44:48 +0300

    Extend fuel-agent gate with ironic checking
    -Extend fuel-agent gate with ironic checking.
    -Refactor update_fuel_agent_rpm method to be more universal
    -Change updating fuel-agent in bootstrap to use fuel-bootstrap methods
    
    Change-Id: I495e8c15ad05188bb0312ca8cf4fbc1169a88bae
    Closes-Bug: #1551156

--- gates_tests/tests/test_review_in_fuel_agent.py
+++ gates_tests/tests/test_review_in_fuel_agent.py
@@ -1,37 +1,49 @@
-#    Copyright 2015 Mirantis, Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+import yaml
 
 from proboscis import test
 
-from gates_tests.helpers.utils import patch_and_assemble_ubuntu_bootstrap
-from gates_tests.helpers.utils import replace_fuel_agent_rpm
+from gates_tests.helpers.utils import replace_rpm_package
 
+from fuelweb_test import logger
+from fuelweb_test.helpers import ironic_actions
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
-from fuelweb_test.settings import OPENSTACK_RELEASE
-from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.helpers.ssh_manager import SSHManager
+from fuelweb_test.settings import NEUTRON_SEGMENT
+from fuelweb_test.tests.test_ironic_base import TestIronicDeploy
 
 from fuelweb_test import settings
 
+from gates_tests.helpers.utils import \
+    check_package_version_injected_in_bootstraps
+from gates_tests.helpers.utils import rebuild_and_activate_new_bootstrap
+
 
 @test(groups=["review_fuel_agent"])
-class Gate(TestBasic):
+class Gate(TestIronicDeploy):
     """Using in fuel-agent CI-gates
     Update fuel-agent on master node, bootstrap from review,
     build environment images and provision one node"""
 
+    @staticmethod
+    def update_bootstrap_cli_yaml():
+        ssh = SSHManager()
+        admin_remote = ssh._get_remote(ssh.admin_ip)
+        path = "/etc/fuel-bootstrap-cli/fuel_bootstrap_cli.yaml"
+        with admin_remote.open(path, 'r') as bootstrap_yaml:
+            bootstrap_yaml_raw = yaml.load(bootstrap_yaml)
+            logger.debug(bootstrap_yaml_raw)
+            new_repo = {'name': 'auxiliary', 'priority': '1200',
+                        'section': 'main restricted',
+                        'suite': 'auxiliary', 'type': 'deb',
+                        'uri': 'http://127.0.0.1:8080/ubuntu/auxiliary/'}
+            bootstrap_yaml_raw['repos'].append(new_repo)
+            logger.debug(bootstrap_yaml_raw)
+        with admin_remote.open(path, 'w') as bootstrap_yaml:
+            yaml.dump(
+                bootstrap_yaml_raw, bootstrap_yaml, default_flow_style=False)
+
     @test(depends_on_groups=['prepare_release'],
-          groups=["review_fuel_agent_one_node_provision"])
+          groups=["review_fuel_agent_ironic_deploy"])
     @log_snapshot_after_test
     def gate_patch_fuel_agent(self):
         """ Revert snapshot, update fuel-agent, bootstrap from review
@@ -39,13 +51,20 @@ class Gate(TestBasic):
 
     Scenario:
         1. Revert snapshot "ready"
-        2. Update fuel-agent on master node
+        2. Update fuel-agent, fuel-bootstrap-cli on master node
         3. Update bootstrap
-        4. Bootstrap 1 slave
-        5. Create environment via FUEL CLI
-        6. Assign controller role
-        7. Provisioning node
+        4. Bootstrap 3 slaves
+        5. Add 1 node with controller role
+        6. Add 1 node with ironic role
+        7. Add 1 node with compute role
+        8. Deploy the cluster
+        9. Verify fuel-agent version in ubuntu and ironic-bootstrap
+        10. Upload image to glance
+        11. Enroll Ironic nodes
+        12. Boot nova instance
+        13. Check Nova instance status
 
+        Snapshot review_fuel_agent_ironic_deploy
         """
         if not settings.UPDATE_FUEL:
             raise Exception(
@@ -54,36 +73,63 @@ class Gate(TestBasic):
         self.env.revert_snapshot("ready")
 
         self.show_step(2)
-        replace_fuel_agent_rpm()
+        replace_rpm_package('fuel-agent')
+        replace_rpm_package('fuel-bootstrap-cli')
 
         self.show_step(3)
-        patch_and_assemble_ubuntu_bootstrap(self.env)
+        self.update_bootstrap_cli_yaml()
+        rebuild_and_activate_new_bootstrap(self)
 
         self.show_step(4)
         self.env.bootstrap_nodes(
-            self.env.d_env.nodes().slaves[:1])
+            self.env.d_env.nodes().slaves[:5])
 
-        release_id = self.fuel_web.get_releases_list_for_os(
-            release_name=OPENSTACK_RELEASE)[0]
+        data = {
+            'volumes_ceph': True,
+            'images_ceph': True,
+            'objects_ceph': True,
+            'volumes_lvm': False,
+            'tenant': 'ceph1',
+            'user': 'ceph1',
+            'password': 'ceph1',
+            'net_provider': 'neutron',
+            'net_segment_type': NEUTRON_SEGMENT['vlan'],
+            'ironic': True}
+        nodes = {
+            'slave-01': ['controller', 'ceph-osd'],
+            'slave-02': ['controller', 'ironic', 'ceph-osd'],
+            'slave-03': ['controller', 'ironic', 'ceph-osd'],
+            'slave-04': ['ironic'],
+            'slave-05': ['compute']}
 
         self.show_step(5)
-        cmd = ('fuel env create --name={0} --release={1} --nst=tun '
-               '--json'.format(self.__class__.__name__, release_id))
-        env_result = self.ssh_manager.execute_on_remote(
-            ip=self.ssh_manager.admin_ip,
-            cmd=cmd, jsonify=True)['stdout_json']
-        cluster_id = env_result['id']
-
         self.show_step(6)
-        self.fuel_web.update_nodes(
-            cluster_id,
-            {
-                'slave-01': ['controller'],
-            }
-        )
-
-        cluster_id = self.fuel_web.get_last_created_cluster()
         self.show_step(7)
-        self.fuel_web.provisioning_cluster_wait(cluster_id)
+        self.show_step(8)
 
-        self.env.make_snapshot("review_fuel_agent_one_node_provision")
+        cluster_id = self._deploy_ironic_cluster(settings=data, nodes=nodes)
+
+        ironic_conn = ironic_actions.IronicActions(
+            self.fuel_web.get_public_vip(cluster_id),
+            user='ceph1',
+            passwd='ceph1',
+            tenant='ceph1')
+
+        self.show_step(9)
+        check_package_version_injected_in_bootstraps("fuel-agent")
+
+        check_package_version_injected_in_bootstraps("fuel-agent",
+                                                     ironic=True)
+
+        self.show_step(10)
+        self.show_step(11)
+        self._create_os_resources(ironic_conn)
+
+        self.show_step(12)
+        self._boot_nova_instances(ironic_conn)
+
+        self.show_step(13)
+        ironic_conn.wait_for_vms(ironic_conn)
+        ironic_conn.verify_vms_connection(ironic_conn)
+
+        self.env.make_snapshot("review_fuel_agent_ironic_deploy")
