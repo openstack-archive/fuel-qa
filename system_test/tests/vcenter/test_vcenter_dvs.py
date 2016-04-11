b3920bcc157be2617e74210d8cc3fd0a7113e1d0
ekhomyakova <ekhomyakova@mirantis.com>
2016-04-11 14:03:08 +0300

    Added failover tests for vCenter
    Change-Id: I6b0a3691223fbd89fe46667243b48b1398e936c2
    Closes-bug: #1575742

--- system_test/tests/vcenter/test_vcenter_dvs.py
+++ system_test/tests/vcenter/test_vcenter_dvs.py
@@ -14,22 +14,22 @@
 
 from fuelweb_test.settings import DVS_PLUGIN_PATH
 from fuelweb_test.settings import DVS_PLUGIN_VERSION
-from fuelweb_test.settings import VMWARE_IMG_NAME
-from fuelweb_test.settings import VMWARE_IMG_URL
 from fuelweb_test.settings import VMWARE_IMG_LOGIN
+from fuelweb_test.settings import VMWARE_IMG_NAME
 from fuelweb_test.settings import VMWARE_IMG_PASSWORD
+from fuelweb_test.settings import VMWARE_IMG_URL
 
 from system_test import testcase
-from system_test.tests import ActionTest
 from system_test.actions import BaseActions
 from system_test.actions import VMwareActions
+from system_test.tests import ActionTest
 
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
                   'system_test.vcenter.deploy_vcenter_dvs_run_ostf'])
 class DeployWithVMware(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -64,7 +64,7 @@ class DeployWithVMware(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.scale_vcenter_dvs'])
 class ScaleWithVMware(ActionTest, BaseActions, VMwareActions):
-    """Deploy and scale cluster with vCenter and dvs plugin
+    """Deploy and scale cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -105,7 +105,7 @@ class ScaleWithVMware(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.scale_vcenter_dvs_skipsrvcheck'])
 class ScaleWithVMwareSkipSrvCheck(ActionTest, BaseActions, VMwareActions):
-    """Deploy and scale cluster with vCenter and dvs plugin
+    """Deploy and scale cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -148,7 +148,7 @@ class ScaleWithVMwareSkipSrvCheck(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.deploy_with_custom_hostname'])
 class DeployWithCustomHostname(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and custom hostname
+    """Deploy cluster with vCenter and custom hostname.
 
     Scenario:
         1. Upload plugin to the master node
@@ -185,7 +185,7 @@ class DeployWithCustomHostname(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.check_nova_config'])
 class CheckNovaConfig(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and custom hostname
+    """Deploy cluster with vCenter and custom hostname.
 
     Scenario:
         1. Upload plugin to the master node
@@ -222,7 +222,7 @@ class CheckNovaConfig(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.check_nova_srv'])
 class CheckNovaSrv(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and custom hostname
+    """Deploy cluster with vCenter and custom hostname.
 
     Scenario:
         1. Upload plugin to the master node
@@ -257,9 +257,56 @@ class CheckNovaSrv(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_reset_ctrl'])
+class HardResetPrimaryWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Hard reset primary controller and check vCenter functionality.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster
+        8. Create instances on Nova and vCenter
+        9. Hard reset primary controller
+        10. Wait 5-10 minutes
+        11. Verify networks
+        12. Ensure that VIPs are moved to other controller
+        12. Ensure connectivity between VMs
+        13. Run OSTF tests
+
+    Duration 3h 00min
+    Snapshot vcenter_reset_ctrl
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'deploy_cluster',
+        'create_instances',
+        'hard_reset_primary',
+        'network_check',
+        'check_up_vips',
+        'check_vm_connect',
+        'delete_instances',
+        'health_check_sanity_smoke_ha'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.check_cinder_srv'])
 class CheckCinderVmwareSrv(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and custom hostname
+    """Deploy cluster with vCenter and custom hostname.
 
     Scenario:
         1. Upload plugin to the master node
@@ -296,7 +343,7 @@ class CheckCinderVmwareSrv(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.attach_empty_volume'])
 class AttachEmptyVol(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -329,9 +376,62 @@ class AttachEmptyVol(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_shutdown_ctrl'])
+class ShutdownPrimaryWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Shutdown primary controller and check vCenter functionality.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster
+        8. Create instances on Nova and vCenter
+        9. Shutdown primary controller
+        10. Verify networks
+        11. Ensure that VIPs are moved to other controller
+        12. Ensure connectivity between VMs
+        13. Run OSTF tests (one should fail)
+        14. Turn on primary controller
+        15. Wait 5-10 minutes
+        16. Verify networks
+        17. Run OSTF tests
+
+    Duration 3h 00min
+    Snapshot vcenter_shutdown_ctrl
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'deploy_cluster',
+        'create_instances',
+        'shutdown_primary',
+        'network_check',
+        'check_up_vips',
+        'check_vm_connect',
+        'delete_instances',
+        'ostf_with_services_fail',
+        'turn_on_primary',
+        'network_check',
+        'health_check_sanity_smoke_ha'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.bootable_vol'])
 class BootableVol(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -364,9 +464,56 @@ class BootableVol(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_reboot_ctrl'])
+class SafeRebootPrimaryWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Safe reboot primary controller and check vCenter functionality.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster
+        8. Create instances on Nova and vCenter
+        9. Safe reboot primary controller
+        10. Wait 5-10 minutes
+        11. Verify networks
+        12. Ensure that VIPs are moved to other controller
+        13. Ensure connectivity between VMs
+        14. Run OSTF tests
+
+    Duration 3h 00min
+    Snapshot vcenter_reboot_ctrl
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'deploy_cluster',
+        'create_instances',
+        'safe_reboot_primary',
+        'network_check',
+        'check_up_vips',
+        'check_vm_connect',
+        'delete_instances',
+        'health_check_sanity_smoke_ha'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.disable_enable_compute_service'])
 class DisableEnableVMwareServices(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -399,9 +546,63 @@ class DisableEnableVMwareServices(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_shutdown_cindervmware'])
+class ShutdownCinderNodeWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Shutdown one of CinderVMDK node.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster
+        8. Create instances on KVM and vCenter
+        9. Run all OSTF tests
+        10. Shutdown one of CinderVMDK node
+        11. Run vCenter OSTF tests
+        12. Power on CinderVMDK node and wait for it to load
+        13. Run vCenter OSTF tests
+        14. Shutdown another CinderVMDK node
+        15. Run vCenter OSTF tests
+        16. Power on CinderVMDK node and wait for it to load
+        17. Run all OSTF tests
+
+    Duration 3h 00min
+    Snapshot vcenter_shutdown_cindervmware
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'deploy_cluster',
+        'create_instances',
+        'health_check_sanity_smoke_ha',
+        'shutdown_cinder_node',
+        'vcenter_ostf',
+        'power_on_cinder_node',
+        'vcenter_ostf',
+        'shutdown_another_cinder_node',
+        'vcenter_ostf',
+        'power_on_cinder_node',
+        'delete_instances',
+        'health_check_sanity_smoke_ha'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.upload_image'])
 class UploadImage(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -441,7 +642,7 @@ class UploadImage(ActionTest, BaseActions, VMwareActions):
                   'system_test.vcenter',
                   'system_test.vcenter.vmxnet3'])
 class Vmxnet3(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -479,9 +680,47 @@ class Vmxnet3(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_reboot_cindervmware'])
+class RebootCinderNodeWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Restart CinderVMware node.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster
+        8. Reboot CinderVMware node.
+        9. Check CinderVMware services.
+
+    Duration 3h 00min
+    Snapshot vcenter_reboot_cindervmware
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'deploy_cluster',
+        'reboot_cinder_vmware',
+        'check_cinder_vmware_srv',
+        'health_check'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.create_batch_of_instances'])
 class CreateBatchInstances(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -514,9 +753,43 @@ class CreateBatchInstances(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_iname_glance_ds'])
+class DeployINameDSWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Deploy with controller and incorrect name of vCenter Glance Datastore.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster (Deploy should fail)
+
+    Duration 3h 00min
+    Snapshot vcenter_iname_glance_ds
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'config_ids_glance',
+        'fail_deploy_cluster'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.diff_disk_types'])
 class DiffDiskTypes(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -549,9 +822,43 @@ class DiffDiskTypes(ActionTest, BaseActions, VMwareActions):
 
 @testcase(groups=['system_test',
                   'system_test.vcenter',
+                  'system_test.vcenter.vcenter_iname_glance_dc'])
+class DeployINameDCWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Deploy with controller and incorrect name of vCenter Glance Datacenter.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Deploy the cluster (Deploy should fail)
+
+    Duration 3h 00min
+    Snapshot vcenter_iname_glance_dc
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'config_idc_glance',
+        'fail_deploy_cluster'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
                   'system_test.vcenter.neutron_public_net'])
 class DeployNeutronPublicNet(ActionTest, BaseActions, VMwareActions):
-    """Deploy cluster with vCenter and dvs plugin
+    """Deploy cluster with vCenter and dvs plugin.
 
     Scenario:
         1. Upload plugin to the master node
@@ -580,4 +887,41 @@ class DeployNeutronPublicNet(ActionTest, BaseActions, VMwareActions):
         'deploy_cluster',
         'check_neutron_public',
         'check_gw_on_vmware_nodes'
+    ]
+
+
+@testcase(groups=['system_test',
+                  'system_test.vcenter',
+                  'system_test.vcenter.vcenter_idatastore'])
+class DeployIDSWithVMware(ActionTest, BaseActions, VMwareActions):
+    """Deploy with controller and not correct regex of vCenter Datastore.
+
+    Scenario:
+        1. Upload plugin to the master node
+        2. Install plugin
+        3. Create cluster
+        4. Configure dvs settings (depends on yaml config)
+        5. Add nodes (depends on yaml config)
+        6. Configure vmware settings (depends on yaml config)
+        7. Redefine vmware settings with incorrect ds
+        8. Deploy the cluster
+        9. Run OSTF tests (should fail)
+
+    Duration 3h 00min
+    Snapshot vcenter_idatastore
+    """
+
+    plugin_name = "fuel-plugin-vmware-dvs"
+    plugin_path = DVS_PLUGIN_PATH
+    plugin_version = DVS_PLUGIN_VERSION
+
+    actions_order = [
+        'prepare_env_with_plugin',
+        'create_env',
+        'configure_dvs_plugin',
+        'add_nodes',
+        'configure_vcenter',
+        'config_idatastore',
+        'deploy_cluster',
+        'fail_ostf'
     ]
