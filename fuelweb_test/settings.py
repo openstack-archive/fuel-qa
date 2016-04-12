c6b50d2a052656770211046a264538b79ae37fa5
Ksenia Demina <kdemina@mirantis.com>
2016-04-12 20:17:11 +0300

    Add test for different amount of pinned CPU
    This testgroup should be run with this parameters:
    
    export KVM_USE=True
    export SLAVE_NODE_CPU=6
    export DRIVER_ENABLE_ACPI=true
    export NUMA_NODES=2
    export IFACE_0=ens3
    export IFACE_1=ens4
    export IFACE_2=ens5
    export IFACE_3=ens6
    export IFACE_4=ens7
    export IFACE_5=ens8
    
    Change-Id: Ida99b43992c854db33eab8c60ca52b0b73297f08
    Implements: blueprint test-numa-cpu-pinning

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -98,6 +98,7 @@ HARDWARE = {
     "admin_node_memory": os.environ.get("ADMIN_NODE_MEMORY", 3072),
     "admin_node_cpu": os.environ.get("ADMIN_NODE_CPU", 2),
     "slave_node_cpu": os.environ.get("SLAVE_NODE_CPU", 1),
+    "numa_nodes": os.environ.get("NUMA_NODES", 0),
 }
 if OPENSTACK_RELEASE_UBUNTU in OPENSTACK_RELEASE:
     slave_mem_default = 2560
