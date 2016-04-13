5b4f5d3c0e0c3030a911bbd2efdac6cfa6c6e322
Ilya Bumarskov <ibumarskov@mirantis.com>
2016-04-13 21:40:42 +0300

    vCenter tests with OpenStack actions for cinder and nova
    Change-Id: If98635479026b4df60354904e543b18d3abc81d4
    Closes-bug: #1570048

--- fuelweb_test/helpers/os_actions.py
+++ fuelweb_test/helpers/os_actions.py
@@ -42,6 +42,11 @@ class OpenStackActions(common.Common):
             if image.name.startswith(name):
                 return image
 
+    def get_hosts(self):
+        hosts = self.nova.hosts.list()
+        if hosts:
+            return hosts
+
     def get_hypervisors(self):
         hypervisors = self.nova.hypervisors.list()
         if hypervisors:
@@ -64,13 +69,19 @@ class OpenStackActions(common.Common):
         if servers:
             return servers
 
+    def get_flavor_by_name(self, name):
+        return [flavor for flavor in self.nova.flavors.list()
+                if flavor.name == name].pop()
+
     def create_server(
             self,
             name=None,
             security_groups=None,
             flavor_id=None,
             net_id=None,
-            timeout=100
+            timeout=100,
+            image=None,
+            **kwargs
     ):
         """ Creates simple server, like in OSTF.
 
@@ -79,6 +90,7 @@ class OpenStackActions(common.Common):
         :param flavor_id: micro_flavor if None
         :param net_id: network id, could be omitted.
         :param timeout: int=100
+        :param image: TestVM if None.
         :return: Server, in started state
         """
         def find_micro_flavor():
@@ -92,15 +104,18 @@ class OpenStackActions(common.Common):
             security_groups = [self.create_sec_group_for_ssh()]
         if not flavor_id:
             flavor_id = find_micro_flavor().id
+        if image is None:
+            image = self._get_cirros_image().id
 
         nics = [{'net-id': net_id}] if net_id else None
 
         srv = self.nova.servers.create(
             name=name,
-            image=self._get_cirros_image().id,
+            image=image,
             flavor=flavor_id,
             security_groups=[sec_group.name for sec_group in security_groups],
-            nics=nics)
+            nics=nics,
+            **kwargs)
 
         try:
             helpers.wait(
@@ -281,8 +296,9 @@ class OpenStackActions(common.Common):
         server = self.get_instance_detail(server.id)
         return server
 
-    def create_volume(self, size=1, image_id=None):
-        volume = self.cinder.volumes.create(size=size, imageRef=image_id)
+    def create_volume(self, size=1, image_id=None, **kwargs):
+        volume = self.cinder.volumes.create(size=size, imageRef=image_id,
+                                            **kwargs)
         helpers.wait(
             lambda: self.cinder.volumes.get(volume.id).status == "available",
             timeout=100)
@@ -452,6 +468,9 @@ class OpenStackActions(common.Common):
     def get_image_list(self):
         return self.glance.images.list()
 
+    def update_image(self, image, **kwargs):
+        self.glance.images.update(image, **kwargs)
+
     def get_image(self, image_name):
         image_list = self.get_image_list()
         for img in image_list:
@@ -462,8 +481,44 @@ class OpenStackActions(common.Common):
     def get_image_data(self, image_name):
         return self.glance.images.data(image_name)
 
+    def get_security_group_list(self):
+        return self.nova.security_groups.list()
+
+    def get_security_group(self, sg_name):
+        sg_list = self.get_security_group_list()
+        for sg in sg_list:
+            if sg.name == sg_name:
+                return sg
+        return None
+
     def get_nova_service_list(self):
         return self.nova.services.list()
+
+    def get_nova_service_status(self, service):
+        services = self.get_nova_service_list()
+        for s in services:
+            if s.host == service.host and s.binary == service.binary:
+                return s.status
+
+    def enable_nova_service(self, service, timeout=30):
+        self.nova.services.enable(service.host, service.binary)
+        helpers.wait(
+            lambda: self.get_nova_service_status(service) == "enabled",
+            timeout=timeout,
+            timeout_msg="Service {0} on {1} does not reach enabled "
+                        "state, current state "
+                        "is {2}".format(service.binary, service.host,
+                                        service.status))
+
+    def disable_nova_service(self, service, timeout=30):
+        self.nova.services.disable(service.host, service.binary)
+        helpers.wait(
+            lambda: self.get_nova_service_status(service) == "disabled",
+            timeout=timeout,
+            timeout_msg="Service {0} on {1} does not reach disabled "
+                        "state, current state "
+                        "is {2}".format(service.binary, service.host,
+                                        service.status))
 
     def delete_nova_service(self, service_id):
         return self.nova.services.delete(service_id)
