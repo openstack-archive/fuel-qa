5c32794079121b404a0b9fb96340b143b77b80d6
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-05-06 12:37:27 +0300

    Sort node ids in cli acceptance tests
    For improving predicability we should sort nodes ids
    Closes-bug:1578664
    
    Change-Id: Ibf3a197c307fd849dac477f6983d3cbfe4c5ceb4

--- fuelweb_test/tests/test_cli.py
+++ fuelweb_test/tests/test_cli.py
@@ -132,9 +132,8 @@ class CommandLineTest(test_cli_base.CommandLine):
         Duration 50m
         """
         self.env.revert_snapshot("ready_with_3_slaves")
-        node_ids = [self.fuel_web.get_nailgun_node_by_devops_node(
-            self.env.d_env.nodes().slaves[
-                slave_id])['id']for slave_id in range(3)]
+        node_ids = sorted([node['id'] for node in
+                           self.fuel_web.client.list_nodes()])
         release_id = self.fuel_web.get_releases_list_for_os(
             release_name=OPENSTACK_RELEASE)[0]
         admin_ip = self.ssh_manager.admin_ip
@@ -478,7 +477,8 @@ class CommandLineTest(test_cli_base.CommandLine):
         )
 
         # Get nodes ids
-        node_ids = [node['id'] for node in self.fuel_web.client.list_nodes()]
+        node_ids = sorted([node['id'] for node in
+                           self.fuel_web.client.list_nodes()])
         admin_ip = self.ssh_manager.admin_ip
         self.show_step(5)
         cluster_settings = self.download_settings(cluster_id)
