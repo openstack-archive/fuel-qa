7ac8739c899a7900fb726edc9ebd634834090084
Mikhail Samoylov <msamoylov@mirantis.com>
2016-05-10 14:54:35 +0300

    Test for unlock_settings_tab feature
    This test checkes that feature "unlock settings"  wiil be working
    with different cluster and network settings which
    changing during deployment
    
    Change-Id: Ia72da06a9ebb982d5c22a98bfe162623073aeed9

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -917,7 +917,7 @@ class FuelWebClient29(object):
 
     @logwrap
     def check_cluster_settings(self, cluster_id, cluster_attributes):
-        task_id = self.get_last_task_id(cluster_id, 'deployment')
+        task_id = self.get_first_task_id(cluster_id, 'deployment')
         cluster_settings = \
             self.client.get_cluster_settings_for_deployment_task(task_id)
         logger.debug('Cluster settings before deploy {}'.format(
@@ -930,7 +930,7 @@ class FuelWebClient29(object):
 
     @logwrap
     def check_network_settings(self, cluster_id, network_settings):
-        task_id = self.get_last_task_id(cluster_id, 'deployment')
+        task_id = self.get_first_task_id(cluster_id, 'deployment')
         network_configuration = \
             self.client.get_network_configuration_for_deployment_task(task_id)
         logger.debug('Network settings before deploy {}'.format(
@@ -944,7 +944,7 @@ class FuelWebClient29(object):
     @logwrap
     def check_deployment_info_save_for_task(self, cluster_id):
         try:
-            task_id = self.get_last_task_id(cluster_id, 'deployment')
+            task_id = self.get_first_task_id(cluster_id, 'deployment')
             self.client.get_deployment_info_for_task(task_id)
         except Exception:
             logger.error(
@@ -952,7 +952,7 @@ class FuelWebClient29(object):
                     task_id))
 
     @logwrap
-    def get_last_task_id(self, cluster_id, task_name):
+    def get_first_task_id(self, cluster_id, task_name):
         tasks = self.client.get_tasks()
         tasks_ids = []
         for task in tasks:
@@ -963,6 +963,17 @@ class FuelWebClient29(object):
     def deploy_cluster_wait_progress(self, cluster_id, progress,
                                      return_task=None):
         task = self.deploy_cluster(cluster_id)
+        self.assert_task_success(task, interval=30, progress=progress)
+        if return_task:
+            return task
+
+    def redeploy_cluster_changes_wait(self, cluster_id, progress, data=None,
+                                      return_task=None):
+        logger.info('Re-deploy cluster {}'
+                    ' to apply the changed settings'.format(cluster_id))
+        if data is None:
+            data = {}
+        task = self.client.redeploy_cluster_changes(cluster_id, data)
         self.assert_task_success(task, interval=30, progress=progress)
         if return_task:
             return task
