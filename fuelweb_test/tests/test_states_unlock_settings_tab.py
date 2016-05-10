8c4653a1daa46c5d74b9c08a14539bb077c50cc8
Mikhail Samoylov <msamoylov@mirantis.com>
2016-05-10 14:54:35 +0300

    Test for unlock_settings_tab feature.
    This test checkes that feature "unlock settings"  wiil be working
    with different cluster and network settings which
    changing during deployment.
    
            Scenario:
                1. Create cluster
                2. Download default cluster settings
                3. Create custom_config and upload it to cluster
                4. Add 3 nodes with controller role and 2 nodes with compute role
                5. Deploy the cluster
                6. Stop deployment process
                7. Get current settings
                8. Change and save them (that means settings are unlocked)
                9. Redeploy cluster via api
                10. Get cluster and network settings via api (api load deployed)
                11. Compare settings from step 8 and 10 (them must be equal)
                12. Get default settings via api (load defaults)
                13. Compare settings from step 2 and 13 (them must be equal)
                14. Redeploy cluster
                15. Stop deployment process
                16. Redeploy cluster
                17. Run OSTF
    
    Change-Id: Ia72da06a9ebb982d5c22a98bfe162623073aeed9

--- fuelweb_test/tests/test_states_unlock_settings_tab.py
+++ fuelweb_test/tests/test_states_unlock_settings_tab.py
@@ -12,6 +12,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
+
+from netaddr import IPNetwork
+from proboscis.asserts import assert_equal
+from proboscis.asserts import assert_true
 from proboscis import test
 
 from fuelweb_test import logger
@@ -67,9 +72,30 @@ class UnlockSettingsTabStates(TestBasic):
     def get_cluster_attributes(self):
         return self.fuel_web.client.get_cluster_attributes(self.cluster_id)
 
+    def get_networks(self):
+        return self.fuel_web.client.get_networks(self.cluster_id)
+
+    def get_deployed_cluster_attributes(self):
+        return self.fuel_web.client.get_deployed_cluster_attributes(
+            self.cluster_id)
+
+    def get_deployed_network_configuration(self):
+        return self.fuel_web.client.get_deployed_network_configuration(
+            self.cluster_id)
+
+    def get_default_cluster_settings(self):
+        return self.fuel_web.client.get_default_cluster_settings(
+            self.cluster_id)
+
     def update_cluster_attributes(self, attributes):
         self.fuel_web.client.update_cluster_attributes(self.cluster_id,
                                                        attributes)
+
+    def update_network_settings(self,
+                                networking_parameters=None, networks=None):
+        self.fuel_web.client.update_network(
+            self.cluster_id, networking_parameters=networking_parameters,
+            networks=networks)
 
     @staticmethod
     def change_settings(attrs):
@@ -87,6 +113,63 @@ class UnlockSettingsTabStates(TestBasic):
                 value = editable[group][opt]['value']
                 editable[group][opt]['value'] = not value
         return attrs
+
+    @staticmethod
+    def compare_networks(old_settings, new_settings):
+        logger.debug("Old setting are:{}".format(old_settings))
+        logger.debug("New setting are:{}".format(new_settings))
+        for setting in old_settings:
+            if setting != 'networks':
+                if old_settings[setting] != new_settings[setting]:
+                    return False
+        for net1 in old_settings['networks']:
+            for net2 in new_settings['networks']:
+                if net1['name'] == net2['name'] and set(net1) != set(net2):
+                    return False
+                else:
+                    continue
+        return True
+
+    @staticmethod
+    def compare_settings(old_attrs, new_attrs):
+        skipped_options =\
+            [
+                'service_user.password',
+                'public_ssl.cert_data',
+                'storage.bootstrap_osd_key',
+                'storage.radosgw_key',
+                'storage.admin_key',
+                'storage.fsid',
+                'storage.mon_key',
+                'workloads_collector.password',
+                'murano_settings.murano_glance_artifacts_plugin',
+                'additional_components.murano_glance_artifacts_plugin',
+                'common.debug',
+                'external_dns.dns_list',
+                'public_ssl.horizon',
+                'public_ssl.services',
+                u'public_ssl.cert_source',
+                u'operator_user.password'
+            ]
+        logger.debug("Old default cluster settings: {}".format(old_attrs))
+        logger.debug("New default cluster settings: {}".format(new_attrs))
+        editable_old = old_attrs['editable']
+        editable_new = new_attrs['editable']
+        for group in editable_old:
+            for opt in editable_old[group]:
+                key = '.'.join([group, opt])
+                if key in skipped_options or 'metadata' in key:
+                    continue
+                else:
+                    old_val = editable_old[group][opt]['value']
+                    new_val = editable_new[group][opt]['value']
+                    if old_val != new_val:
+                        logger.debug(
+                            "Failed key old value: {}".format(old_val))
+                        logger.debug(
+                            "Failed key new value: {}".format(new_val))
+                        return False
+        return True
 
     def change_netconfig_task(self, fail=True):
         ssh_manager = self.ssh_manager
@@ -203,3 +286,113 @@ class UnlockSettingsTabStates(TestBasic):
         self.run_ostf()
         self.show_step(11)
         self.env.make_snapshot("failed_deploy_unlock")
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["unlock_settings_tab_positive"])
+    @log_snapshot_after_test
+    def unlock_settings_tab_positive(self):
+        """
+
+        Scenario:
+            1. Create cluster
+            2. Download default cluster settings
+            3. Create custom_config and upload it to cluster
+            4. Add 3 nodes with controller role and 2 nodes with compute role
+            5. Deploy the cluster
+            6. Stop deployment process
+            7. Get current settings
+            8. Change and save them (that means settings are unlocked)
+            9. Redeploy cluster via api
+            10. Get cluster and network settings via api (api load deployed)
+            11. Compare settings from step 8 and 10 (them must be equal)
+            12. Get default settings via api (load defaults)
+            13. Compare settings from step 2 and 13 (them must be equal)
+            14. Redeploy cluster
+            15. Stop deployment process
+            16. Redeploy cluster
+            17. Run OSTF
+
+        Duration 50m
+        Snapshot unlock_settings_tab_positive
+
+        """
+        self.env.revert_snapshot("ready_with_5_slaves")
+        self.show_step(1)
+        self.create_cluster()
+        self.show_step(2)
+        default_config = self.get_cluster_attributes()
+        self.show_step(3)
+        new_config = copy.deepcopy(default_config)
+        editable = new_config['editable']
+        editable['access']['email']['value'] = 'custom@localhost'
+        editable[
+            'neutron_advanced_configuration']['neutron_qos']['value'] = True
+        editable['common']['puppet_debug']['value'] = False
+        self.update_cluster_attributes(new_config)
+        self.show_step(4)
+        self.update_nodes(
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute'],
+                'slave-05': ['compute']
+            }
+        )
+        self.show_step(5)
+        self.fuel_web.deploy_cluster_wait_progress(cluster_id=self.cluster_id,
+                                                   progress=10)
+        self.show_step(6)
+        self.fuel_web.stop_deployment_wait(self.cluster_id)
+        self.show_step(7)
+        new_cluster_settings = self.get_cluster_attributes()
+        self.show_step(8)
+        editable = new_cluster_settings['editable']
+        editable['access']['email']['value'] = 'custom2@localhost'
+        editable['public_ssl']['horizon']['value'] = False
+        editable['public_ssl']['services']['value'] = False
+        self.update_cluster_attributes(new_cluster_settings)
+        current_network_settings = self.get_networks()
+        networking_parameters = \
+            current_network_settings['networking_parameters']
+        networking_parameters['vlan_range'] = [1015, 1030]
+        networking_parameters['gre_id_range'] = [3, 65535]
+        current_networks = current_network_settings['networks']
+        for network in current_networks:
+            if network['cidr'] is not None and network['name'] != 'public':
+                cidr = IPNetwork(network['cidr'])
+                cidr.prefixlen += 1
+                network['cidr'] = str(cidr)
+                network['ip_ranges'][0][1] = str(cidr[-2])
+        self.update_network_settings(
+            networking_parameters=networking_parameters,
+            networks=current_networks)
+        self.show_step(9)
+        self.fuel_web.deploy_cluster_changes_wait(
+            self.cluster_id, new_cluster_settings)
+        self.show_step(10)
+        deployed_settings = self.get_deployed_cluster_attributes()
+        deployed_net_conf = self.get_deployed_network_configuration()
+        self.show_step(11)
+        assert_equal(new_cluster_settings, deployed_settings,
+                     message="Cluster settings before deploy"
+                             " are not equal with deployed settings")
+        assert_true(self.compare_networks(
+            current_network_settings, deployed_net_conf),
+            message='Network settings comparing failed')
+        self.show_step(12)
+        default_settings = self.get_default_cluster_settings()
+        self.show_step(13)
+        assert_true(
+            self.compare_settings(default_config, default_settings),
+            message='Default settings are not equal')
+        self.show_step(14)
+        self.fuel_web.redeploy_cluster_changes_wait_progress(
+            cluster_id=self.cluster_id, progress=30)
+        self.show_step(15)
+        self.fuel_web.stop_deployment_wait(self.cluster_id)
+        self.show_step(16)
+        self.deploy_cluster()
+        self.show_step(17)
+        self.run_ostf()
+        self.env.make_snapshot("unlock_settings_tab_positive")
