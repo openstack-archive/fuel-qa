2518d0682a7b04c710d1f9e519a5c570ca599ee0
Anton Studenov <astudenov@mirantis.com>
2016-06-08 12:58:59 +0300

    Move testrail scripts to /reporter dir
    This is a first step for moving it to separate repo
    Dummy files were added to previous place
    
    blueprint: package-testrail-reporter
    Change-Id: I69d0e690a8e7d3ecd0b4da6cfbc0da11f63c0dd2

---
+++ reporter/setup.py
@@ -0,0 +1,43 @@
+#    Copyright 2013 - 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='reporter',
+    version='1.0.0',
+    description='Library for creating and publishing reports',
+    author='Mirantis, Inc.',
+    author_email='product@mirantis.com',
+    url='http://mirantis.com',
+    keywords='fuel universal reporter',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fueltestrail_reporter = reporter.testrail.report:main',
+            'fueltestrail_statistics = '
+                'reporter.testrail.generate_statistics:main',
+            'fueltestrail_failure_grouping = '
+                'reporter.testrail.generate_failure_group_statistics:main',
+            'fueltestrail_upload_cases = '
+                'reporter.testrail.upload_cases_description:main',
+            'fueltestrail_upload_suite = '
+                'reporter.testrail.upload_tempest_test_suite:main',
+        ]
+    },
+)
