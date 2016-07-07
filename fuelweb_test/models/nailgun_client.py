5ae4f80a4a626a838cb60a1c5024781783a60f06
Alisa Tselovalnikova <atselovalnikova@mirantis.com>
2016-07-07 15:14:41 +0300

    Test compute rebooting after migration of fuel master
    Test are checking functionality of Fuel Master node after the hard and warm
    restarts of compute node where Fuel Master was migrated to.
    
    Change-Id: I97b7743045254a76b0d2ccf67b42629d18d07738
    Closes-Bug: #1581524

--- fuelweb_test/models/nailgun_client.py
+++ fuelweb_test/models/nailgun_client.py
@@ -123,6 +123,10 @@ class NailgunClient(object):
         return self._delete(url="/clusters/{}/".format(cluster_id)).json()
 
     @logwrap
+    def get_node_by_id(self, node_id):
+        return self._get(url="/nodes/{}".format(node_id)).json()
+
+    @logwrap
     def update_node(self, node_id, data):
         return self._put(
             "/nodes/{}/".format(node_id), json=data
