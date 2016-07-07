0cbedf7778c7fa268891aae1c2c4a04fa6026205
Sergey Ryabin <sryabin@mirantis.com>
2016-07-07 14:53:21 +0300

    Add more flexibility to octane patcher
    Change-Id: I4c6e308de3106c9d89324a0e8f52d52340090872

--- fuelweb_test/tests/tests_upgrade/octane_patcher.sh
+++ fuelweb_test/tests/tests_upgrade/octane_patcher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-set -e
-cd /usr/lib/python*/site-packages/octane
+set -ex
+cd $1
+shift
 echo -n $* | xargs -d" " -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | grep current_revision | cut -d: -f2 | sed "s/[, ]//g" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch -p2"'
