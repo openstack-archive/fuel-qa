66f499ba9b8d0dc0bf838dc1a3ac03512333b72c
Alisa Tselovalnikova <atselovalnikova@mirantis.com>
2016-07-07 15:14:41 +0300

    Test compute rebooting after migration of fuel master
    Test are checking functionality of Fuel Master node after the hard and warm
    restarts of compute node where Fuel Master was migrated to.
    
    Change-Id: I97b7743045254a76b0d2ccf67b42629d18d07738
    Closes-Bug: #1581524

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1992,13 +1992,22 @@ class FuelWebClient29(object):
 
         return ip_ranges, expected_ips
 
+    @logwrap
+    def get_nailgun_node_online_status(self, node):
+        return self.client.get_node_by_id(node['id'])['online']
+
+    def get_devops_node_online_status(self, node):
+        return self.get_nailgun_node_online_status(
+            self.get_nailgun_node_by_devops_node(node))
+
     def warm_shutdown_nodes(self, devops_nodes):
         logger.info('Shutting down (warm) nodes %s',
                     [n.name for n in devops_nodes])
         for node in devops_nodes:
             logger.debug('Shutdown node %s', node.name)
-            with self.get_ssh_for_node(node.name) as remote:
-                remote.check_call('/sbin/shutdown -Ph now')
+            nailgun_node = self.get_nailgun_node_by_devops_node(node)
+            self.ssh_manager.execute_on_remote(ip=nailgun_node['ip'],
+                                               cmd='/sbin/shutdown -Ph now')
 
         for node in devops_nodes:
             self.wait_node_is_offline(node)
@@ -2015,6 +2024,12 @@ class FuelWebClient29(object):
                     [n.name for n in devops_nodes])
         self.warm_shutdown_nodes(devops_nodes)
         self.warm_start_nodes(devops_nodes)
+
+    def warm_reboot_ips(self, ips):
+        for ip in ips:
+            logger.debug('Reboot (warm restart) ip {0}'.format(ip))
+            self.ssh_manager.execute_on_remote(ip=ip,
+                                               cmd='/sbin/shutdown -r now')
 
     def cold_restart_nodes(self, devops_nodes,
                            wait_offline=True, wait_online=True,
@@ -2159,6 +2174,14 @@ class FuelWebClient29(object):
             self.wait_node_is_online(node, timeout=timeout)
 
     @logwrap
+    def wait_cluster_nodes_get_online_state(self, cluster_id,
+                                            timeout=4 * 60):
+        for node in self.client.list_cluster_nodes(cluster_id):
+            wait(lambda: self.get_nailgun_node_online_status(node),
+                 timeout=timeout, timeout_msg='Node {0} has not become'
+                                              ' online'.format(node['name']))
+
+    @logwrap
     def wait_mysql_galera_is_up(self, node_names, timeout=60 * 4):
         def _get_galera_status(_remote):
             cmd = ("mysql --connect_timeout=5 -sse \"SELECT VARIABLE_VALUE "
@@ -2185,6 +2208,20 @@ class FuelWebClient29(object):
                         "MySQL Galera isn't ready on {0}: {1}".format(
                             node_name, _get_galera_status(remote)))
         return True
+
+    @logwrap
+    def mcollective_nodes_online(self, cluster_id):
+        nodes_uids = set([str(n['id']) for n in
+                          self.client.list_cluster_nodes(cluster_id)])
+        # 'mco find' returns '1' exit code if rabbitmq is not ready
+        out = self.ssh_manager.execute_on_remote(
+            ip=self.ssh_manager.admin_ip,
+            cmd='mco find', assert_ec_equal=[0, 1])['stdout_str']
+        ready_nodes_uids = set(out.split('\n'))
+        unavailable_nodes = nodes_uids - ready_nodes_uids
+        logger.debug('Nodes {0} are not reacheable via'
+                     ' mcollective'.format(unavailable_nodes))
+        return not unavailable_nodes
 
     @logwrap
     def wait_cinder_is_up(self, node_names):
