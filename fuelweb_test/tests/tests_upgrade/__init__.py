0da597f3c5396d9f3902bef3374e5ac93f888005
Alexey Stepanov <astepanov@mirantis.com>
2016-07-18 15:56:52 +0300

    OS Upgrade tests: separate helpers
    1. test_os_upgrade is moved under test_os_upgrade
    2. main code is moved to separate place for excluding copy-paste
      in new tests.
    3. Fixed logging due to unreadable output
    4. Switch to check_call -> remove pain of cherry-picking
    5. Use octane-cleanup
    
    Closes-bug: #1612236
    Change-Id: Ida7f5901f070a9ef507ce6027fd2618b8617d89f

--- fuelweb_test/tests/tests_upgrade/__init__.py
+++ fuelweb_test/tests/tests_upgrade/__init__.py
@@ -18,10 +18,12 @@ from fuelweb_test.tests.tests_upgrade import test_data_driven_upgrade  # noqa
 from fuelweb_test.tests.tests_upgrade import test_data_driven_upgrade_net_tmpl  # noqa
 from fuelweb_test.tests.tests_upgrade import test_data_driven_upgrade_plugin  # noqa
 from fuelweb_test.tests.tests_upgrade import upgrader_tool  # noqa
+from fuelweb_test.tests.tests_upgrade import test_os_upgrade  # noqa
 
 __all__ = [
     'test_data_driven_upgrade',
     'test_data_driven_upgrade_net_tmpl',
     'test_data_driven_upgrade_plugin',
-    'upgrader_tool'
+    'upgrader_tool',
+    'test_os_upgrade'
 ]
