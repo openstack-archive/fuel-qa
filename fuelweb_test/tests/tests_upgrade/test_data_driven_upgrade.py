209de14d6b88cfdabbbd2121cbd7acf80d7f2afc
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-07-28 13:52:35 +0300

    Add missing timeout parameter
    Cloud removing is not fast operation - default timeout is not enough
    
    Change-Id: Ib39defb4b2dd4686c4afe10ee18d4e0c787ec354

--- fuelweb_test/tests/tests_upgrade/test_data_driven_upgrade.py
+++ fuelweb_test/tests/tests_upgrade/test_data_driven_upgrade.py
@@ -535,7 +535,8 @@ class UpgradeSmoke(DataDrivenUpgradeBase):
         )
         self.fuel_web.client.delete_cluster(cluster_id)
         wait(lambda: not any([cluster['id'] == cluster_id for cluster in
-                              self.fuel_web.client.list_clusters()]))
+                              self.fuel_web.client.list_clusters()]),
+             timeout=10 * 60)
         self.env.bootstrap_nodes(devops_nodes)
 
         self.show_step(3)
