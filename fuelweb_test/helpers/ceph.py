0144d6700c78e34b48ae60ec9a8ee2c5a51730bb
Dmitry Teselkin <dteselkin@mirantis.com>
2016-07-29 19:13:22 +0300

    Add support for 'jewel' ceph on Xenial
    1. Replace ceph-all service check
    
      jewel in xenial doesn't provide ceph-all meta-service,
      this commit replaces it with checks of particular services.
    
    2. Update another ceph-related service management command.
    
    blueprint: mos-xenial
    
    Change-Id: I8fcc1e763c2da984fad96cc58ebcd72d5de53ee3

--- fuelweb_test/helpers/ceph.py
+++ fuelweb_test/helpers/ceph.py
@@ -21,6 +21,7 @@ from fuelweb_test.settings import DNS_SUFFIX
 from fuelweb_test.settings import OPENSTACK_RELEASE
 from fuelweb_test.settings import OPENSTACK_RELEASE_CENTOS
 from fuelweb_test.settings import OPENSTACK_RELEASE_UBUNTU
+from fuelweb_test.settings import UBUNTU_SERVICE_PROVIDER
 
 
 def start_monitor(remote):
@@ -136,13 +137,42 @@ def check_disks(remote, nodes_ids):
 
 
 def check_service_ready(remote, exit_code=0):
+    cmds = []
     if OPENSTACK_RELEASE_UBUNTU in OPENSTACK_RELEASE:
-        cmd = 'service ceph-all status'
+        if UBUNTU_SERVICE_PROVIDER == 'systemd':
+            # Gather services on remote node
+            cmd = 'systemctl show --property=Id ceph-mon*service '\
+                  'ceph-osd*service ceph-radosgw*service'
+            result = remote.execute(cmd)
+            if result['exit_code'] != 0:
+                return False
+
+            ceph_services = []
+            for line in result['stdout']:
+                try:
+                    (key, value) = line.strip().split('=', 1)
+                    ceph_services.append(value)
+                except:
+                    pass
+            # Now create commands to check service health
+            # 'systemctl is-failed' returns 0 if any service that matches
+            # the pattern has failed, otherwise error code is non-zero.
+            # That means we have to invert exit code to follow the logic
+            # 'return 0 if every service is running'
+            for service in ceph_services:
+                cmds.append('systemctl is-active -q {}'.format(service))
+        else:
+            cmds.append('service ceph-all status')
     else:
-        cmd = 'service ceph status'
-    if remote.execute(cmd)['exit_code'] == exit_code:
-        return True
-    return False
+        cmds.append('service ceph status')
+
+    if not cmds:
+        return False
+
+    for cmd in cmds:
+        if remote.execute(cmd)['exit_code'] != exit_code:
+            return False
+    return True
 
 
 def health_overall_status(remote):
