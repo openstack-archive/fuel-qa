8d5ba49252c560e7036807294b99451cc0919cf3
Mikhail Samoylov <msamoylov@mirantis.com>
2016-08-02 14:08:12 +0300

    Add insecure option to glance client.
    Closes bug: 1605136
    
    Change-Id: I3f2c7231671e7ce2aa08b01c7250c8631d426e64

--- fuelweb_test/helpers/common.py
+++ fuelweb_test/helpers/common.py
@@ -86,11 +86,14 @@ class Common(object):
     @property
     def glance(self):
         endpoint = self.__make_endpoint(
-            self._get_url_for_svc(service_type='image'))
-        return GlanceClient(
-            session=self.keystone_session,
-            endpoint=endpoint,
-            endpoint_override=endpoint)
+                self._get_url_for_svc(service_type='image'))
+        # return GlanceClient(
+        #     session=self.keystone_session,
+        #     endpoint=endpoint,
+        #     endpoint_override=endpoint,
+        #     insecure=True)
+        return GlanceClient(endpoint=endpoint, endpoint_override=endpoint,
+                            insecure=True, auth_token=self.keystone.auth_token)
 
     @property
     def neutron(self):
