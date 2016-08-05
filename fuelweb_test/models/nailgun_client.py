85de32586c8ce540c88650904bb393ade64897c2
Mikhail Samoylov <msamoylov@mirantis.com>
2016-08-05 12:39:11 +0300

    Add json method for return methods in configdb smoke test.
    Closes bug: 1609811
    
    Change-Id: I38341e48f75c9408db7677d132e9436508cc5fc2

--- fuelweb_test/models/nailgun_client.py
+++ fuelweb_test/models/nailgun_client.py
@@ -855,7 +855,8 @@ class NailgunClient(object):
                    '{env_id}/resources/{resource}' \
                    '/values'.format(env_id=env_id, resource=resource_name)
         endpoint = endpoint + '?effective' if effective else endpoint
-        return self._get(endpoint)
+        logger.info(self._get(endpoint).json())
+        return self._get(endpoint).json()
 
     @logwrap
     def put_global_resource_value(self, env_id, resource, data):
@@ -900,7 +901,7 @@ class NailgunClient(object):
                                     node_id=node_id)
         endpoint = endpoint + '?effective' if effective else endpoint
 
-        return self._get(endpoint)
+        return self._get(endpoint).json()
 
     @logwrap
     def get_node_resource_name_value(self, env_id, resource_name, node_id,
