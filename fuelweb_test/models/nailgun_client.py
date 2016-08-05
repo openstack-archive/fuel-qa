1756b561128dab21f02a6fa2a83920d9c2d29895
Mikhail Samoylov <msamoylov@mirantis.com>
2016-08-05 12:39:11 +0300

    Add json method for return methods in configdb smoke test. Status code 308 was added to the tuple redirects for keystone session.
    Closes bug: 1609811
    
    Change-Id: I38341e48f75c9408db7677d132e9436508cc5fc2

--- fuelweb_test/models/nailgun_client.py
+++ fuelweb_test/models/nailgun_client.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import json
 from warnings import warn
 
 from keystoneauth1.identity import V2Password
@@ -839,6 +840,7 @@ class NailgunClient(object):
                    '{env_id}/resources/{resource}' \
                    '/values'.format(env_id=env_id, resource=resource_id)
         endpoint = endpoint + '?effective' if effective else endpoint
+
         return self._get(endpoint).json()
 
     @logwrap
@@ -855,7 +857,8 @@ class NailgunClient(object):
                    '{env_id}/resources/{resource}' \
                    '/values'.format(env_id=env_id, resource=resource_name)
         endpoint = endpoint + '?effective' if effective else endpoint
-        return self._get(endpoint)
+
+        return self._get(endpoint).json()
 
     @logwrap
     def put_global_resource_value(self, env_id, resource, data):
@@ -900,7 +903,7 @@ class NailgunClient(object):
                                     node_id=node_id)
         endpoint = endpoint + '?effective' if effective else endpoint
 
-        return self._get(endpoint)
+        return self._get(endpoint).json()
 
     @logwrap
     def get_node_resource_name_value(self, env_id, resource_name, node_id,
@@ -919,7 +922,7 @@ class NailgunClient(object):
                                     node_id=node_id)
         endpoint = endpoint + '?effective' if effective else endpoint
 
-        return self._get(endpoint)
+        return self._get(endpoint).json()
 
     @logwrap
     def put_node_resource_value(self, env_id, resource, node_id, data):
