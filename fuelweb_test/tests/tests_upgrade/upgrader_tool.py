da0d06bd2d8cbdaf27c3808dfc26e71a05a27ed1
Alexey Stepanov <astepanov@mirantis.com>
2016-08-12 10:20:42 +0300

    Upgrade tests will use modern SSHClient API
    run_on_remote and run_on_remote_get_results eliminated
    SSHClient is memorized, so cache is not required
    
    Change-Id: I0b364ace4e9e198cda6a961b31a16097c70d07a0

--- fuelweb_test/tests/tests_upgrade/upgrader_tool.py
+++ fuelweb_test/tests/tests_upgrade/upgrader_tool.py
@@ -7,7 +7,6 @@ from proboscis.asserts import assert_true
 
 from fuelweb_test import settings, logger
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
-from fuelweb_test.helpers.utils import run_on_remote_get_results
 from fuelweb_test.tests.tests_upgrade.test_data_driven_upgrade_base import \
     DataDrivenUpgradeBase
 
@@ -102,7 +101,9 @@ class UpgradeCustom(DataDrivenUpgradeBase):
         cmd = "cd {} && ".format(self.tarball_remote_dir)
         cmd += "tar -xpvf" if ext.endswith("tar") else "lrzuntar"
 
-        run_on_remote_get_results(self.admin_remote, cmd)
+        # pylint: disable=no-member
+        self.admin_remote.check_call(cmd)
+        # pylint: enable=no-member
         cmd = "sh {} --no-rollback --password {}".format(
             os.path.join(self.tarball_remote_dir, "upgrade.sh"),
             settings.KEYSTONE_CREDS['password'])
@@ -115,7 +116,9 @@ class UpgradeCustom(DataDrivenUpgradeBase):
 
         signal.signal(signal.SIGALRM, handler)
         signal.alarm(60 * 60)
-        run_on_remote_get_results(self.admin_remote, cmd)
+        # pylint: disable=no-member
+        self.admin_remote.check_call(cmd)
+        # pylint: enable=no-member
         signal.alarm(0)
 
         self.env.make_snapshot(self.restore_snapshot_name, is_make=True)
