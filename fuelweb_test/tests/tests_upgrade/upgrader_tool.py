902719210889792936a7f0f2aaed8a518cc99a2b
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-08-15 14:59:43 +0300

    Enhance upgrader tool with all-in-one yaml definition
    Its difficult to use several files for one procedure so lets combine the data.
    Yaml file was modified for including several steps for upgrade tests modification
    
    Change-Id: I8cfe08299098bf890e032d93439e936f5f6b9c86

--- fuelweb_test/tests/tests_upgrade/upgrader_tool.py
+++ fuelweb_test/tests/tests_upgrade/upgrader_tool.py
@@ -1,10 +1,29 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from __future__ import unicode_literals
 
+# pylint: disable=import-error
+# pylint: disable=no-name-in-module
+from distutils.version import StrictVersion
+# pylint: enable=no-name-in-module
+# pylint: enable=import-error
 import os
 
 from devops.helpers.templates import yaml_template_load
-from proboscis import test
-from proboscis.asserts import assert_true
+from proboscis import test, SkipTest
+from proboscis.asserts import assert_true, assert_equal
 
 from fuelweb_test import settings, logger
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
@@ -14,7 +33,6 @@ from fuelweb_test.tests.tests_upgrade.test_data_driven_upgrade_base import \
 
 @test
 class UpgradeCustom(DataDrivenUpgradeBase):
-
     def __init__(self):
         super(UpgradeCustom, self).__init__()
         data = yaml_template_load(
@@ -22,16 +40,24 @@ class UpgradeCustom(DataDrivenUpgradeBase):
         self.upgrade_data = data
 
         logger.debug("Got following data from upgrade template:")
-        logger.debug(''.join(
-            ["{}:{}".format(key, value) for key, value in data.items()]))
+        for step in data:
+            logger.debug("\n".join(
+                ["{}:{}".format(key, value) for key, value in step.items()]))
 
-        self.source_snapshot_name = data["source_snapshot_name"]
-        self.backup_snapshot_name = data["backup_snapshot_name"]
-        self.restore_snapshot_name = data["restore_snapshot_name"]
-        self.tarball_remote_dir = data["tarball_remote_dir"]
-
-        self.backup_name = data["backup_name"]
-        self.repos_backup_name = data["repos_backup_name"]
+    def _get_current_step(self):
+        for item in self.upgrade_data:
+            if item['action'] == 'backup':
+                if self.env.d_env.has_snapshot(item['backup_snapshot_name']):
+                    continue
+                else:
+                    return item
+            elif item['action'] == 'restore':
+                if self.env.d_env.has_snapshot(item['restore_snapshot_name']):
+                    continue
+                else:
+                    return item
+        raise SkipTest("All steps are executed, all snapshots are exists; "
+                       "Nothing to do")
 
     @test(groups=['upgrade_custom_backup'])
     @log_snapshot_after_test
@@ -43,10 +69,30 @@ class UpgradeCustom(DataDrivenUpgradeBase):
         2. Create backup file using 'octane fuel-backup'
         3. Download the backup to the host
         """
-        self.check_run(self.backup_snapshot_name)
-        assert_true(self.source_snapshot_name)
-        self.env.revert_snapshot(self.source_snapshot_name)
+        current_step = self._get_current_step()
+        logger.info("Current step: {}".format(current_step['name']))
+        assert_equal(
+            current_step['action'], 'backup',
+            "Steps order incorrect! {!r} should be 'backup'".format(
+                current_step['action']))
 
+        self.source_snapshot_name = current_step["source_snapshot_name"]
+        self.backup_snapshot_name = current_step["backup_snapshot_name"]
+
+        self.backup_name = current_step["backup_name"]
+        self.repos_backup_name = current_step["repos_backup_name"]
+
+        self.revert_source()
+        assert_equal(
+            StrictVersion(current_step['fuel_version']),
+            self.fuel_version,
+            "Wrong fuel version in current step; "
+            "should be {!r}, actual {!r}".format(
+                StrictVersion(current_step['fuel_version']),
+                self.fuel_version))
+        # clean up existing files for avoiding "No space left"
+        self.env.d_env.get_admin_remote().check_call(
+            "rm -rf {}".format(self.remote_dir_for_backups))
         self.do_backup(self.backup_path, self.local_path,
                        self.repos_backup_path, self.repos_local_path)
         self.env.make_snapshot(self.backup_snapshot_name, is_make=True)
@@ -64,34 +110,60 @@ class UpgradeCustom(DataDrivenUpgradeBase):
         5. Restore master node using 'octane fuel-restore'
 
         """
+        current_step = self._get_current_step()
+        assert_equal(
+            current_step['action'], 'restore',
+            "Steps order incorrect! {!r} should be 'restore'".format(
+                current_step['action']))
+        self.backup_snapshot_name = current_step["backup_snapshot_name"]
+        self.restore_snapshot_name = current_step["restore_snapshot_name"]
 
-        self.check_run(self.restore_snapshot_name)
+        self.backup_name = current_step["backup_name"]
+        self.repos_backup_name = current_step["repos_backup_name"]
+
+        self.show_step(1)
+        self.revert_backup()
+
+        assert_equal(
+            StrictVersion(current_step['source_fuel_version']),
+            self.fuel_version,
+            "Wrong fuel version in current step; "
+            "should be {!r}, actual {!r}".format(
+                StrictVersion(current_step['source_fuel_version']),
+                self.fuel_version))
+
         assert_true(os.path.exists(self.repos_local_path))
         assert_true(os.path.exists(self.local_path))
 
-        self.show_step(1)
-        assert_true(self.env.revert_snapshot(self.backup_snapshot_name))
         self.show_step(2)
         post_reinstall_snapshot = "post_reinstall_" + self.backup_snapshot_name
         if not self.env.d_env.has_snapshot(post_reinstall_snapshot):
             self.reinstall_master_node()
             self.env.make_snapshot(post_reinstall_snapshot)
-            self.env.d_env.resume()
         else:
             self.env.d_env.revert(post_reinstall_snapshot)
-            self.env.d_env.resume()
+        self.env.d_env.resume()
         self.show_step(3)
         self.show_step(4)
         self.show_step(5)
         self.do_restore(self.backup_path, self.local_path,
                         self.repos_backup_path, self.repos_local_path)
+        assert_equal(
+            StrictVersion(current_step['target_fuel_version']),
+            self.fuel_version,
+            "Wrong fuel version in current step; "
+            "should be {!r}, actual {!r}".format(
+                StrictVersion(current_step['target_fuel_version']),
+                self.fuel_version))
+
         self.env.make_snapshot(self.restore_snapshot_name, is_make=True)
         self.cleanup()
 
-    @test(groups=['upgrade_custom_tarball'])
+    @test(groups=['upgrade_custom_tarball'], enabled=False)
     @log_snapshot_after_test
     def upgrade_custom_tarball(self):
         """Upgrade master node via tarball"""
+        # TODO(vkhlyunev): revive this test when 6.0-8.0 will be implemented
         self.check_run(self.backup_snapshot_name)
         assert_true(self.source_snapshot_name)
         self.env.revert_snapshot(self.source_snapshot_name)
