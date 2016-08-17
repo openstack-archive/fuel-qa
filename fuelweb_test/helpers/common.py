86698164347fd8afc93c447ee0a429b9248648d1
Alexey Stepanov <astepanov@mirantis.com>
2016-08-17 13:21:25 +0300

    Do not provide endpoint for glance client
    Do not provide endpoint for glance client
    GlanceClient without endpoint could not start without session
    and we using session only
    Closes-bug: #1605136
    
    Change-Id: Ie5984b8fb5e5f531d2f22f19bedf2ac0eba0a655
    (cherry picked from commit a81ed1ba10ddfcb99a3fd7dedc35f1a5b455aef8)

--- fuelweb_test/helpers/common.py
+++ fuelweb_test/helpers/common.py
@@ -87,7 +87,6 @@ class Common(object):
             self._get_url_for_svc(service_type='image'))
         return GlanceClient(
             session=self.keystone_session,
-            endpoint=endpoint,
             endpoint_override=endpoint)
 
     @property
