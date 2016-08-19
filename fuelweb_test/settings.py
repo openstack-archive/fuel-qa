2c92b345b897de9a0223d44ce517f3b52a4cb568
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-08-19 19:00:25 +0300

    Use predictable step detection in upgrade custom tool
    Right now the tool does not working from the middle after re-launch.
    We have to specify the step for each run of tests
    
    Change-Id: I7203b597c8320d68b5cd412e99c84dfb29249ba5

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -429,11 +429,13 @@ MAKE_SNAPSHOT = get_var_as_bool('MAKE_SNAPSHOT', False)
 FUEL_SETTINGS_YAML = os.environ.get('FUEL_SETTINGS_YAML',
                                     '/etc/fuel/astute.yaml')
 
-UPGRADE_TEST_TEMPLATE = os.environ.get("UPGRADE_TEST_TEMPLATE")
-TARBALL_PATH = os.environ.get('TARBALL_PATH')
-
 # Full path to the custom fuel setting yaml
 CUSTOM_FUEL_SETTING_YAML = os.environ.get('CUSTOM_FUEL_SETTING_YAML', None)
+
+# Upgrade-related variables
+UPGRADE_TEST_TEMPLATE = os.environ.get("UPGRADE_TEST_TEMPLATE")
+UPGRADE_CUSTOM_STEP_NAME = os.environ.get("UPGRADE_CUSTOM_STEP_NAME")
+TARBALL_PATH = os.environ.get('TARBALL_PATH')
 
 UPGRADE_FUEL_FROM = os.environ.get('UPGRADE_FUEL_FROM', '8.0')
 UPGRADE_FUEL_TO = os.environ.get('UPGRADE_FUEL_TO', '9.0')
@@ -445,6 +447,7 @@ UPGRADE_BACKUP_FILES_LOCAL_DIR = os.environ.get(
         os.path.curdir, "..", "backup_storage"))
 UPGRADE_BACKUP_FILES_REMOTE_DIR = os.environ.get(
     'UPGRADE_BACKUP_FILES_REMOTE_DIR', "/var/upgrade/backups")
+# End of upgrade-related variables
 
 SNAPSHOT = os.environ.get('SNAPSHOT', '')
 # For 5.1.1 we have 2 releases in tarball and should specify what we need
