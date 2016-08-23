57807f8e003403510df7b918e9f99ff4f0eabfd0
Alexandr Kostrikov <akostrikov@mirantis.com>
2016-08-23 15:22:15 +0300

    Implement configdb coverage
    There are new cli actions which should be covered
    
    Change-Id: I531733b296765b0d472644fbe3739e03c0475fca
    Closes-bug: #1616047

--- fuelweb_test/helpers/utils.py
+++ fuelweb_test/helpers/utils.py
@@ -51,6 +51,162 @@ from fuelweb_test.helpers.ssh_manager import SSHManager
 from gates_tests.helpers import exceptions
 
 
+class FuelAccessParams(object):
+    """Value object to represent and map yaml file values of fuel master node
+    access to openrc file.
+    Should not use any api."""
+
+    def __init__(self,
+                 tls_service_enabled=False,
+                 tls_keystone_enabled=False):
+        self._username = None
+        self._password = None
+        self._project = None
+        self._service_address = None
+        self._service_port = None
+        self._keystone_address = None
+        self._keystone_port = None
+        self._tls_service_enabled = tls_service_enabled
+        self._tls_keystone_enabled = tls_keystone_enabled
+
+    @property
+    def username(self):
+        # type: () -> basestring
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        # type: (basestring) -> basestring
+        self._username = value
+
+    @property
+    def password(self):
+        # type: () -> basestring
+        return self._username
+
+    @password.setter
+    def password(self, value):
+        # type: (basestring) -> basestring
+        self._password = value
+
+    @property
+    def project(self):
+        # type: () -> basestring
+        return self._project
+
+    @project.setter
+    def project(self, value):
+        # type: (basestring) -> basestring
+        self._project = value
+
+    @property
+    def service_address(self):
+        # type: () -> basestring
+        return self._service_address
+
+    @service_address.setter
+    def service_address(self, value):
+        # type: (basestring) -> basestring
+        self._service_address = value
+
+    @property
+    def service_port(self):
+        # type: () -> basestring
+        return self._service_port
+
+    @service_port.setter
+    def service_port(self, value):
+        # type: (basestring) -> basestring
+        self._service_port = value
+
+    @property
+    def keystone_address(self):
+        # type: () -> basestring
+        address = self.service_address
+        if self._keystone_address:
+            address = self._keystone_address
+        return address
+
+    @keystone_address.setter
+    def keystone_address(self, value):
+        # type: (basestring) -> basestring
+        self._keystone_address = value
+
+    @property
+    def keystone_port(self):
+        # type: () -> basestring
+        return self._keystone_port
+
+    @keystone_port.setter
+    def keystone_port(self, value):
+        # type: (basestring) -> basestring
+        self._keystone_port = value
+
+    @property
+    def os_auth_url(self):
+        # type: () -> basestring
+        protocol = 'http'
+        if self._tls_keystone_enabled:
+            protocol = 'https'
+        return "{protocol}://{keystone_address}:{keystone_port}".format(
+            protocol=protocol,
+            keystone_address=self.keystone_address,
+            keystone_port=self.keystone_port
+        )
+
+    @property
+    def service_url(self):
+        # type: () -> basestring
+        protocol = 'http'
+        if self._tls_service_enabled:
+            protocol = 'https'
+        return "{protocol}://{service_address}:{service_port}".format(
+            protocol=protocol,
+            service_address=self.service_address,
+            service_port=self.service_port
+        )
+
+    @property
+    def to_openrc_content(self):
+        """Method to represent access credentials in openrc format.
+
+        :return: string content for openrc file
+        """
+        # type: () -> basestring
+        env_template = ('export OS_USERNAME="{username}"\n'
+                        'export OS_PASSWORD="{password}"\n'
+                        'export OS_TENANT_NAME="{project}"\n'
+                        'export SERVICE_URL="{service_url}"\n'
+                        'export OS_AUTH_URL="{os_auth_url}"\n')
+
+        return env_template.format(
+            username=self.username,
+            password=self.username,
+            project=self.project,
+            service_url=self.service_url,
+            os_auth_url=self.os_auth_url,
+        )
+
+    @staticmethod
+    def from_yaml_params(yaml_content):
+        # type: (dict) -> FuelAccessParams
+        """The method to initialize value object from parsed yaml from
+        master node.
+
+        :param yaml_content: dict from yaml to parse
+        :return: FuelAccessParams instance to work with
+        """
+        access_params = FuelAccessParams()  # type:FuelAccessParams
+        access_params.username = yaml_content['OS_USERNAME']
+        access_params.password = yaml_content['OS_PASSWORD']
+        access_params.project = yaml_content['OS_TENANT_NAME']
+        access_params.service_address = yaml_content['SERVER_ADDRESS']
+        access_params.service_port = yaml_content['SERVER_PORT']
+        access_params.keystone_port = yaml_content['KEYSTONE_PORT']
+
+        return access_params
+
+
 @logwrap
 def get_yaml_to_json(node_ssh, filename):
     cmd = ("python -c 'import sys, yaml, json; json.dump("
@@ -1160,33 +1316,63 @@ def dict_merge(a, b):
 
 
 @logwrap
-def install_configdb(master_node_ip):
+def get_access_config_file(ssh_manager):
+    """Get path to file on master node, which contains access parameters.
+    That can be changed in fuel library/fuel main.
+
+    :param ssh_manager: ssh manager instance
+    :return: string with path to file
+    """
+    return ssh_manager.execute_on_remote(
+        ip=ssh_manager.admin_ip,
+        cmd='ls -1 $HOME/.config/fuel/fuel_client.yaml')['stdout_str']
+
+
+@logwrap
+def install_configdb():
     """ Install ConfigDB extension on master node
 
-    :param master_node_ip: string with fuel master ip address
     :return: None
     """
-    ip = master_node_ip
+    if not settings.PERESTROIKA_REPO:
+        raise exceptions.FuelQAVariableNotSet(
+            'PERESTROIKA_REPO',
+            'http://perestroika-repo-tst.infra.mirantis.net/mos-repos/centos/')
     ssh_manager = SSHManager()
-    asserts.assert_is_not_none(
-        settings.PERESTROIKA_REPO,
-        message='PERESTROIKA_REPO is empty, please set it to correct path'
-    )
-    cmds = ['yum-config-manager --add-repo '
-            '{}'.format(settings.PERESTROIKA_REPO),
+    admin_ip = ssh_manager.admin_ip
+    fuel_config_file = get_access_config_file(ssh_manager)
+    openrc_content = ''
+    with YamlEditor(fuel_config_file, ip=admin_ip) as yaml_editor:
+        openrc_content = FuelAccessParams.from_yaml_params(
+            yaml_editor.content).to_openrc_content
 
-            'yum-config-manager --add-repo {}'.format(
-                settings.PACKAGES_CENTOS),
+    openrc_path = '/root/.openrc'
+    ssh_manager.check_call(
+        admin_ip, 'touch {openrc_path}'.format(openrc_path=openrc_path))
+    with ssh_manager.open_on_remote(admin_ip, openrc_path, 'w') as openrc_file:
+        openrc_file.write(openrc_content)
 
-            'rpm --import {}'.format(settings.MASTER_CENTOS_GPG),
-
-            'yum install -y tuning-box',
-            'nailgun_syncdb',
-            "sudo -u postgres psql -c '\dt' nailgun | grep tuning_box",
-            'service nailgun restart'
-            ]
-    for cmd in cmds:
-        ssh_manager.execute_on_remote(ip=ip, cmd=cmd)
+    commands = [
+        'yum-config-manager --add-repo '
+        '{}'.format(settings.PERESTROIKA_REPO),
+        'yum-config-manager --add-repo {}'.format(settings.PACKAGES_CENTOS),
+        'rpm --import {}'.format(settings.MASTER_CENTOS_GPG),
+        # TODO(akostrikov) Temporary hack to be on the edge.
+        'yum install -y tuning-box git',
+        'yum remove -y tuning-box',
+        'git clone http://github.com/openstack/tuning-box',
+        'cd tuning-box/ && python setup.py install',
+        # TODO(akostrikov) Hack end
+        'nailgun_syncdb',
+        "sudo -u postgres psql -c '\dt' nailgun | grep tuning_box",
+        'service nailgun restart',
+        '. .openrc; openstack service create --name tuning-box config',
+        ('. .openrc; openstack endpoint create'
+         ' --publicurl $SERVICE_URL/api/config'
+         ' --region RegionOne tuning-box;')
+    ]
+    for install_command in commands:
+        ssh_manager.check_call(admin_ip, install_command)
 
 
 # pylint: disable=eval-used
