3129a9dd119188e862c71fc942f170235c5fcacd
Alexey Stepanov <astepanov@mirantis.com>
2016-08-24 09:55:10 +0300

    Drop cidr.numhosts usage: not available in netaddr
    Temporary solution was made on fuel-devops side,
    correct solution: use len(cidr) instead
    
    Change-Id: I409e4af545fdc36ded4fc7f5a5f8f0da3a000d0c

--- fuelweb_test/tests/test_multiple_networks.py
+++ fuelweb_test/tests/test_multiple_networks.py
@@ -550,7 +550,7 @@ class TestMultipleClusterNets(TestNetworkTemplatesBase):
         # floating range
         public2_cidr = self.env.d_env.get_network(name='public2').ip
         new_settings_float = {
-            'floating_ranges': [[str(public2_cidr[public2_cidr.numhosts // 2]),
+            'floating_ranges': [[str(public2_cidr[len(public2_cidr) // 2]),
                                  str(public2_cidr[-2])]]
         }
         self.fuel_web.client.update_network(cluster_id, new_settings_float)
