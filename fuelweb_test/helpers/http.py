c59d0b9846a0ce24fdf289cbef94b1f77af3fd10
Alexey Stepanov <astepanov@mirantis.com>
2016-08-25 12:04:50 +0300

    Create initial unittest infrastructure
    Create initial unittest infrastructure:
    .core
    .core._tests
    tox.ini: cover
    add collector client as initial target for refactor and testing
    Blueprint: fuel-qa-self-test
    
    Change-Id: I35207eb7bb45ec20d1b61f28be564097420c9f27

--- fuelweb_test/helpers/http.py
+++ fuelweb_test/helpers/http.py
@@ -1,43 +1,18 @@
-#    Copyright 2013 Mirantis, Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+from __future__ import absolute_import
 
-import json
-# pylint: disable=import-error
-# noinspection PyUnresolvedReferences
-from six.moves.urllib import request
-# pylint: enable=import-error
+from traceback import print_stack
+from warnings import warn
 
+from fuelweb_test import logger
 
-class HTTPClientZabbix(object):
-    """HTTPClientZabbix."""  # TODO documentation
+from core.helpers.http import HTTPClientZabbix
 
-    def __init__(self, url):
-        self.url = url
-        self.opener = request.build_opener(request.HTTPHandler)
+msg = (
+    'fuelweb_test.helpers.http is deprecated and will be dropped '
+    'on 14.09.2016. Please use core.models.collector_client instead'
+)
+warn(msg)
+print_stack()
+logger.critical(msg)
 
-    def get(self, endpoint=None, cookie=None):
-        req = request.Request(self.url + endpoint)
-        if cookie:
-            req.add_header('cookie', cookie)
-        return self.opener.open(req)
-
-    def post(self, endpoint=None, data=None, content_type="text/css",
-             cookie=None):
-        if not data:
-            data = {}
-        req = request.Request(self.url + endpoint, data=json.dumps(data))
-        req.add_header('Content-Type', content_type)
-        if cookie:
-            req.add_header('cookie', cookie)
-        return self.opener.open(req)
+__all__ = ['HTTPClientZabbix']
