d05381ca3f480849127b5de02091f4e1b9c29451
Volodymyr Shypyguzov <vshypyguzov@mirantis.com>
2016-08-30 10:45:12 +0300

    Refactor shutdown_ceph test. Add power_outage_ceph
    + refactor existing shutdown_ceph_for_all test
    + add new power_outage_ceph_for_all test
    
    Related blueprint run-fuel-qa-on-baremetal
    
    Change-Id: I2413370370193b8288beb9851a9306f5c8537d15

--- fuelweb_test/tests/tests_strength/test_failover_group_3.py
+++ fuelweb_test/tests/tests_strength/test_failover_group_3.py
@@ -35,11 +35,109 @@ cirros_auth = SSHAuth(**settings.SSH_IMAGE_CREDENTIALS)
 class FailoverGroup3(TestBasic):
     """FailoverGroup3"""  # TODO documentation
 
+    def mount_volume_and_create_bigfile(self, instance_ip, file_size):
+        cmds = ['sudo sh -c "/usr/sbin/mkfs.ext4 /dev/vdb"',
+                'sudo sh -c "/bin/mount /dev/vdb /mnt"',
+                'sudo sh -c "/usr/bin/nohup'
+                ' /bin/dd if=/dev/zero of=/mnt/bigfile '
+                'bs=1M count={} &"'.format(file_size)]
+
+        with self.fuel_web.get_ssh_for_node("slave-01") as remote:
+            for cmd in cmds:
+                res = remote.execute_through_host(
+                    hostname=instance_ip,
+                    cmd=cmd,
+                    auth=cirros_auth)
+                logger.info('RESULT for {}: {}'.format(
+                    cmd,
+                    utils.pretty_log(res))
+                )
+            logger.info('Wait 7200 untill "dd" ends')
+            for _ in range(720):
+                cmd = 'ps -ef |grep -v grep| grep "dd if" '
+                res = remote.execute_through_host(
+                    hostname=instance_ip,
+                    cmd=cmd,
+                    auth=cirros_auth)
+                if res['exit_code'] != 0:
+                    break
+                time.sleep(10)
+                logger.debug('Wait another 10 sec -'
+                             ' totally waited {} sec'.format(10 * _))
+            else:
+                raise TimeoutError('BigFile has not been'
+                                   ' created yet, after 7200 sec')
+            cmd = 'md5sum /mnt/bigfile'
+            md5s = remote.execute_through_host(
+                hostname=instance_ip,
+                cmd=cmd,
+                auth=cirros_auth)['stdout']
+            return md5s
+
+    def get_ceph_size(self, cluster_id):
+        ceph_nodes = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            cluster_id, ['ceph-osd'])
+        total_ceph_size = 0
+        for node in ceph_nodes:
+            total_ceph_size += \
+                self.fuel_web.get_node_partition_size(node['id'], 'ceph')
+        return int(total_ceph_size)
+
+    def create_ceph_volume(self, cluster_id, volume_size):
+        os = os_actions.OpenStackActions(
+            controller_ip=self.fuel_web.get_public_vip(cluster_id),
+            user='failover', passwd='failover', tenant='failover')
+
+        volume = os.create_volume(size=volume_size)
+
+        logger.info('Created volume: {0}'.format(volume.id))
+
+        return volume
+
+    def attach_volume(self, instance, volume):
+
+        ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+
+        logger.info("Attach volumes")
+        cmd = 'nova volume-attach {srv_id} {volume_id} /dev/vdb'
+
+        self.ssh_manager.execute_on_remote(
+            ip=ip,
+            cmd='. openrc; ' + cmd.format(srv_id=instance.id,
+                                          volume_id=volume.id)
+        )
+
+    def get_hypervisors(self, cluster_id):
+        os = os_actions.OpenStackActions(
+            controller_ip=self.fuel_web.get_public_vip(cluster_id),
+            user='failover', passwd='failover', tenant='failover')
+        return os.get_hypervisors()
+
+    def create_instance_with_floating_ip(self, cluster_id, hypervisor_name):
+        os = os_actions.OpenStackActions(
+            controller_ip=self.fuel_web.get_public_vip(cluster_id),
+            user='failover', passwd='failover', tenant='failover')
+        net_name = self.fuel_web.get_cluster_predefined_networks_name(
+            cluster_id)['private_net']
+
+        instance = os.create_server_for_migration(
+            neutron=True,
+            availability_zone="nova:{0}".format(hypervisor_name),
+            label=net_name
+        )
+        logger.info("New instance {0} created on {1}"
+                    .format(instance.id, hypervisor_name))
+
+        floating_ip = os.assign_floating_ip(instance)
+        logger.info("Floating address {0} associated with instance {1}"
+                    .format(floating_ip.ip, instance.id))
+        return instance, floating_ip
+
     @test(depends_on_groups=['prepare_slaves_9'],
-          groups=['shutdown_ceph_for_all'])
+          groups=['deploy_ha_ceph_3_osd_vxlan'])
     @log_snapshot_after_test
-    def shutdown_ceph_for_all(self):
-        """Shutdown of Neutron Vxlan, ceph for all cluster
+    def deploy_ha_ceph_3_osd_vxlan(self):
+        """Deploy ceph for all cluster with 3 OSD and Neutron Vxlan
 
         Scenario:
             1. Create cluster with Neutron Vxlan, ceph for all,
@@ -49,19 +147,11 @@ class FailoverGroup3(TestBasic):
             4. Deploy cluster
             5. Verify networks
             6. Run OSTF
-            7. Create 2 volumes and 2 instances with attached volumes
-            8. Fill ceph storages up to 30%(15% for each instance)
-            9. Shutdown of all nodes
-            10. Wait 5 minutes
-            11. Start cluster
-            12. Wait until OSTF 'HA' suite passes
-            13. Verify networks
-            14. Run OSTF tests
+            Duration 120m
 
-        Duration 230m
+        Snapshot deploy_ha_ceph_3_osd_vxlan
 
         """
-
         self.env.revert_snapshot('ready_with_9_slaves')
 
         self.show_step(1, initialize=True)
@@ -109,112 +199,65 @@ class FailoverGroup3(TestBasic):
         self.show_step(6)
         self.fuel_web.run_ostf(cluster_id)
 
-        self.show_step(7)
-        os = os_actions.OpenStackActions(
-            controller_ip=self.fuel_web.get_public_vip(cluster_id),
-            user='failover', passwd='failover', tenant='failover')
-        net_name = self.fuel_web.get_cluster_predefined_networks_name(
-            cluster_id)['private_net']
-        hypervisors = os.get_hypervisors()
-        hypervisor_name = hypervisors[0].hypervisor_hostname
-        instance_1 = os.create_server_for_migration(
-            neutron=True,
-            availability_zone="nova:{0}".format(hypervisor_name),
-            label=net_name
-        )
-        logger.info("New instance {0} created on {1}"
-                    .format(instance_1.id, hypervisor_name))
+        self.env.make_snapshot('deploy_ha_ceph_3_osd_vxlan', is_make=True)
 
-        floating_ip_1 = os.assign_floating_ip(instance_1)
+    @test(depends_on_groups=['deploy_ha_ceph_3_osd_vxlan'],
+          groups=['shutdown_ceph_for_all'])
+    @log_snapshot_after_test
+    def shutdown_ceph_for_all(self):
+        """Shutdown of Neutron Vxlan, ceph for all cluster
+
+        Scenario:
+            1. Do preconditions from deploy_ha_ceph_3_osd_vxlan
+            2. Create 2 volumes and 2 instances with attached volumes
+            3. Fill ceph storages up to 30%(15% for each instance)
+            4. Shutdown of all nodes
+            5. Wait 5 minutes
+            6. Start cluster
+            7. Wait until OSTF 'HA' suite passes
+            8. Verify networks
+            9. Run OSTF tests
+            10. Verify bigfile md5 checksum
+
+        Duration 230m
+
+        """
+        self.show_step(1)
+        self.env.revert_snapshot("deploy_ha_ceph_3_osd_vxlan")
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        self.show_step(2)
+        hypervisors = self.get_hypervisors(cluster_id)
+        instance_1, floating_ip_1 = \
+            self.create_instance_with_floating_ip(
+                cluster_id=cluster_id,
+                hypervisor_name=hypervisors[0].hypervisor_hostname)
         logger.info("Floating address {0} associated with instance {1}"
                     .format(floating_ip_1.ip, instance_1.id))
+        instance_2, floating_ip_2 = \
+            self.create_instance_with_floating_ip(
+                cluster_id=cluster_id,
+                hypervisor_name=hypervisors[1].hypervisor_hostname)
 
-        hypervisor_name = hypervisors[1].hypervisor_hostname
-        instance_2 = os.create_server_for_migration(
-            neutron=True,
-            availability_zone="nova:{0}".format(hypervisor_name),
-            label=net_name
+        self.show_step(3)
+        ceph_size = self.get_ceph_size(cluster_id=cluster_id)
+        volume_size = int((ceph_size // 1024) * 0.15) + 1
+        self.create_ceph_volume(cluster_id=cluster_id, volume_size=volume_size)
+        self.create_ceph_volume(cluster_id=cluster_id, volume_size=volume_size)
+
+        file_size = int(ceph_size * 0.15)
+        md5s = dict()
+        md5s[floating_ip_1.ip] = self.mount_volume_and_create_bigfile(
+            instance_ip=floating_ip_1.ip,
+            file_size=file_size
         )
-        logger.info("New instance {0} created on {1}"
-                    .format(instance_2.id, hypervisor_name))
-
-        floating_ip_2 = os.assign_floating_ip(instance_2)
-        logger.info("Floating address {0} associated with instance {1}"
-                    .format(floating_ip_2.ip, instance_2.id))
-
-        self.show_step(8)
-        ceph_nodes = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
-            cluster_id, ['ceph-osd'])
-        total_ceph_size = 0
-        for node in ceph_nodes:
-            total_ceph_size += \
-                self.fuel_web.get_node_partition_size(node['id'], 'ceph')
-        percent_15_mb = 0.15 * total_ceph_size
-        percent_15_gb = percent_15_mb // 1024
-        volume_size = int(percent_15_gb + 1)
-
-        volume_1 = os.create_volume(size=volume_size)
-        volume_2 = os.create_volume(size=volume_size)
-
-        logger.info('Created volumes: {0}, {1}'.format(volume_1.id,
-                                                       volume_2.id))
-
-        ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-
-        logger.info("Attach volumes")
-        cmd = 'nova volume-attach {srv_id} {volume_id} /dev/vdb'
-
-        self.ssh_manager.execute_on_remote(
-            ip=ip,
-            cmd='. openrc; ' + cmd.format(srv_id=instance_1.id,
-                                          volume_id=volume_1.id)
-        )
-        self.ssh_manager.execute_on_remote(
-            ip=ip,
-            cmd='. openrc; ' + cmd.format(srv_id=instance_2.id,
-                                          volume_id=volume_2.id)
+        md5s[floating_ip_2.ip] = self.mount_volume_and_create_bigfile(
+            instance_ip=floating_ip_2.ip,
+            file_size=file_size
         )
 
-        cmds = ['sudo sh -c "/usr/sbin/mkfs.ext4 /dev/vdb"',
-                'sudo sh -c "/bin/mount /dev/vdb /mnt"',
-                'sudo sh -c "/usr/bin/nohup'
-                ' /bin/dd if=/dev/zero of=/mnt/bigfile '
-                'bs=1M count={} &"'.format(int(percent_15_mb))]
-
-        md5s = {floating_ip_1.ip: '', floating_ip_2.ip: ''}
-        with self.fuel_web.get_ssh_for_node("slave-01") as remote:
-            for ip in [floating_ip_1.ip, floating_ip_2.ip]:
-                for cmd in cmds:
-                    res = remote.execute_through_host(
-                        hostname=ip,
-                        cmd=cmd,
-                        auth=cirros_auth)
-                    logger.info('RESULT for {}: {}'.format(
-                        cmd,
-                        utils.pretty_log(res))
-                    )
-                logger.info('Wait 7200 untill "dd" ends')
-                for _ in range(720):
-                    cmd = 'ps -ef |grep -v grep| grep "dd if" '
-                    res = remote.execute_through_host(
-                        hostname=ip,
-                        cmd=cmd,
-                        auth=cirros_auth)
-                    if res['exit_code'] != 0:
-                        break
-                    time.sleep(10)
-                    logger.debug('Wait another 10 sec -'
-                                 ' totally waited {} sec'.format(10 * _))
-                else:
-                    raise TimeoutError('BigFile has not been'
-                                       ' created yet, after 7200 sec')
-                cmd = 'md5sum /mnt/bigfile'
-                md5s[ip] = remote.execute_through_host(
-                    hostname=ip,
-                    cmd=cmd,
-                    auth=cirros_auth)['stdout']
-
-        self.show_step(9)
+        self.show_step(4)
         nodes = {'compute': [], 'controller': [], 'ceph-osd': []}
 
         for role in nodes:
@@ -227,21 +270,112 @@ class FailoverGroup3(TestBasic):
         self.fuel_web.warm_shutdown_nodes(nodes['controller'])
         self.fuel_web.warm_shutdown_nodes(nodes['ceph-osd'])
 
-        self.show_step(10)
+        self.show_step(5)
         time.sleep(300)
 
-        self.show_step(11)
+        self.show_step(6)
         self.fuel_web.warm_start_nodes(nodes['ceph-osd'])
         self.fuel_web.warm_start_nodes(nodes['controller'])
-        self.show_step(12)
+        self.show_step(7)
         self.fuel_web.assert_ha_services_ready(cluster_id)
         self.fuel_web.warm_start_nodes(nodes['compute'])
         self.fuel_web.assert_os_services_ready(cluster_id)
 
-        self.show_step(13)
+        self.show_step(8)
         self.fuel_web.verify_network(cluster_id)
-        self.show_step(14)
+        self.show_step(9)
         self.fuel_web.run_ostf(cluster_id)
+        self.show_step(10)
+        with self.fuel_web.get_ssh_for_node("slave-01") as remote:
+            for ip in [floating_ip_1.ip, floating_ip_2.ip]:
+                cmd = 'md5sum /mnt/bigfile'
+                md5 = remote.execute_through_host(
+                    hostname=ip,
+                    cmd=cmd,
+                    auth=cirros_auth)['stdout']
+                assert_equal(md5, md5s[ip],
+                             "Actual md5sum {0} doesnt match"
+                             " with old one {1} on {2}".format(
+                                 md5, md5s[ip], ip))
+
+    @test(depends_on_groups=['deploy_ha_ceph_3_osd_vxlan'],
+          groups=['power_outage_ceph_for_all'])
+    @log_snapshot_after_test
+    def power_outage_ceph_for_all(self):
+        """Shutdown of Neutron Vxlan, ceph for all cluster
+
+        Scenario:
+            1. Do preconditions from deploy_ha_ceph_3_osd_vxlan
+            2. Create 2 volumes and 2 instances with attached volumes
+            3. Fill ceph storages up to 30%(15% for each instance)
+            4. Cold shutdown of all nodes
+            5. Wait 5 minutes
+            6. Start cluster
+            7. Wait until OSTF 'HA' suite passes
+            8. Verify networks
+            9. Run OSTF tests
+            10. Verify bigfile md5 checksum
+
+        Duration 230m
+
+        """
+        self.show_step(1)
+        self.env.revert_snapshot("deploy_ha_ceph_3_osd_vxlan")
+
+        cluster_id = self.fuel_web.get_last_created_cluster()
+
+        self.show_step(2)
+        hypervisors = self.get_hypervisors(cluster_id)
+        instance_1, floating_ip_1 = \
+            self.create_instance_with_floating_ip(
+                cluster_id=cluster_id,
+                hypervisor_name=hypervisors[0].hypervisor_hostname)
+        logger.info("Floating address {0} associated with instance {1}"
+                    .format(floating_ip_1.ip, instance_1.id))
+        instance_2, floating_ip_2 = \
+            self.create_instance_with_floating_ip(
+                cluster_id=cluster_id,
+                hypervisor_name=hypervisors[1].hypervisor_hostname)
+
+        self.show_step(3)
+        ceph_size = self.get_ceph_size(cluster_id=cluster_id)
+        volume_size = int((ceph_size // 1024) * 0.15) + 1
+        self.create_ceph_volume(cluster_id=cluster_id, volume_size=volume_size)
+        self.create_ceph_volume(cluster_id=cluster_id, volume_size=volume_size)
+
+        file_size = int(ceph_size * 0.15)
+        md5s = dict()
+        md5s[floating_ip_1.ip] = self.mount_volume_and_create_bigfile(
+            instance_ip=floating_ip_1.ip,
+            file_size=file_size
+        )
+        md5s[floating_ip_2.ip] = self.mount_volume_and_create_bigfile(
+            instance_ip=floating_ip_2.ip,
+            file_size=file_size
+        )
+
+        self.show_step(4)
+        n_nodes = self.fuel_web.client.list_cluster_nodes(
+            cluster_id=cluster_id)
+        d_nodes = self.fuel_web.get_devops_nodes_by_nailgun_nodes(n_nodes)
+        for d_node in d_nodes:
+            d_node.destroy()
+
+        self.show_step(5)
+        time.sleep(300)
+
+        self.show_step(6)
+        for d_node in d_nodes:
+            d_node.start()
+        self.show_step(7)
+        self.fuel_web.assert_ha_services_ready(cluster_id)
+        self.fuel_web.assert_os_services_ready(cluster_id)
+
+        self.show_step(8)
+        self.fuel_web.verify_network(cluster_id)
+        self.show_step(9)
+        self.fuel_web.run_ostf(cluster_id)
+        self.show_step(10)
         with self.fuel_web.get_ssh_for_node("slave-01") as remote:
             for ip in [floating_ip_1.ip, floating_ip_2.ip]:
                 cmd = 'md5sum /mnt/bigfile'
