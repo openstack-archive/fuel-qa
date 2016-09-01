6dba1577a1b78ad8c94c60b8e964d4b7aa9f8360
Maksym Strukov <mstrukov@mirantis.com>
2016-09-01 21:27:19 +0300

    [WIP] Make fuel-qa compatible with Timmy log snapshot tool
    Change-Id: I649bb924a73549da1ba1bbc239e0bb075706beaf
    Closes-Bug: #1619414

--- fuelweb_test/helpers/checkers.py
+++ fuelweb_test/helpers/checkers.py
@@ -1264,7 +1264,7 @@ def check_plugin_path_env(var_name, plugin_path):
 
 def check_snapshot_logs(ip, snapshot_name, controller_fqdns, compute_fqdns):
     snapshot_path_master = "/var/dump/{}".format(snapshot_name)
-    master_hostname = ''.join((FUEL_MASTER_HOSTNAME, DNS_SUFFIX))
+    master_hostname = 'logs-node-0'
     snapshot_logs_path = os.path.join(
         '{0}/fuelweb_test/config_templates/'.format(os.environ.get(
             "WORKSPACE", "./")), 'snapshot_logs.yaml')
@@ -1311,7 +1311,7 @@ def check_snapshot_logs(ip, snapshot_name, controller_fqdns, compute_fqdns):
         logger.debug("checking controller logs...")
         for log in snapshot_logs['controller'].split():
             logger.debug("checking {} log file".format(log))
-            log_path = "{dump_path}/{fqdn}/{log}".format(
+            log_path = "{dump_path}/logs-{fqdn}/{log}".format(
                 dump_path=snapshot_path_master,
                 fqdn=controller_fqdn.replace(DNS_SUFFIX, ""), log=log)
             cmd = "ls {}".format(log_path)
@@ -1345,7 +1345,7 @@ def check_snapshot_logs(ip, snapshot_name, controller_fqdns, compute_fqdns):
         logger.debug("checking compute logs...")
         for log in snapshot_logs['compute'].split():
             logger.debug("checking {} log file".format(log))
-            log_path = "{dump_path}/{fqdn}/{log}".format(
+            log_path = "{dump_path}/logs-{fqdn}/{log}".format(
                 dump_path=snapshot_path_master,
                 fqdn=compute_fqdn.replace(DNS_SUFFIX, ""), log=log)
             cmd = "ls {}".format(log_path)
