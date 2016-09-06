464f25742e004c8a1b93038aa68e836077277b80
Sergey Ryabin <sryabin@mirantis.com>
2016-09-06 18:09:56 +0300

    Patch framework v2
    Change-Id: Ib8a1e86e2585d57ae39db5a3c1c64d71aebf1db7

--- fuelweb_test/tests/tests_upgrade/octane_patcher.sh
+++ fuelweb_test/tests/tests_upgrade/octane_patcher.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
-set -e
-cd /usr/lib/python*/site-packages/octane
-echo -n $* | xargs -d" " -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | grep current_revision | cut -d: -f2 | sed "s/[, ]//g" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch -p2"'
+set -ex
+PATCH_DIR=$1
+shift
+
+show_only_unmerged() {
+  xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | grep -q "\"status\": \"MERGED\"" && (echo http://review.openstack.org/% MERGED > /dev/stderr) || echo %'
+}
+
+show_only_unapplied() {
+  xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch -N --follow-symlinks --batch -p2 --silent --dry-run 2>&1 >/dev/null && echo % || (echo http://review.openstack.org/% cant be applied > /dev/stderr)"'
+}
+
+cr_filter() {
+  grep -oE '[0-9]+?'
+}
+
+
+
+apply_patches() {
+  cd $1
+  shift
+  cr_filter | show_only_unapplied | xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch --batch -p2 && echo http://review.openstack.org/% successfully"'
+}
+
+test $# -ge 1 && echo $* | apply_patches ${PATCH_DIR}
