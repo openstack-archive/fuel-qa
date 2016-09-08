c6cfa89ce105423edd3302bb013421c93c59485f
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-09-08 11:13:35 +0300

    Use ExampleV4 plugin in backup-restore tests
    V3 plugin is deprecated in 9.1 and contains dropped tasks - it's
    impossible to use in for testing - replacing the plugin.
    
    Change-Id: Id25a324d490144556938c80b1b6931ce361dd237
    Closes-bug:1621105

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -390,6 +390,8 @@ UPGRADE_FUEL_TO = os.environ.get('UPGRADE_FUEL_TO', '8.0')
 OCTANE_PATCHES = os.environ.get('OCTANE_PATCHES', None)
 EXAMPLE_V3_PLUGIN_REMOTE_URL = os.environ.get('EXAMPLE_V3_PLUGIN_REMOTE_URL',
                                               None)
+EXAMPLE_V4_PLUGIN_REMOTE_URL = os.environ.get('EXAMPLE_V4_PLUGIN_REMOTE_URL',
+                                              None)
 UPGRADE_BACKUP_FILES_LOCAL_DIR = os.environ.get(
     'UPGRADE_BACKUP_FILES_LOCAL_DIR', os.path.join(
         os.path.curdir, "..", "backup_storage"))
