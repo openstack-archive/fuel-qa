44acb7b0800a9690803d64127eaf4ca7318c2ce6
Vasily Gorin <vgorin@mirantis.com>
2016-09-16 13:18:24 +0300

    Add insecure value for vmware configuration.
    We need to set the 'insecure' value to True.
    
    Change-Id: Ic90d1b5eb5959df3636f074ab099a90cd7e5a315
    Closes-Bug: #1623864

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -764,7 +764,8 @@ class FuelWebClient29(object):
                     "datacenter": "",
                     "vcenter_host": "",
                     "vcenter_password": "",
-                    "datastore": "", },
+                    "datastore": "",
+                    "vcenter_insecure": True},
                 "availability_zones": [
                     {"vcenter_username": VCENTER_USERNAME,
                      "nova_computes": [
@@ -782,6 +783,8 @@ class FuelWebClient29(object):
                      "vcenter_host": VCENTER_IP,
                      "az_name": "vcenter",
                      "vcenter_password": VCENTER_PASSWORD,
+                     "vcenter_insecure": True
+
                      }],
                 "network": {"esxi_vlan_interface": "vmnic0"}
             }
