1a73c6abd8c26b06b2554666d7b73c9d9d0d5e31
Artem Grechanichenko <agrechanichenko@mirantis.com>
2016-09-22 16:53:21 +0300

    Disable all online mos-repos if FORCE_DISABLE_UPDATES
    Change-Id: I0c3aba8a38351a5442ecb1901b74b5660bd45357

--- fuelweb_test/tests/base_test_case.py
+++ fuelweb_test/tests/base_test_case.py
@@ -177,7 +177,15 @@ class TestBasic(object):
         time.sleep(10)
         self.env.set_admin_keystone_password()
         self.env.sync_time(['admin'])
-        if settings.UPDATE_MASTER:
+        if settings.FORCE_DISABLE_UPDATES:
+            cmd = "find /etc/yum.repos.d/ -type f -regextype posix-egrep" \
+                  " -regex '.*/mos+\-(updates|security|holdback).repo' | " \
+                  "xargs -n1 -i sed -i 's/enabled=1/enabled=0/' -i {}"
+            self.ssh_manager.execute_on_remote(
+                ip=self.ssh_manager.admin_ip,
+                cmd=cmd
+            )
+        elif settings.UPDATE_MASTER:
             if settings.UPDATE_FUEL_MIRROR:
                 for i, url in enumerate(settings.UPDATE_FUEL_MIRROR):
                     conf_file = '/etc/yum.repos.d/temporary-{}.repo'.format(i)
@@ -207,14 +215,6 @@ class TestBasic(object):
             logger.info('Enabled sending of statistics to {0}:{1}'.format(
                 settings.FUEL_STATS_HOST, settings.FUEL_STATS_PORT
             ))
-        if settings.PATCHING_DISABLE_UPDATES:
-            cmd = "find /etc/yum.repos.d/ -type f -regextype posix-egrep" \
-                  " -regex '.*/mos+\-(updates|security).repo' | " \
-                  "xargs -n1 -i sed -i 's/enabled=1/enabled=0/' -i {}"
-            self.ssh_manager.execute_on_remote(
-                ip=self.ssh_manager.admin_ip,
-                cmd=cmd
-            )
         if settings.DISABLE_OFFLOADING:
             logger.info(
                 '========================================'
