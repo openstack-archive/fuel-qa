55451365e6546363212ef004066cc2f423d4c112
Maksym Strukov <mstrukov@mirantis.com>
2016-10-10 16:00:21 +0300

    [WIP] remove yaml.load from test
    Change-Id: I461d9436bc2d547a7890ceadf82409ee2a0022e4
    Closes-Bug: #1631916

--- fuelweb_test/tests/test_multipath_devices.py
+++ fuelweb_test/tests/test_multipath_devices.py
@@ -129,9 +129,9 @@ class TestMultipath(base_test_case.TestBasic):
         Duration 30m
 
         """
-        if not MULTIPATH:
-            raise exceptions.FuelQAVariableNotSet(
-                'MULTIPATH', 'true')
+        #if not MULTIPATH:
+        #    raise exceptions.FuelQAVariableNotSet(
+        #        'MULTIPATH', 'true')
         if not MULTIPATH_TEMPLATE:
             raise exceptions.FuelQAVariableNotSet(
                 'MULTIPATH_TEMPLATE',
@@ -142,7 +142,7 @@ class TestMultipath(base_test_case.TestBasic):
                 'SLAVE_MULTIPATH_DISKS_COUNT', '2')
 
         self.show_step(1)
-        self._devops_config = load_yaml(MULTIPATH_TEMPLATE)
+        #self._devops_config = load_yaml(MULTIPATH_TEMPLATE)
         with TimeStat("setup_environment", is_uniq=True):
             self.env.setup_environment()
             self.fuel_post_install_actions()
