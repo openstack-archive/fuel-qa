bddbadc5339fd3767981462c42b9b61b97fb40c7
Alexandr Kostrikov <akostrikov@mirantis.com>
2016-10-20 17:10:31 +0300

    Temporary disable ceilometer check
    The service is under uwsgi so it can
    not be checked with ps ax
    
    Change-Id: Ib17a7f58f140832839227e67c2ca1540162864b9
    Closes-bug: 1634920

--- fuelweb_test/tests/test_services.py
+++ fuelweb_test/tests/test_services.py
@@ -535,9 +535,15 @@ class CeilometerHAOneControllerMongo(OSTFCeilometerHelper):
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-03")['ip']
         partitions = utils.get_mongo_partitions(_ip, "vda5")
@@ -589,10 +595,16 @@ class CeilometerHAOneControllerMongo(OSTFCeilometerHelper):
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_one_controller_multirole")
@@ -646,10 +658,16 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id,
                        skip_tests=['test_check_volume_events'])
@@ -697,10 +715,16 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_multirole", is_make=True)
@@ -791,10 +815,16 @@ class CeilometerHAMongo(OSTFCeilometerHelper):
         )
         self.fuel_web.deploy_cluster_wait(cluster_id)
 
-        _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         self.run_tests(cluster_id)
         self.env.make_snapshot("deploy_ceilometer_ha_with_external_mongo")
@@ -857,10 +887,16 @@ class HeatHAOneController(TestBasic):
         _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
         checkers.verify_service(_ip, service_name='heat-api', count=5)
 
-        _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
-        checkers.verify_service(_ip,
-                                service_name='ceilometer-api',
-                                ignore_count_of_proccesses=True)
+        for service_name in ['ceilometer-agent-notification',
+                             'ceilometer-collector',
+                             'ceilometer-polling',
+                             'aodh-listener',
+                             'aodh-notifier',
+                             'aodh-evaluator']:
+            _ip = self.fuel_web.get_nailgun_node_by_name("slave-01")['ip']
+            checkers.verify_service(_ip,
+                                    service_name=service_name,
+                                    ignore_count_of_proccesses=True)
 
         logger.debug('Run Heat OSTF platform tests')
 
@@ -946,10 +982,16 @@ class HeatHA(TestBasic):
 
         for slave in ["slave-01", "slave-02", "slave-03"]:
             _ip = self.fuel_web.get_nailgun_node_by_name(slave)['ip']
-            checkers.verify_service(_ip, service_name='heat-api', count=5)
-            checkers.verify_service(_ip,
-                                    service_name='ceilometer-api',
-                                    ignore_count_of_proccesses=True)
+            for service_name in ['ceilometer-agent-notification',
+                                 'ceilometer-collector',
+                                 'ceilometer-polling',
+                                 'aodh-listener',
+                                 'aodh-notifier',
+                                 'aodh-evaluator']:
+                checkers.verify_service(_ip, service_name='heat-api', count=5)
+                checkers.verify_service(_ip,
+                                        service_name=service_name,
+                                        ignore_count_of_proccesses=True)
 
         logger.debug('Run Heat OSTF platform tests')
 
