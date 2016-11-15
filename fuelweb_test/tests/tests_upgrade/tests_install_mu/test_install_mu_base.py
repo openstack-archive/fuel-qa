d118ade1082fd73e7e729c91a98bd488702b8388
ibumarskov <ibumarskov@mirantis.com>
2016-11-15 14:16:22 +0300

    Add ability to use MOS MU tool for upgrade
    All upgrade test cases were moved from MUInstallBase to
    MUInstallNoHA class due to issue with double test import
    Add test with customization
    
    Change-Id: I3eedc9f37422fe768f916d910386141ca1d36123

--- fuelweb_test/tests/tests_upgrade/tests_install_mu/test_install_mu_base.py
+++ fuelweb_test/tests/tests_upgrade/tests_install_mu/test_install_mu_base.py
@@ -20,7 +20,6 @@ from proboscis.asserts import assert_true
 from fuelweb_test import logger
 from fuelweb_test.helpers.utils import pretty_log
 from fuelweb_test.helpers.utils import YamlEditor
-from fuelweb_test.helpers.decorators import log_snapshot_after_test
 from fuelweb_test import settings
 from fuelweb_test.tests import test_cli_base
 
@@ -30,6 +29,10 @@ from gates_tests.helpers import exceptions
 
 @test(groups=["prepare_mu_installing"])
 class MUInstallBase(test_cli_base.CommandLine):
+    if settings.USE_MOS_MU_FOR_UPGRADE:
+        repos = 'mos9.2'
+    else:
+        repos = 'proposed'
 
     def _add_cluster_repo(self, cluster_id, repo):
         attributes = self.fuel_web.client.get_cluster_attributes(cluster_id)
@@ -74,6 +77,20 @@ class MUInstallBase(test_cli_base.CommandLine):
             command=cmd
         )
 
+    def _prepare_for_update_mos_mu(self, cluster_id):
+        logger.info('Prepare Enviroment')
+        mos_mu_path = 'cd {} ;'.format(settings.MOS_MU_PATH)
+        ext_vars = '\'{{"env_id":{0}, "snapshot_repo":"{1}"}}\'' \
+                   ''.format(cluster_id,
+                             settings.MOS_UBUNTU_MIRROR_ID)
+        cmd = '{0} ansible-playbook playbooks/mos9_prepare_env.yml -e ' \
+              '{1}'.format(mos_mu_path, ext_vars)
+
+        self.ssh_manager.check_call(
+            ip=self.ssh_manager.admin_ip,
+            command=cmd
+        )
+
     def _add_centos_test_proposed_repo(self, repo_url, key):
         cmds = ["yum-config-manager --add-repo {}".format(repo_url),
                 "rpm --import  {}".format(key)]
@@ -83,6 +100,10 @@ class MUInstallBase(test_cli_base.CommandLine):
                 command=cmd)
 
     def _check_for_potential_updates(self, cluster_id, updated=False):
+
+        if settings.USE_MOS_MU_FOR_UPGRADE:
+            logger.warning('SKIPPED DUE TO ABSENT OF DB FOR CUDET')
+            return True
 
         # "cudet" command don't have json output
         if updated:
@@ -164,7 +185,7 @@ class MUInstallBase(test_cli_base.CommandLine):
             command=cmd
         )
 
-    def _install_mu(self, cluster_id, repos='proposed'):
+    def _install_mu(self, cluster_id, repos):
         if settings.UPGRADE_CLUSTER_FROM_PROPOSED:
             cmd = "fuel2 update install --env {} --repos {} " \
                   "--restart-rabbit --restart-mysql".format(cluster_id,
@@ -186,11 +207,73 @@ class MUInstallBase(test_cli_base.CommandLine):
         task_id = int(std_out.split("fuel2 task show")[1].split("`")[0])
         task = self.get_task(task_id)
 
-        self.assert_cli_task_success(task,
-                                     timeout=120 * 60)
+        self.assert_cli_task_success(task, timeout=settings.DEPLOYMENT_TIMEOUT)
+
+        if settings.USE_MOS_MU_FOR_UPGRADE:
+            mos_mu_path = 'cd {} ;'.format(settings.MOS_MU_PATH)
+
+            logger.info('Upgrade kernel on 4.4')
+            command = \
+                '{0} ansible-playbook playbooks/mos9_env_upgrade_kernel_' \
+                '4.4.yml -e \'{{"env_id":{1}}}\''.format(mos_mu_path,
+                                                         cluster_id)
+            self.ssh_manager.check_call(
+                ip=self.ssh_manager.admin_ip,
+                command=command)
+
+            logger.info('Update ceph')
+            command = \
+                '{0} ansible-playbook playbooks/update_ceph.yml -e \'' \
+                '{{"env_id":{1},"restart_ceph":false}}\''.format(mos_mu_path,
+                                                                 cluster_id)
+            self.ssh_manager.check_call(
+                ip=self.ssh_manager.admin_ip,
+                command=command)
+
+            logger.info('Apply patches if possible')
+            command = \
+                '{0} ansible-playbook playbooks/mos9_apply_patches.yml -e \'' \
+                '{{"env_id":{1}}}\''.format(mos_mu_path, cluster_id)
+            self.ssh_manager.check_call(
+                ip=self.ssh_manager.admin_ip,
+                command=command)
+
+            logger.info('Restart all nodes in environment')
+            command = \
+                '{0} ansible-playbook playbooks/restart_env.yml -e \'' \
+                '{{"env_id":{1}}}\''.format(mos_mu_path, cluster_id)
+            self.ssh_manager.check_call(
+                ip=self.ssh_manager.admin_ip,
+                command=command)
+
+    def _redeploy_noop(self, cluster_id, timeout=settings.DEPLOYMENT_TIMEOUT):
+        logger.info("Run the configuration check on your environment using "
+                    "Noop run")
+        cmd = "fuel2 env redeploy --noop {}".format(cluster_id)
+
+        std_out = self.ssh_manager.check_call(
+            ip=self.ssh_manager.admin_ip,
+            command=cmd
+        ).stdout_str
+
+        # "fuel2 update" command don't have json output
+        assert_true(
+            "Deployment task with id" in std_out,
+            "fuel2 redeploy command don't return task id: \n {}".format(std_out))
+
+        task_id = int(std_out.split("Deployment task with id")[1].split()[0])
+        task = self.get_task(task_id)
+
+        self.assert_cli_task_success(task, timeout=timeout)
 
     def _prepare_cluster_for_mu(self):
+        if settings.USE_MOS_MU_FOR_UPGRADE:
+            repo_url = settings.UPGRADE_CLUSTER_FROM_PROPOSED_RPM
+            key = settings.UPGRADE_CLUSTER_FROM_PROPOSED_RPM_KEY
+            self._add_centos_test_proposed_repo(repo_url, key)
 
+            self._prepare_cluster_for_mu_via_mos_mu()
+            return True
         cluster_id = self.fuel_web.get_last_created_cluster()
 
         mos_repo = {
@@ -230,7 +313,7 @@ class MUInstallBase(test_cli_base.CommandLine):
                 editor.content['BOOTSTRAP']['repos'].append(proposed)
         self.show_step(self.next_step)
         self.show_step(self.next_step)
-        self.env.admin_install_updates()
+        self.env.admin_actions.admin_install_updates()
 
         self.show_step(self.next_step)
         self._prepare_for_update(cluster_id)
@@ -238,159 +321,54 @@ class MUInstallBase(test_cli_base.CommandLine):
         self.show_step(self.next_step)
         self.env.admin_actions.wait_for_fuel_ready(timeout=600)
 
-    @test(depends_on_groups=["deploy_multirole_compute_cinder"],
-          groups=["prepare_for_install_mu_non_ha_cluster"])
-    @log_snapshot_after_test
-    def prepare_for_install_mu_non_ha_cluster(self):
-        """Update master node and install packages for MU installing
+    def _prepare_cluster_for_mu_via_mos_mu(self):
 
-        Scenario:
-            1. Revert snapshot deploy_multirole_compute_cinder
-            2. Enable updates repo
-            3. Prepare master node for update
-            4. Update master node
-            5. Prepare env for update
-            6. Check Fuel services
+        cluster_id = self.fuel_web.get_last_created_cluster()
 
-        Duration: 20m
-        Snapshot: prepare_for_install_mu_non_ha_cluster
-        """
+        self.show_step(self.next_step)
+        self.show_step(self.next_step)
+        self.show_step(self.next_step)
+        self.env.admin_actions.prepare_admin_node_for_mos_mu()
+        self.env.admin_actions.admin_install_updates_mos_mu()
 
-        self.check_env_var()
-        self.check_run("prepare_for_install_mu_non_ha_cluster")
+        self.show_step(self.next_step)
+        self._prepare_for_update_mos_mu(cluster_id)
 
-        self.show_step(1)
-        self.env.revert_snapshot("deploy_multirole_compute_cinder")
+        self.show_step(self.next_step)
+        self.env.admin_actions.wait_for_fuel_ready(timeout=600)
 
-        self._prepare_cluster_for_mu()
+    def apply_customization(self, cluster_id, patch_file, path):
+        file_name = patch_file.split('/')[-1]
+        logger.info("Apply patch {0} on all nodes in cluster {1}".format(
+            file_name,cluster_id))
 
-        self.env.make_snapshot(
-            "prepare_for_install_mu_non_ha_cluster",
-            is_make=True)
+        nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
+        for node in nodes:
+            logger.debug("Apply patch on {}".format(node['hostname']))
+            with self.fuel_web.get_ssh_for_nailgun_node(node) as remote:
+                remote.upload(patch_file, './')
+                remote.execute('cd {0} && patch -p1 < ~/{1}'.format(
+                    path, file_name))
 
-    @test(depends_on_groups=["ceph_rados_gw"],
-          groups=["prepare_for_install_mu_ha_cluster"])
-    @log_snapshot_after_test
-    def prepare_for_install_mu_ha_cluster(self):
-        """Update master node and install packages for MU installing
+    def get_customization_via_mos_mu(self, cluster_id):
+        self._redeploy_noop(cluster_id)
 
-        Scenario:
-            1. Revert snapshot ceph_rados_gw
-            2. Enable updates repo
-            3. Prepare master node for update
-            4. Update master node
-            5. Prepare env for update
-            6. Check Fuel services
+        mos_mu_path = 'cd {} ;'.format(settings.MOS_MU_PATH)
 
-        Duration: 20m
-        Snapshot: prepare_for_install_mu_ha_cluster
-        """
+        logger.info('Gathering code customizations')
+        command = \
+            '{0} ansible-playbook playbooks/gather_customizations.yml -e \'' \
+            '{{"env_id":{1}}}\''.format(mos_mu_path, cluster_id)
 
-        self.check_env_var()
+        self.ssh_manager.check_call(
+            ip=self.ssh_manager.admin_ip,
+            command=command)
 
-        self.check_run("prepare_for_install_mu_ha_cluster")
+        logger.info('Verify patches')
+        command = \
+            '{0} ansible-playbook playbooks/verify_patches.yml -e \'' \
+            '{{"env_id":{1}}}\''.format(mos_mu_path, cluster_id)
 
-        self.show_step(1)
-        self.env.revert_snapshot("ceph_rados_gw")
-
-        self._prepare_cluster_for_mu()
-
-        self.env.make_snapshot(
-            "prepare_for_install_mu_ha_cluster",
-            is_make=True)
-
-    @test(depends_on_groups=["ironic_deploy_ceilometer"],
-          groups=["prepare_for_install_mu_services_1"])
-    @log_snapshot_after_test
-    def prepare_for_install_mu_services_1(self):
-        """Update master node and install packages for MU installing
-
-        Scenario:
-            1. Revert snapshot ironic_deploy_ceilometer
-            2. Enable updates repo
-            3. Prepare master node for update
-            4. Update master node
-            5. Prepare env for update
-            6. Check Fuel services
-
-        Duration: 20m
-        Snapshot: prepare_for_install_mu_services_1
-        """
-
-        self.check_env_var()
-
-        self.check_run("prepare_for_install_mu_services_1")
-
-        self.show_step(1)
-
-        self.env.revert_snapshot("ironic_deploy_ceilometer")
-
-        self._prepare_cluster_for_mu()
-
-        self.env.make_snapshot(
-            "prepare_for_install_mu_services_1",
-            is_make=True)
-
-    @test(depends_on_groups=["deploy_sahara_ha_tun"],
-          groups=["prepare_for_install_mu_services_2"])
-    @log_snapshot_after_test
-    def prepare_for_install_mu_services_2(self):
-        """Update master node and install packages for MU installing
-
-        Scenario:
-            1. Revert snapshot deploy_sahara_ha_tun
-            2. Enable updates repo
-            3. Prepare master node for update
-            4. Update master node
-            5. Prepare env for update
-            6. Check Fuel services
-
-        Duration: 20m
-        Snapshot: prepare_for_install_mu_services_2
-        """
-
-        self.check_env_var()
-
-        self.check_run("prepare_for_install_mu_services_2")
-
-        self.show_step(1)
-
-        self.env.revert_snapshot("deploy_sahara_ha_tun")
-
-        self._prepare_cluster_for_mu()
-
-        self.env.make_snapshot(
-            "prepare_for_install_mu_services_2",
-            is_make=True)
-
-    @test(depends_on_groups=["deploy_murano_ha_with_tun"],
-          groups=["prepare_for_install_mu_services_3"])
-    @log_snapshot_after_test
-    def prepare_for_install_mu_services_3(self):
-        """Update master node and install packages for MU installing
-
-        Scenario:
-            1. Revert snapshot deploy_sahara_ha_tun
-            2. Enable updates repo
-            3. Prepare master node for update
-            4. Update master node
-            5. Prepare env for update
-            6. Check Fuel services
-
-        Duration: 20m
-        Snapshot: prepare_for_install_mu_services_3
-        """
-
-        self.check_env_var()
-
-        self.check_run("prepare_for_install_mu_services_3")
-
-        self.show_step(1)
-
-        self.env.revert_snapshot("deploy_murano_ha_with_tun")
-
-        self._prepare_cluster_for_mu()
-
-        self.env.make_snapshot(
-            "prepare_for_install_mu_services_3",
-            is_make=True)
+        self.ssh_manager.check_call(
+            ip=self.ssh_manager.admin_ip,
+            command=command)
