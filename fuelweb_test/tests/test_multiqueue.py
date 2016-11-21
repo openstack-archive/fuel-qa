6e37504e274998ae2e80626e1138e53236d9152a
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2016-11-21 13:26:50 +0300

    [wip] Detach dpdk and hugepages config method from test class
    This refacrot is required for new nvf test implemented later
    
    Change-Id: Id50a9d4dc414d66b569cf1f0ac7c06754d6484a5

--- fuelweb_test/tests/test_multiqueue.py
+++ fuelweb_test/tests/test_multiqueue.py
@@ -27,6 +27,8 @@ from fuelweb_test import logger
 from fuelweb_test import settings
 from fuelweb_test.helpers import os_actions
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.helpers.utils import check_setting_equal, \
+    check_setting_greater
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 
@@ -36,11 +38,19 @@ class TestMultiqueue(TestBasic):
 
     def __init__(self):
         super(TestMultiqueue, self).__init__()
-        assert_true(settings.KVM_USE, "Multiqueue feature requires "
-                                      "KVM_USE=true env variable!")
-        assert_true(settings.HARDWARE["slave_node_cpu"] > 1,
-                    "Multiqueue feature requires more than 1 cpu for "
-                    "enabling queues!")
+        check_setting_equal('KVM_USE', True)
+        check_setting_greater('HARDWARE', 2, sub_setting='slave_node_cpu',
+                              env_var_name='SLAVE_NODE_CPU')
+
+    @staticmethod
+    def check_cpu_pinning_requirements():
+        check_setting_equal('KVM_USE', True)
+        check_setting_equal('HARDWARE', sub_setting='slave_node_cpu',
+                            expected_value=6, env_var_name='SLAVE_NODE_CPU')
+        check_setting_equal('HARDWARE', sub_setting='numa_nodes',
+                            expected_value=2, env_var_name='NUMA_NODES')
+        check_setting_equal('ACPI_ENABLE', True,
+                            env_var_name='DRIVER_ENABLE_ACPI')
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
           groups=["multiqueue_base_check"])
@@ -151,3 +161,122 @@ class TestMultiqueue(TestBasic):
                      "RX queues count is not equal to vcpus count")
         assert_equal(result.stdout_str.count("tx"), vcpus,
                      "TX queues count is not equal to vcpus count")
+
+    @test(groups=["multiqueue_with_dpdk_and_numa"],
+          depends_on_groups=["prepare_slaves_5"])
+    def multiqueue_with_dpdk_and_numa(self):
+        """LALALA
+
+        Scenario:
+        1. lalala
+
+        """
+        self.check_cpu_pinning_requirements()
+        self.env.revert_snapshot("ready_with_5_slaves")
+        self.env.bootstrap_nodes([self.env.d_env.get_node(name='slave-06')])
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            settings={
+                'volumes_lvm': False,
+                'volumes_ceph': True,
+                'images_ceph': True,
+                'objects_ceph': True,}
+        )
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute', 'ceph-osd'],
+                'slave-05': ['compute', 'ceph-osd'],
+                'slave-06': ['compute', 'ceph-osd']
+            }
+        )
+        computes = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            cluster_id, ['compute'], role_status='pending_roles')
+        for compute in computes:
+            self.fuel_web.setup_hugepages(
+                compute['id'], hp_2mb=512, hp_dpdk_mb=256)
+            self.fuel_web.enable_dpdk(compute['id'])
+            self.fuel_web.enable_cpu_pinning(compute['id'])
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self.fuel_web.run_ostf(cluster_id)
+
+
+        # update image's metadata
+        self.show_step(self.next_step)
+        cluster_id = self.fuel_web.get_last_created_cluster()
+        os_conn = os_actions.OpenStackActions(
+            self.fuel_web.get_public_vip(cluster_id))
+        test_vm_image = os_conn.glance.images.find(name="TestVM")
+        test_vm_image.update(properties={'hw_vif_multiqueue_enabled': True})
+        compute = self.fuel_web.get_nailgun_node_by_name('slave-04')
+
+        nova_compute = os_conn.nova.hypervisors.list().pop()
+
+        aggregate_name = 'qweqwe' + str(random.randint(0, 1000))
+        aggregate = os_conn.create_aggregate(aggregate_name,
+                                             metadata={'pinned': 'true'},
+                                             hosts=[compute['fqdn']])
+        vcpus = nova_compute.vcpus
+        # create flavor
+        self.show_step(self.next_step)
+
+        name = 'system_test-{}'.format(random.randint(10, 10000))
+        extra_specs_flavor = {'aggregate_instance_extra_specs:pinned': 'true',
+                              'hw:cpu_policy': 'dedicated',
+                              'hw:mem_page_size': 2048}
+
+        flavor_id = random.randint(10, 10000)
+        os_conn.create_flavor(name=name, ram=64, vcpus=2, disk=1,
+                              flavorid=flavor_id,
+                              extra_specs=extra_specs_flavor)
+
+        self.show_step(self.next_step)
+        net_name = self.fuel_web.get_cluster_predefined_networks_name(
+            cluster_id)['private_net']
+
+        server = os_conn.create_server_for_migration(neutron=True,
+                                                     label=net_name,
+                                                     flavor=flavor_id)
+        os_conn.verify_instance_status(server, 'ACTIVE')
+
+        self.show_step(self.next_step)
+        floating_ip = os_conn.assign_floating_ip(server)
+        logger.info("Floating address {0} associated with instance {1}"
+                    .format(floating_ip.ip, server.id))
+
+        logger.info("Wait for ping from instance {} "
+                    "by floating ip".format(server.id))
+        devops_helpers.wait(
+            lambda: devops_helpers.tcp_ping(floating_ip.ip, 22),
+            timeout=700,
+            timeout_msg=("Instance {0} is unreachable for {1} seconds".
+                         format(server.id, 300)))
+
+        cirros_auth = SSHAuth(**settings.SSH_IMAGE_CREDENTIALS)
+        slave_01_ssh = self.fuel_web.get_ssh_for_node("slave-01")
+
+        self.show_step(self.next_step)
+        result = slave_01_ssh.execute_through_host(
+            hostname=floating_ip.ip,
+            cmd="sudo /sbin/ethtool -L eth0 combined {}".format(2),
+            auth=cirros_auth)
+
+        assert_equal(
+            result.exit_code, 0,
+            "Enabling queues using ethtool failed!\n{}".format(result))
+
+        self.show_step(self.next_step)
+        result = slave_01_ssh.execute_through_host(
+            hostname=floating_ip.ip,
+            cmd="ls /sys/class/net/eth0/queues",
+            auth=cirros_auth
+        )
+        assert_equal(result.stdout_str.count("rx"), 2,
+                     "RX queues count is not equal to vcpus count")
+        assert_equal(result.stdout_str.count("tx"), 2,
+                     "TX queues count is not equal to vcpus count")
+
