a957e78beb69b78dc9060b6c7b87f713f4f9b4e9
Sergey Novikov <snovikov@mirantis.com>
2017-01-17 19:59:01 +0400

    Update bond tests
    Juat another update of bond tests according to
    fix of https://bugs.launchpad.net/fuel/+bug/1652068
    
    Change-Id: I3c0b2dd09f54fefb52baef914047507fd9cc0511
    Closes-Bug: 1644588

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1743,10 +1743,9 @@ class FuelWebClient29(object):
         else:
             logger.debug("Using new interface serialization scheme")
             offloading_types = set([
-                offloading_type
+                offloading_type['name']
                 for iface in target_ifaces
-                for offloading_type in
-                iface['attributes']['offloading']['modes']['value']])
+                for offloading_type in iface['meta']['offloading_modes']])
         return list(offloading_types)
 
     @logwrap
@@ -3177,10 +3176,9 @@ class FuelWebClient29(object):
                 target_interface = interface
                 break
 
-        if target_interface['type'] == 'bond':
-            target_interface['bond_properties']['type__'] = 'dpdkovs'
-
         if 'interface_properties' in target_interface.keys():
+            if target_interface['type'] == 'bond':
+                target_interface['bond_properties']['type__'] = 'dpdkovs'
             logger.debug("Using old interface serialization scheme")
             target_interface['interface_properties']['dpdk'][
                 'enabled'] = switch_to
