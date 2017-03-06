bef5c4e09c13b1a6cb589359c77fde87ae21be19
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2017-03-06 19:05:49 +0400

    Add a template for security scan related devops environment
     We use prepared image for create VM with security scan service
    
    Change-Id: I9c2c383971b5e72a1414a43dcb4db8f3ad22bc78

--- fuelweb_test/helpers/fuel_actions.py
+++ fuelweb_test/helpers/fuel_actions.py
@@ -18,9 +18,6 @@ import yaml
 
 from devops.helpers.helpers import wait
 from devops.error import TimeoutError
-from devops.models import DiskDevice
-from devops.models import Node
-from devops.models import Volume
 from proboscis.asserts import assert_equal
 
 from fuelweb_test import logger
@@ -35,7 +32,6 @@ from fuelweb_test.helpers.utils import run_on_remote_get_results
 from fuelweb_test.settings import FUEL_PLUGIN_BUILDER_REPO
 from fuelweb_test.settings import FUEL_USE_LOCAL_NTPD
 from fuelweb_test import settings as hlp_data
-from fuelweb_test.settings import NESSUS_IMAGE_PATH
 
 
 class BaseActions(object):
@@ -575,21 +571,3 @@ class DockerActions(object):
         for container in self.list_containers():
             self.admin_remote.execute(
                 "dockerctl shell {0} bash -c '{1}'".format(container, cmd))
-
-
-class NessusActions(object):
-    """ NessusActions."""   # TODO documentation
-
-    def __init__(self, d_env):
-        self.devops_env = d_env
-
-    def add_nessus_node(self):
-        node = Node.node_create(
-            name='slave-nessus',
-            environment=self.devops_env,
-            boot=['hd'])
-        node.attach_to_networks()
-        volume = Volume.volume_get_predefined(NESSUS_IMAGE_PATH)
-        DiskDevice.node_attach_volume(node=node, volume=volume)
-        node.define()
-        node.start()
