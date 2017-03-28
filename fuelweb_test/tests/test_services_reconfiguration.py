0dfd7edf236d0ffbd28ecca9386e7c29fe237a38
Igor Gajsin <igajsin@mirantis.com>
2017-03-28 12:56:19 +0300

    Add timeout to check_service_was_restated
    There is the function to check that service was restarted. And there is
    test case two_clusters_reconfiguration, which uploads openstack-config
    with
      notify: Service[nova-scheduler]
    and checks that puppet restarts the nova-scheduler.
    
    Unfortunately, there is a time gap between uploading config to nailgun
    and applying puppet on a node. So we have to add a timeout to the
    checker for covering that case.
    
    Closes-bug: #1673750
    Change-Id: Icef1e15489727a13a753b9921df0cc5527f9ab2d

--- fuelweb_test/tests/test_services_reconfiguration.py
+++ fuelweb_test/tests/test_services_reconfiguration.py
@@ -166,21 +166,26 @@ class ServicesReconfiguration(TestBasic):
                                            param['option'],
                                            param['value'])
 
-    def check_service_was_restarted(self, nodes, uptime_before, service_name):
+    def check_service_was_restarted(self, nodes, uptime_before, service_name,
+                                    timeout=10*60, interval=60):
         """
         :param nodes: a list of nailgun nodes
         :param uptime_before: a dictionary of ip nodes and process uptime
         :param service_name: a string of service name
         :return:
         """
+        def is_restarted(nodes):
+            for node in nodes:
+                with self.env.d_env.get_ssh_to_remote(node) as remote:
+                    uptime = utils.get_process_uptime(remote, service_name)
+                    if uptime > uptime_before[node]:
+                        return False
+            return true
+
         nodes = [x['ip'] for x in nodes]
-        for node in nodes:
-            with self.env.d_env.get_ssh_to_remote(node) as remote:
-                uptime = utils.get_process_uptime(remote, service_name)
-                asserts.assert_true(uptime <= uptime_before[node],
-                                    'Service "{0}" was not '
-                                    'restarted on {1}'.format(service_name,
-                                                              node))
+        helpers.wait(lambda: is_restarted(nodes), timeout=timeout,
+                     interval=interval, timeout_msg='Service "{0}" was not'
+                     'restarted on {1}.'.format(service_name, node))
 
     def check_overcommit_ratio(self, os_conn, cluster_id):
         """
