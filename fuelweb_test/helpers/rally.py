aa3c224da6fe8afc91a15990cf230f241b84551b
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2017-04-12 15:16:49 +0400

    Rewrite rally deployment show parser
    For now rally throws warnings to stdout which leads to broken awk
    parser. Fixing this awk is pointless because we can to this work using python
    
    Change-Id: Id6744aa067ebf400f0eabf7ca0233327aa05f589
    Closes-bug:1682046

--- fuelweb_test/helpers/rally.py
+++ fuelweb_test/helpers/rally.py
@@ -168,16 +168,16 @@ class RallyEngine(object):
         return [line.strip() for line in result['stdout']]
 
     def show_deployment(self, deployment_uuid):
-        cmd = ("rally deployment show {0} | awk -F "
-               "'[[:space:]]*\\\\|[[:space:]]*' '/\w/{{print $2\",\"$3\",\"$4"
-               "\",\"$5\",\"$6\",\"$7\",\"$8}}'").format(deployment_uuid)
+        cmd = "rally deployment show {0}".format(deployment_uuid)
         result = self.run_container_command(cmd)
-        assert_equal(len(result['stdout']), 2,
-                     "Command 'rally deployment show' returned unexpected "
-                     "value: expected 2 lines, got {0}: ".format(result))
-        keys = [k for k in result['stdout'][0].strip().split(',') if k != '']
-        values = [v for v in result['stdout'][1].strip().split(',') if v != '']
-        return {keys[i]: values[i] for i in range(0, len(keys))}
+        output_payload = [
+            [word.strip() for word in line.split("|") if word.strip()]
+            for line in result.stdout if line.startswith("|")]
+        assert_equal(
+            len(output_payload), 2,
+            "Command 'rally deployment show' returned unexpected "
+            "value: expected 2 lines, got {0}: ".format(result.stdout_str))
+        return dict(zip(*output_payload))
 
     def list_tasks(self):
         cmd = "rally task list --uuids-only"
