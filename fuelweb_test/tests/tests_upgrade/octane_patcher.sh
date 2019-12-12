2185a4e10483e3c42c8f09e1b1299b13f0c7a956
wangfaxin <wangfaxin@inspur.com>
2019-12-12 14:39:13 +0800

    Update the community page.
    Change-Id: I88bc88f75358ebbc8af9ea5aae93b996203b1a14

--- fuelweb_test/tests/tests_upgrade/octane_patcher.sh
+++ fuelweb_test/tests/tests_upgrade/octane_patcher.sh
@@ -4,11 +4,11 @@ PATCH_DIR=$1
 shift
 
 show_only_unmerged() {
-  xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | grep -q "\"status\": \"MERGED\"" && (echo http://review.openstack.org/% MERGED > /dev/stderr) || echo %'
+  xargs -tI% sh -c 'curl -s "https://review.opendev.org/changes/%/detail?O=2002" | grep -q "\"status\": \"MERGED\"" && (echo http://review.opendev.org/% MERGED > /dev/stderr) || echo %'
 }
 
 show_only_unapplied() {
-  xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch -N --follow-symlinks --batch -p2 --silent --dry-run 2>&1 >/dev/null && echo % || (echo http://review.openstack.org/% cant be applied > /dev/stderr)"'
+  xargs -tI% sh -c 'curl -s "https://review.opendev.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.opendev.org/changes/%/revisions/{}/patch?download | base64 -d | patch -N --follow-symlinks --batch -p2 --silent --dry-run 2>&1 >/dev/null && echo % || (echo http://review.opendev.org/% cant be applied > /dev/stderr)"'
 }
 
 cr_filter() {
@@ -20,7 +20,7 @@ cr_filter() {
 apply_patches() {
   cd $1
   shift
-  cr_filter | show_only_unmerged | show_only_unapplied | xargs -tI% sh -c 'curl -s "https://review.openstack.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.openstack.org/changes/%/revisions/{}/patch?download | base64 -d | patch --batch -p2 && echo http://review.openstack.org/% successfully"'
+  cr_filter | show_only_unmerged | show_only_unapplied | xargs -tI% sh -c 'curl -s "https://review.opendev.org/changes/%/detail?O=2002" | sed -nE "/current_revision/ {s/[ ]+?.current_revision.: .//;s/.,\$//p;q}" | xargs -tI{} sh -c "curl -s https://review.opendev.org/changes/%/revisions/{}/patch?download | base64 -d | patch --batch -p2 && echo http://review.opendev.org/% successfully"'
 }
 
 test $# -ge 1 && echo $* | apply_patches ${PATCH_DIR}
